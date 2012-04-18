b69676eaf85dcd8a54466e17bf4d05217e701d3e
William Stein <wstein@gmail.com>
2012-04-18 15:58:26 -0700

    first map.py api

--- src/sage/modular/pollack_stevens/map.py
+++ src/sage/modular/pollack_stevens/map.py
@@ -1,30 +1,32 @@
 
-
-class Map(object):
+class ManinMap(object):
     """
     Map from the set of right cosets of Gamma0(N) in SL_2(Z) to a
-    coefficient module.
+    coefficient module that satisfies the Manin relations.
     """
     def __init__(self, manin_relations, defining_data):
         self._manin_relations = manin_relations
         raise NotImplementedError
-        self._codomain = ?
+        #self._codomain = ?
 
     def __add__(self, right):
         """
-        Return difference self + right.
+        Return difference self + right, where self and right are
+        assumed to have identical codomains and Manin relations.
         """
         raise NotImplementedError
 
     def __sub__(self, right):
         """
-        Return difference self - right.
+        Return difference self - right, where self and right are
+        assumed to have identical codomains and Manin relations.
         """
         raise NotImplementedError
 
     def __mul__(self, left):
         """
-        Return left*self, where left acts on the codomain.
+        Return scalar multiplication left*self, where left acts on the
+        codomain.
         """
         raise NotImplementedError
 
@@ -32,7 +34,34 @@ class Map(object):
         return "Map from the set of right cosets of Gamma0(%s) in SL_2(Z) to %s"%(
             self._manin_relations.level(), self._codomain)
 
-    
+    def __call__(self, A):
+        # NOTE: we should use the high performance specialized type
+        #   sage.matrix.matrix_integer_2x2.Matrix_integer_2x2
+        # for our matrices.
+        raise NotImplementedError
+
+    def right_action(self, gamma):
+        """
+        Returns self | gamma, where gamma is a 2x2 integer matrix.
+        
+        The action is defined by (self | gamma)(D) = self(gamma D)|gamma
+
+        For the action by a single element gamma to be well defined,
+        gamma must normalize Gamma_0(N).  However, this right action
+        can also be used to define Hecke operators, in which case each
+        individual self | gamma is not a modular symbol on Gamma_0(N),
+        but the sum over acting by the appropriate double coset
+        representatives is.
+
+        INPUT:
+
+        - ``gamma`` - 2 x 2 matrix which acts on the values of self
+
+        OUTPU:
+
+        - ManinMap
+        """
+        raise NotImplementedError
     
 
     
