7b8afe92c79b574c5b67e5935abb8d454ffe4fe8
David Roe <roed.math@gmail.com>
2012-04-20 17:17:24 -0700

    Merge branch 'master' of https://github.com/haikona/OMS

--- src/sage/modular/pollack_stevens/padic_lseries.py
+++ src/sage/modular/pollack_stevens/padic_lseries.py
@@ -2,28 +2,31 @@ from sage.all import *
 
 class pAdicLseries(SageObject):
     r"""
-    The `p`-adic `L`-series associated to an overconvergent eigenform.
+    The `p`-adic `L`-series associated to an overconvergent eigensymbol.
     """
 
-    def __init__(self, M, p):
+    def __init__(self, symb):
         r"""
+
+        INPUT:
+            - ``symb`` -- overconvergent eigensymbol
         """
-        self._M = M
-        self._p = p
+        self._symb = symb
 
     def prime(self):
         """
         """
-        return self._p
+        return self._symb.parent().prime()
 
     def _repr_(self):
         """
         Return print representation.
         """
-        s = "%s-adic L-series of %s"%(self._p, self._M)
+        s = "%s-adic L-series of $s"%(self._p, self._symb)
         return s
 
     def series(self, n, quadratic_twist, prec):
         """
         """
         pass
+
