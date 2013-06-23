2df5bfc30dc31561772200662ed070c2f67a3726
Felix Salfelder <felix@em.cs.uni-frankfurt.de>
2013-06-23 11:56:50 +0200

    sage-doc: build system. mostly dist

--- src/doc/autogen.sh
+++ src/doc/autogen.sh
@@ -1,16 +1,11 @@
-#! /bin/sh
-#
-# $Id: autogen.sh,v 1.1 2009-10-23 12:01:23 felix Exp $
+#!/bin/sh
+# this file is part of Sage
+# (c) 2013 Felix Salfelder
+# license: gplv3+
 #
 # Run the various GNU autotools to bootstrap the build
-# system.  Should only need to be done once.
 
-# for now avoid using bash as not everyone has that installed
-CONFIG_SHELL=/bin/sh
-export CONFIG_SHELL
-# ACLOCAL_FLAGS=-Im4
-
-# libtoolize -c -f -i
+# this is supposedly called by toplevel autogen.sh.
 
 echo "Running aclocal..."
 aclocal $ACLOCAL_FLAGS || exit 1
@@ -20,8 +15,3 @@ automake -a -c --gnu --add-missing  || exit 1
 
 echo "Running autoconf..."
 autoconf || exit 1
-
-echo "not Running configure..."
-##./configure $@ || exit 1
-
-
