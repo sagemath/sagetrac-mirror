9e186631ce059d10f92fb298a2eb26643a89a43a
Jean-Pierre Flori <jean-pierre.flori@ssi.gouv.fr>
2013-09-16 09:51:15 +0200

    Adding relaxed implementation of p-adics (in a dirty way).

--- src/sage/libs/mmx/series.pxd
+++ src/sage/libs/mmx/series.pxd
@@ -23,21 +23,48 @@ cdef extern from "algebramix/series.hpp" namespace "mmx":
           pass
      cdef cppclass series_carry_naive:
           pass
+     cdef cppclass series_carry_relaxed[C]:
+          pass
 
-ctypedef series[modular[modulus[int] ], series_carry_p_adic[series_carry_naive] ] mmx_padic
+ctypedef series[modular[modulus[int] ], series_carry_p_adic[series_carry_naive] ] mmx_padic_naive
+ctypedef series[modular[modulus[int] ], series_carry_p_adic[series_carry_relaxed[series_carry_naive]] ] mmx_padic_relaxed
+#ctypedef mmx_padic_naive mmx_padic_relaxed
 
-cdef extern from "algebramix/p_adic.hpp" namespace "mmx":
+# Common stuff
+cdef extern from "numerix/modular.hpp" namespace "mmx":
+     # Cython does not support templated functions
      cdef void mmx_set_modulus "mmx::modular<mmx::modulus<int> >::set_modulus" (int)
-     mmx_padic* create_padic "new mmx::series<mmx::modular<mmx::modulus<int> >, mmx::series_carry_p_adic<mmx::series_carry_naive> >" (const mmx_padic& C)
-     mmx_padic itsc "mmx::integer_to_series_carry<mmx::modular<mmx::modulus<int> >, mmx::series_carry_p_adic<mmx::series_carry_naive> >" (integer)
-     mmx_padic operator + (const mmx_padic& f, const mmx_padic& g)
-     mmx_padic operator - (const mmx_padic& f, const mmx_padic& g)
-     mmx_padic operator * (const mmx_padic& f, const mmx_padic& g)
-     mmx_padic operator / (const mmx_padic& f, const mmx_padic& g)
 
-     syntactic flatten (const mmx_padic& f)
-     port operator << (const port& out, const mmx_padic& x)
+# Naive implementation
+cdef extern from "algebramix/p_adic.hpp" namespace "mmx":
+     # Cython does not support templated functions
+     mmx_padic_naive itsc_naive "mmx::integer_to_series_carry<mmx::modular<mmx::modulus<int> >, mmx::series_carry_p_adic<mmx::series_carry_naive> >" (integer)
 
-cdef class MMXpadic:
-     cdef mmx_padic* x
+     mmx_padic_naive operator + (const mmx_padic_naive& f, const mmx_padic_naive& g)
+     mmx_padic_naive operator - (const mmx_padic_naive& f, const mmx_padic_naive& g)
+     mmx_padic_naive operator * (const mmx_padic_naive& f, const mmx_padic_naive& g)
+     mmx_padic_naive operator / (const mmx_padic_naive& f, const mmx_padic_naive& g)
+
+     syntactic flatten_naive "mmx::flatten" (const mmx_padic_naive& f)
+     port operator << (const port& out, const mmx_padic_naive& x)
+
+# Relaxed implementation
+cdef extern from "algebramix/p_adic.hpp" namespace "mmx":
+     # Cython does not support templated functions
+     mmx_padic_relaxed itsc_relaxed "mmx::integer_to_series_carry<mmx::modular<mmx::modulus<int> >, mmx::series_carry_p_adic<mmx::series_carry_relaxed<mmx::series_carry_naive> > >" (integer)
+
+     mmx_padic_relaxed operator + (const mmx_padic_relaxed& f, const mmx_padic_relaxed& g)
+     mmx_padic_relaxed operator - (const mmx_padic_relaxed& f, const mmx_padic_relaxed& g)
+     mmx_padic_relaxed operator * (const mmx_padic_relaxed& f, const mmx_padic_relaxed& g)
+     mmx_padic_relaxed operator / (const mmx_padic_relaxed& f, const mmx_padic_relaxed& g)
+
+     syntactic flatten_relaxed "mmx::flatten" (const mmx_padic_relaxed& f)
+     port operator << (const port& out, const mmx_padic_relaxed& x)
+
+cdef class MMXpadic_naive:
+     cdef mmx_padic_naive* x
+     cdef int p
+
+cdef class MMXpadic_relaxed:
+     cdef mmx_padic_relaxed* x
      cdef int p
--- src/sage/libs/mmx/series.pyx
+++ src/sage/libs/mmx/series.pyx
@@ -1,54 +1,107 @@
 include "sage/ext/stdsage.pxi"
 
-cdef class MMXpadic:
+cdef class MMXpadic_naive:
      def __cinit__(self):
-         self.x = new mmx_padic()
+         self.x = new mmx_padic_naive()
 
      def __init__(self, int val, int p):
          cdef integer c_val = integer(val)
          self.p = p
          mmx_set_modulus(p)
-         self.x[0] = itsc(c_val)
-#         self.x = create_padic(itsc(c_val))
+         self.x[0] = itsc_naive(c_val)
+#         self.x = new mmx_padic_naive(itsc(c_val))
 
      def __dealloc__(self):
          del self.x
 
-     def __add__(MMXpadic self, other):
-         cdef MMXpadic res = MMXpadic.__new__(MMXpadic)
-         if isinstance(other, MMXpadic):
+     def __add__(MMXpadic_naive self, other):
+         cdef MMXpadic_naive res = MMXpadic_naive.__new__(MMXpadic_naive)
+         if isinstance(other, MMXpadic_naive):
              mmx_set_modulus(self.p)
              res.p = self.p
-             res.x[0] = <mmx_padic> self.x[0] + <mmx_padic> (<MMXpadic>other).x[0]
+             res.x[0] = <mmx_padic_naive> self.x[0] + <mmx_padic_naive> (<MMXpadic_naive>other).x[0]
          return res
 
-     def __sub__(MMXpadic self, other):
-         cdef MMXpadic res = MMXpadic.__new__(MMXpadic)
-         if isinstance(other, MMXpadic):
+     def __sub__(MMXpadic_naive self, other):
+         cdef MMXpadic_naive res = MMXpadic_naive.__new__(MMXpadic_naive)
+         if isinstance(other, MMXpadic_naive):
              mmx_set_modulus(self.p)
              res.p = self.p
-             res.x[0] = <mmx_padic> self.x[0] - <mmx_padic> (<MMXpadic>other).x[0]
+             res.x[0] = <mmx_padic_naive> self.x[0] - <mmx_padic_naive> (<MMXpadic_naive>other).x[0]
          return res
 
-     def __mul__(MMXpadic self, other):
-         cdef MMXpadic res = MMXpadic.__new__(MMXpadic)
-         if isinstance(other, MMXpadic):
+     def __mul__(MMXpadic_naive self, other):
+         cdef MMXpadic_naive res = MMXpadic_naive.__new__(MMXpadic_naive)
+         if isinstance(other, MMXpadic_naive):
              mmx_set_modulus(self.p)
              res.p = self.p
-             res.x[0] = <mmx_padic> self.x[0] * <mmx_padic> (<MMXpadic>other).x[0]
+             res.x[0] = <mmx_padic_naive> self.x[0] * <mmx_padic_naive> (<MMXpadic_naive>other).x[0]
          return res
 
-     def __div__(MMXpadic self, other):
-         cdef MMXpadic res = MMXpadic.__new__(MMXpadic)
-         if isinstance(other, MMXpadic):
+     def __div__(MMXpadic_naive self, other):
+         cdef MMXpadic_naive res = MMXpadic_naive.__new__(MMXpadic_naive)
+         if isinstance(other, MMXpadic_naive):
              mmx_set_modulus(self.p)
              res.p = self.p
-             res.x[0] = <mmx_padic> self.x[0] / <mmx_padic> (<MMXpadic>other).x[0]
+             res.x[0] = <mmx_padic_naive> self.x[0] / <mmx_padic_naive> (<MMXpadic_naive>other).x[0]
          return res
 
      def __repr__(self):
-         return <bytes> as_charp(as_mmx(as_generic(flatten(self.x[0]))))
+         return <bytes> as_charp(as_mmx(as_generic(flatten_naive(self.x[0]))))
 
-     def my_print(MMXpadic self):
-         mmout << flatten(self.x[0])
+     def my_print(MMXpadic_naive self):
+         mmout << flatten_naive(self.x[0])
+         print
+
+cdef class MMXpadic_relaxed:
+     def __cinit__(self):
+         self.x = new mmx_padic_relaxed()
+
+     def __init__(self, int val, int p):
+         cdef integer c_val = integer(val)
+         self.p = p
+         mmx_set_modulus(p)
+         self.x[0] = itsc_relaxed(c_val)
+#         self.x = new mmx_padic_relaxed(itsc(c_val))
+
+     def __dealloc__(self):
+         del self.x
+
+     def __add__(MMXpadic_relaxed self, other):
+         cdef MMXpadic_relaxed res = MMXpadic_relaxed.__new__(MMXpadic_relaxed)
+         if isinstance(other, MMXpadic_relaxed):
+             mmx_set_modulus(self.p)
+             res.p = self.p
+             res.x[0] = <mmx_padic_relaxed> self.x[0] + <mmx_padic_relaxed> (<MMXpadic_relaxed>other).x[0]
+         return res
+
+     def __sub__(MMXpadic_relaxed self, other):
+         cdef MMXpadic_relaxed res = MMXpadic_relaxed.__new__(MMXpadic_relaxed)
+         if isinstance(other, MMXpadic_relaxed):
+             mmx_set_modulus(self.p)
+             res.p = self.p
+             res.x[0] = <mmx_padic_relaxed> self.x[0] - <mmx_padic_relaxed> (<MMXpadic_relaxed>other).x[0]
+         return res
+
+     def __mul__(MMXpadic_relaxed self, other):
+         cdef MMXpadic_relaxed res = MMXpadic_relaxed.__new__(MMXpadic_relaxed)
+         if isinstance(other, MMXpadic_relaxed):
+             mmx_set_modulus(self.p)
+             res.p = self.p
+             res.x[0] = <mmx_padic_relaxed> self.x[0] * <mmx_padic_relaxed> (<MMXpadic_relaxed>other).x[0]
+         return res
+
+     def __div__(MMXpadic_relaxed self, other):
+         cdef MMXpadic_relaxed res = MMXpadic_relaxed.__new__(MMXpadic_relaxed)
+         if isinstance(other, MMXpadic_relaxed):
+             mmx_set_modulus(self.p)
+             res.p = self.p
+             res.x[0] = <mmx_padic_relaxed> self.x[0] / <mmx_padic_relaxed> (<MMXpadic_relaxed>other).x[0]
+         return res
+
+     def __repr__(self):
+         return <bytes> as_charp(as_mmx(as_generic(flatten_relaxed(self.x[0]))))
+
+     def my_print(MMXpadic_relaxed self):
+         mmout << flatten_relaxed(self.x[0])
          print
