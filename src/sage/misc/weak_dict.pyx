e60890eea7e9d108431d183c897b37482b12e4cf
Simon King <simon.king@uni-jena.de>
2013-10-31 23:23:43 +0100

    Add direct and indirect stresstests for the weak value callbacks

--- src/sage/misc/weak_dict.pyx
+++ src/sage/misc/weak_dict.pyx
@@ -4,7 +4,7 @@ Fast and safe weak value dictionary
 AUTHORS:
 
 - Simon King (2013-10)
-- Nils Bruin (2013-10)
+- Nils Bruin (2013-10)src/sage/misc/weak_dict.pyx1
 
 Python's :mod:`weakref` module provides
 :class:`~weakref.WeakValueDictionary`. This behaves similar to a dictionary,
@@ -107,6 +107,7 @@ Note that Sage's weak value dictionary is actually an instance of
     sage: issubclass(sage.misc.weak_dict.WeakValueDictionary, dict)
     True
 
+See :trac:`13394` for a discussion of some of the design considerations.
 """
 ########################################################################
 #       Copyright (C) 2013 Simon King <simon.king@uni-jena.de>
@@ -137,6 +138,8 @@ cdef extern from "Python.h":
         Py_ssize_t ma_used
         Py_ssize_t ma_mask
         PyDictEntry* ma_table
+
+    PyObject* Py_None
     #we need this redefinition because we want to be able to call
     #PyWeakref_GetObject with borrowed references. This is the recommended
     #strategy according to Cython/Includes/cpython/__init__.pxd
@@ -173,6 +176,44 @@ cdef PyObject* dummy = init_dummy()
 #this routine looks for the first entry in dict D with given hash of the
 #key and given (identical!) value and deletes that entry.
 cdef del_dictitem_by_exact_value(PyDictObject *mp, PyObject *value, long hash):
+    """
+    This is used in callbacks for the weak values of :class:`WeakValueDictionary`.
+
+    INPUT:
+
+    - ``PyDictObject *mp`` -- pointer to a dict
+    - ``PyObject *value``  -- pointer to a value of the dictionary
+    - ``long hash``        -- hash of the key by which the value is stored in the dict
+
+    The hash bucket determined by the given hash is searched for the item
+    containing the given value. If this item can't be found, the function is
+    silently returning. Otherwise, the item is removed from the dict.
+
+    TESTS:
+
+    The following is an indirect doctest, as discussed on :trac:`13394`.
+    ::
+
+        sage: from sage.misc.weak_dict import WeakValueDictionary
+        sage: V = [set(range(n)) for n in range(5)]
+        sage: D = WeakValueDictionary(enumerate(V))
+
+    The line ``V[k] = None`` triggers execution of the callback functions of
+    the dict values. However, the actual deletion is postponed till after the
+    iteration over the dictionary has finished. Hence, when the callbacks are
+    executed, the values which the callback belongs to has already been
+    overridded by a new value. Therefore, the callback does not delete the
+    item::
+
+        sage: for k in D.iterkeys():    # indirect doctest
+        ....:     V[k] = None
+        ....:     D[k] = ZZ
+        sage: len(D)
+        5
+        sage: D[1]
+        Integer Ring
+
+    """
     cdef size_t i
     cdef size_t perturb
     cdef size_t mask = <size_t> mp.ma_mask
@@ -186,7 +227,7 @@ cdef del_dictitem_by_exact_value(PyDictObject *mp, PyObject *value, long hash):
         return
 
     perturb = hash
-    while (<PyObject *>(ep.me_value) != value):
+    while (<PyObject *>(ep.me_value) != value or ep.me_hash != hash):
         i = (i << 2) + i + perturb +1
         ep = &ep0[i & mask]
         if ep.me_key == NULL:
@@ -208,7 +249,59 @@ cdef del_dictitem_by_exact_value(PyDictObject *mp, PyObject *value, long hash):
     #this could have any effect.
     Py_XDECREF(old_key)
 
-cdef PyObject* Py_None = <PyObject*>None
+def test_del_dictitem_by_exact_value(D, value, h):
+    """
+    This function helps testing some cdef function used to delete dictionary items.
+
+    INPUT:
+
+    - ``D`` -- a Python ``<dict>``.
+    - ``value`` -- an object that is value ``D``.
+    - ``h`` -- the hash of the key under which to find ``value`` in ``D``.
+
+    The underlying cdef function deletes an item from ``D`` that is in the
+    hash bucket determined by ``h`` and whose value is identic with
+    ``value``. Of course, this only makes sense if the pairs ``(h, value)``
+    corresponding to items in ``D`` are pair-wise distinct.
+
+    If a matching item can not be found, the function does nothing and
+    silently returns.
+
+    TESTS:
+
+    See :trac:`13394` for a discussion.
+    ::
+
+        sage: from sage.misc.weak_dict import test_del_dictitem_by_exact_value
+        sage: B=1000
+        sage: L=list(range(B))
+        sage: D1=dict()
+        sage: D2=dict()
+        sage: for i in range(100000):        # long time
+        ....:     ki=L[floor(random()*B)]
+        ....:     vi=L[floor(random()*B)]
+        ....:     D1[ki]=vi
+        ....:     D2[ki]=vi
+        ....:     ko=L[floor(random()*B)]
+        ....:     if ko in D1:
+        ....:         vo=D1[ko]
+        ....:         del D1[ko]
+        ....:         test_del_dictitem_by_exact_value(D2,vo,hash(ko))
+        ....:     assert D1 == D2
+
+    No action is taken if the item prescribed by key hash and value does not
+    exist in the dictionary::
+
+        sage: D = {1: ZZ}
+        sage: test_del_dictitem_by_exact_value(D, ZZ, 2)
+        sage: D
+        {1: Integer Ring}
+        sage: test_del_dictitem_by_exact_value(D, QQ, 1)
+        sage: D
+        {1: Integer Ring}
+
+    """
+    return del_dictitem_by_exact_value(<PyDictObject *>D, <PyObject *>value, h)
 
 cdef class WeakValueDictionary(dict):
     """
@@ -253,6 +346,54 @@ cdef class WeakValueDictionary(dict):
         sage: del v
         sage: len(D)
         0
+
+    TESTS::
+
+    The following reflects the behaviour of the callback ov weak dict values,
+    as discussed on :trac:`13394`.  ::
+
+        sage: from sage.misc.weak_dict import WeakValueDictionary
+        sage: V = [set(range(n)) for n in range(5)]
+        sage: D = WeakValueDictionary(enumerate(V))
+
+    The line ``V[k] = None`` triggers execution of the callback functions of
+    the dictionary values. However, the actual deletion is postponed till
+    after the iteration over the dictionary has finished. Hence, when the
+    callbacks are executed, the values which the callback belongs to has
+    already been overridded by a new value. Therefore, the callback does not
+    delete the item::
+
+        sage: for k in D.iterkeys():    # indirect doctest
+        ....:     V[k] = None
+        ....:     D[k] = ZZ
+        sage: len(D)
+        5
+        sage: D[1]
+        Integer Ring
+
+    The following is a stress test for weak value dictionaries::
+
+        sage: class C(object):
+        ....:     def __init__(self, n):
+        ....:         self.n = n
+        ....:     def __cmp__(self, other):
+        ....:         return cmp(type(self),type(other)) or cmp(self.n, other.n)
+        sage: B = 100
+        sage: L = [None]*B
+        sage: D1 = WeakValueDictionary()
+        sage: D2 = WeakValueDictionary()
+        sage: for i in range(10000):
+        ....:     ki = floor(random()*B)
+        ....:     vi = C(floor(random()*B))
+        ....:     D1[ki] = vi
+        ....:     D2[ki] = vi
+        ....:     L[ki]  = vi
+        ....:     del vi
+        ....:     ko = floor(random()*B)
+        ....:     if ko in D1:
+        ....:         del D1[ko]
+        ....:         L[ko] = None
+        ....:     assert D1 == D2
 
     """
     cdef callback
