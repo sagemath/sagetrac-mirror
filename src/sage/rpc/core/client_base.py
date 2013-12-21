8f20fdfad08c19988dcb41a1da958e0c882bd8cf
Volker Braun <vbraun.name@gmail.com>
2013-12-21 02:20:29 +0000

    add framework for extra initialization data

--- src/sage/rpc/core/client_base.py
+++ src/sage/rpc/core/client_base.py
@@ -42,6 +42,35 @@ class ClientBase(RemoteProcedureCaller):
         self.remote_log = logger_server if remote_logger is None else remote_logger
         super(ClientBase, self).__init__(transport, cookie)
 
+    def init_receive_extra(self, extra):
+        """
+        Receive additional data during initialization.
+
+        Override this method for an easy way to get additional
+        initialization data. It is not permitted to make RPC calls at
+        this point in the negotiation.
+
+        INPUT:
+
+        - ``extra`` -- the output of :meth:`server_base.send_extra` on
+          the other end of the connection.
+        """
+        pass
+
+    def init_send_extra(self):
+        """
+        Send additional data during initialization.
+
+        Override this method for an easy way to pass additional
+        initialization data. It is not permitted to make RPC calls at
+        this point in the negotiation.
+
+        OUTPUT:
+
+        Anything that is a JSON serializeable dictionary value.
+        """
+        return None
+
     def handle_init_connection(self, request):
         self.log.debug('initializing connection %s', request)
         if request.get('type') != TYPE_INIT_CONNECTION:
@@ -62,10 +91,13 @@ class ClientBase(RemoteProcedureCaller):
             error = 'no table of rpc calls'
             self.log.critical(error)
             raise ClientException(error)
+        self.init_receive_extra(request.get('extra', None))
         self._init_remote_rpc(rpc_table)
         self._transport.write({
             'type': TYPE_INIT_REPLY,
             'rpc_table': list(self._rpc.keys()),
-            'ok': True})
+            'ok': True,
+            'extra': self.init_send_extra(),
+        })
         self.rpc.util.remote_is_ready()   # the first RPC call
 
