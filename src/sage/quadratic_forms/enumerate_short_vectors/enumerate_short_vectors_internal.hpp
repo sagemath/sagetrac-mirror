f337fae187c4e1b6dfaa5377d40f0a68bf9cbdaa
Martin Raum <martin@raum-brothers.eu>
2014-01-24 14:57:48 +0100

    Refactor enumeration of short vectors

--- src/sage/quadratic_forms/enumerate_short_vectors/enumerate_short_vectors_internal.hpp
+++ src/sage/quadratic_forms/enumerate_short_vectors/enumerate_short_vectors_internal.hpp
@@ -1,8 +1,33 @@
+/**
+ *
+ * Copyright (C) 2013 Martin Raum
+ * 
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 3
+ * of the License, or (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful, 
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
 #ifndef __ENUMERATE_SHORT_VECTORS_INTERNAL_H
 #define __ENUMERATE_SHORT_VECTORS_INTERNAL_H
 
+#include <vector>
+
 #include "mpfr.h"
 #include "mpfi.h"
+
+
+static const size_t precision_increment = 3;
+
 
 bool
 cholesky_decomposition( const std::vector<std::vector<int>>&,
