9d0cec705ccc5e00e4b643a09db57ff2746d41ea
Travis Scrimshaw <tscrim at ucdavis.edu>
2014-01-30 23:30:44 -0800

    Work on generalizing the Groebner basis for PBW algebras.

--- src/sage/algebras/pbw_algebra.py
+++ src/sage/algebras/pbw_algebra.py
@@ -19,6 +19,7 @@ Algebras in a PBW(-type) basis.
 #                  http://www.gnu.org/licenses/
 #*****************************************************************************
 
+from copy import copy
 from sage.misc.cachefunc import cached_method
 from sage.categories.algebras_with_basis import AlgebrasWithBasis
 from sage.rings.all import ZZ
@@ -266,23 +267,21 @@ class PBWBasisOfFreeAlgebra(CombinatorialFreeModule):
         """
         Return the number of generators of ``self``.
         """
-        return len(self.gens())
+        return self._basis_keys.ngens()
 
     def ideal(self, *args, **kwds):
         """
         Return a side ``side`` ideal of ``self`` given by ``gens``.
         """
-        if kwds.get('side', 'twosided') == 'twosided':
-            return TwoSidedPBWIdeal(self, args)
-        return super(PBWBasisOfFreeAlgebra, self).ideal(self, *args, **kwds)
+        return PBWIdeal(self, args, side=kwds.get('side', 'twosided'))
 
     def quotient(self, I, names=None, category=None):
         """
         Return the quotient of ``self`` by ``I``.
         """
         from sage.algebras.finitely_presented_algebra import FinitelyPresentedAlgebra
-        if not isinstance(I, TwoSidedPBWIdeal):
-            raise TypeError("must be a two sided algebra ideal")
+        if not isinstance(I, PBWIdeal) or I.side() != 'twosided':
+            raise TypeError("must be a two sided PBW algebra ideal")
         return FinitelyPresentedAlgebra(self, I, names)
 
     def free_algebra(self):
@@ -359,7 +358,23 @@ class PBWBasisOfFreeAlgebra(CombinatorialFreeModule):
         return sum([i[1] * self._alg.lie_polynomial(i[0]) for i in list(t)],
                    self._alg.zero())
 
+    def standard_monomial(self, exp, mcmp=None):
+        """
+        Return the standard monomial given by ``exp``.
+
+        INPUT:
+
+        - ``exp`` -- a dictionary whose keys are indices of the generators
+          and values are the exponents
+        """
+        return self.monomial(k**exp[k] for k in sorted(exp, cmp=mcmp))
+        # A generic implementation of this would be:
+        # return self.prod(self.monomial(k)**exp[k] for k in sorted(exp, cmp=mcmp))
+
     class Element(CombinatorialFreeModuleElement):
+        """
+        An element in the PBW basis of the free algebra.
+        """
         def expand(self):
             """
             Expand ``self`` in the monomials of the free algebra.
@@ -375,21 +390,42 @@ class PBWBasisOfFreeAlgebra(CombinatorialFreeModule):
             """
             return self.parent().expansion(self)
 
-class TwoSidedPBWIdeal(Ideal_nc):
+        def leading_exponent(self, mcmp=None):
+            r"""
+            Return the leading exponent of ``self`` as a dictionary.
+
+            The exponent of a monomial `m = x_1^{\alpha_1} \cdots
+            x_k^{\alpha_k}` is the list of powers
+            `[\alpha_1, \ldots, \alpha_k]`.
+            """
+            lf = self.leading_support(mcmp).to_word().lyndon_factorization()
+            exp = {}
+            for f in lf:
+                f = self._basis_keys(f)
+                exp[f] += exp.get(f, 0) + 1
+            return exp
+
+class PBWIdeal(Ideal_nc):
     """
-    A two-sided ideal of an algebra in a PBW(-type) basis.
+    An ideal of an algebra in a PBW(-type) basis.
     """
-    def __init__(self, pbw_algebra, gens):
+    def __init__(self, pbw_algebra, gens, coerce=True, side="twosided"):
         """
         Initialize ``self``.
         """
-        gb = map(lambda x: x / x.leading_coefficient(), gens)
-        self._gb = [1, gb]
-        n = len(pbw_algebra.algebra_generators())
-        self._gb_todo = [(g, h) for g in gb for h in gb]
-        self._gb_todo += [(g, ZZ(i)) for g in gb for i in range(n)]
+        Ideal_nc.__init__(self, pbw_algebra, gens, side)
 
-        Ideal_nc.__init__(self, pbw_algebra, gens, "twosided")
+        gens = self.gens()
+        gb = map(lambda x: x / x.leading_coefficient(), gens)
+        self._gb = gb
+        self._gb_todo = [(g, h) for g in gb for h in gb]
+
+        if side == "twosided":
+            try:
+                keys = pbw_algebra.algebra_generators().keys()
+            except AttributeError:
+                keys = range(len(pbw_algebra.algebra_generators()))
+            self._gb_todo.extend((g, ('ts', k)) for g in gb for k in keys)
 
     def pbw_basis(self):
         """
@@ -408,77 +444,72 @@ class TwoSidedPBWIdeal(Ideal_nc):
             return c
         return cmp(y, x)
 
-    def partial_groebner_basis(self, d):
-        """
-        Return a partial Groebner basis of ``self`` up to degree ``d``.
-        """
-        l_cmp = TwoSidedPBWIdeal._lead_cmp
-        PBW = self.ring()
-        if self._gb[0] is None:
-            return tuple(map(PBW, self._gb[1]))
-        if d <= self._gb[0]:
-            return tuple(map(PBW, filter(lambda x: len(x.leading_support(l_cmp)) <= d,
-                                         self._gb[1])))
-
-        # Run Groebner basis algorithm on the PBW gens
-        gens = self._gb[1] # The gens are in the PBW basis
-        n = len(PBW.algebra_generators())
-        FM = PBW.basis().keys()
-        PBW_from_exp = lambda x: PBW.monomial(FM( [a for a in enumerate(x) if a[1] > 0] ))
-
-        cur = 0
-        while cur < len(self._gb_todo):
-            p = self._gb_todo[cur]
-            if len(p[0].leading_support(l_cmp)) >= d:
-                cur += 1
-                continue
-
-            if p[1] in ZZ:
-                h = p[0]*PBW.gen(p[1]) - PBW.gen(p[1])*p[0]
-            else:
-                if len(p[0].leading_support(l_cmp)) + len(p[1].leading_support(l_cmp)) > d:
-                    cur += 1
-                    continue
-
-                f = map(lambda x: x.leading_support(l_cmp).to_word().evaluation(), p)
-                lcm = list(f[0])
-                for i,v in enumerate(f[1]):
-                    if i > len(lcm):
-                        lcm.append(v)
-                    else:
-                        lcm[i] = max(lcm[i], v)
-                f = map(lambda x: [lcm[i] - v for i,v in enumerate(x)], f)
-                h = PBW_from_exp(f[0]) * p[0] - p[1] * PBW_from_exp(f[1])
-
-            self._gb_todo.pop(cur) # We have taken care of the pairing
-            h = self._normal_form(h, gens)
-
-            if h != PBW.zero():
-                h /= h.leading_coefficient(l_cmp)
-                for g in gens:
-                    self._gb_todo.append((g, h))
-                for i in range(n):
-                    self._gb_todo.append((g, ZZ(i)))
-                gens.append(h)
-
-        if len(self._gb_todo) == 0:
-            d = infinity
-        self._gb = [d, gens]
-        # Convert back
-        if d is None:
-            return tuple(gens)
-        # Remove all elements with larger degree
-        return tuple(filter(lambda x: len(x.leading_support(l_cmp)) <= d, gens))
-
-    def groebner_basis(self):
+    def groebner_basis(self, max_steps=infinity):
         """
         Return a Groebner basis of ``self``.
+
+        INPUT:
+
+        - ``max_steps`` -- (default: infinity) the maximum number of steps to
+          do before terminating
 
         .. WARNING::
 
             This will run forever if the Groebner basis is infinite.
         """
-        return self.partial_groebner_basis(infinity)
+        # Setup variables and functions
+        l_cmp = PBWIdeal._lead_cmp
+        PBW = self.ring()
+
+        n = len(PBW.algebra_generators())
+        FM = PBW.basis().keys()
+        side = self.side()
+        if side == "twosided":
+            gens = PBW.algebra_generators()
+            try:
+                keys = gens.keys()
+            except AttributeError:
+                keys = range(len(gens))
+
+        # Run Groebner basis algorithm
+
+        n_steps = 0
+        while len(self._gb_todo) > 0 and n_steps < max_steps:
+            n_steps += 1
+            p = self._gb_todo.pop()
+
+            if isinstance(p[1], tuple) and p[1][0] == 'ts':
+                i = p[1][1]
+                h = p[0]*gens[i] - gens[i]*p[0]
+            else:
+                # Compute the lcm of the leading terms
+                f = map(lambda x: x.leading_exponent(l_cmp), p)
+                lcm = copy(f)
+                for k,v in f[1].items():
+                    lcm[k] = max(lcm.get(k, 0), v)
+                f = map(lambda x: {k:lcm[k] - v for k,v in x.items()}, f)
+                # Compute the appropriate (left/right) S-polynomial
+                if side == 'right':
+                    h = p[1] * PBW.standard_monomial(f[0], l_cmp) - p[1] * PBW.standard_monomial(f[1], l_cmp)
+                else:
+                    h = PBW.standard_monomial(f[0], l_cmp) * p[0] - PBW.standard_monomial(f[1], l_cmp) * p[1]
+
+            h = self._normal_form(h, self._gb)
+
+            if h != PBW.zero():
+                h /= h.leading_coefficient(l_cmp)
+                if side == "twosided":
+                    for g in self._gb:
+                        self._gb_todo.append((g, h))
+                        self._gb_todo.extend([(g, ('ts', k)) for k in keys])
+                else:
+                    self._gb_todo.extend((g, h) for g in self._gb)
+                self._gb.append(h)
+
+        if len(self._gb_todo) != 0 and n_steps == max_steps:
+            print "WARNING: returning an incomplete Groebner basis"
+
+        return tuple(self._gb)
 
     def _normal_form(self, x, G):
         """
@@ -488,7 +519,8 @@ class TwoSidedPBWIdeal(Ideal_nc):
         PBW = self.ring()
         ret = PBW.zero()
         FM = PBW.basis().keys()
-        l_cmp = TwoSidedPBWIdeal._lead_cmp
+        l_cmp = PBWIdeal._lead_cmp
+        side = self.side()
 
         while x != 0:
             u, la = x.leading_item(l_cmp)
@@ -496,24 +528,40 @@ class TwoSidedPBWIdeal(Ideal_nc):
             for g in G:
                 LM, mu = g.leading_item(l_cmp)
 
+                # TODO: Make this work for general PBW rings
                 # Factor u by LM if possible
                 w = u.to_word()
                 f = LM.to_word().first_pos_in(w)
                 if f is not None:
                     found = True
-                    x -= la / mu * PBW(FM(w[:f])) * g * PBW(FM(w[f+len(LM):]))
+                    if side == 'twosided':
+                        x -= la / mu * PBW(FM(w[:f])) * g * PBW(FM(w[f+len(LM):]))
+                    elif side == 'left':
+                        x -= la / mu * PBW(FM(w[:f]) * FM(w[f+len(LM):])) * g
+                    elif side == 'right':
+                        x -= la / mu * g * PBW(FM(w[:f]) * FM(w[f+len(LM):]))
                     break
             if not found:
                 ret += la * PBW(u)
                 x -= la * PBW(u)
         return ret
 
-    def reduce(self, x):
+    def reduce(self, x, max_steps=infinity):
         """
         Return ``x`` modulo ``self``.
+
+        INPUT:
+
+        - ``max_steps`` -- (default: infinity) the maximum number of steps to
+          compute in the Groebner basis before terminating
+
+        .. WARNING::
+
+            If this terminates after ``max_steps``, the resulting reduction
+            may not fully reduce the element.
         """
         if x == self.ring().zero():
             return x
-        G = self.partial_groebner_basis( len(x.leading_support()) )
+        G = self.groebner_basis(max_steps)
         return self._normal_form(x, G)
 
