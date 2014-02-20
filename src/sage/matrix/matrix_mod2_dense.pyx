f37a617bf36466391ad9787cc7e9729db387378a
Andrew Silver <aesilver@zoho.com>
2014-02-20 08:59:35 -0500

    fixed indentation in matrix mod 2 dense file
    indentation error was introduced in earlier commit, updated accordingly

--- src/sage/matrix/matrix_mod2_dense.pyx
+++ src/sage/matrix/matrix_mod2_dense.pyx
@@ -1662,7 +1662,7 @@ cdef class Matrix_mod2_dense(matrix_dense.Matrix_dense):   # dense or sparse
             Z._subdivide_on_stack(self, other)
         return Z
    
-   def __reduce__(self):
+    def __reduce__(self):
         """
         Serialize ``self``.
 
