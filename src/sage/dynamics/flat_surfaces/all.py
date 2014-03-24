2c43d896aa95487ac2137fc6ff2ab61a3561a881
Unknown User <unknown@sagemath.org>
2014-03-24 05:57:56 +0000

    Strata of Abelian/quadratic differentials
     * implement connected components for quadratic differentials
     * implement Zorich's permutation representative for each quadratic stratum
     * creation of a database which reads .sobj files in the directory SAGE_ROOT/data/flast_surfaces/
     * compute connected components of linear involutions (which use database)
     * modification of erase_marked_points for PermutationIET
    
    Todo
     * doc for erase_marked_points
     * erase_marked_points for PermutationLI

--- src/sage/dynamics/flat_surfaces/all.py
+++ src/sage/dynamics/flat_surfaces/all.py
@@ -1,5 +1,5 @@
 from sage.misc.lazy_import import lazy_import
-lazy_import("sage.dynamics.flat_surfaces.strata",
+lazy_import("sage.dynamics.flat_surfaces.abelian_strata",
             ["AbelianStrata", "AbelianStratum"])
 lazy_import("sage.dynamics.flat_surfaces.quadratic_strata",
-            ["QuadraticStratum"])
+            ["QuadraticStrata", "QuadraticStratum"])
