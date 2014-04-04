1e29bf526e545a67b0ecb629355838098b6331d8
Travis Scrimshaw <tscrim at ucdavis.edu>
2014-04-03 19:29:18 -0700

    Commented out global namespace inclusions.

--- src/sage/algebras/quantum_groups/all.py
+++ src/sage/algebras/quantum_groups/all.py
@@ -1,9 +1,9 @@
 """
-Algebras
+Quantum Groups
 """
 
 #*****************************************************************************
-#       Copyright (C) 2005 William Stein <wstein@gmail.com>
+#       Copyright (C) 2014 Travis Scrimshaw <tscrim at ucdavis.edu>
 #
 #  Distributed under the terms of the GNU General Public License (GPL)
 #
@@ -16,30 +16,7 @@ Algebras
 #
 #                  http://www.gnu.org/licenses/
 #*****************************************************************************
-from sage.misc.lazy_import import lazy_import
 
-from quatalg.all import *
+#from sage.misc.lazy_import import lazy_import
+#lazy_import('sage.algebras.quantum_groups.representations', 'GlobalCrystalBasis')
 
-# Algebra base classes
-from algebra import Algebra, is_Algebra
-
-# Ring element base classes
-from algebra_element import AlgebraElement, is_AlgebraElement
-
-
-from free_algebra import FreeAlgebra, is_FreeAlgebra
-from free_algebra_quotient import FreeAlgebraQuotient
-
-from steenrod.all import *
-
-from finite_dimensional_algebras.all import FiniteDimensionalAlgebra
-
-from group_algebra_new import GroupAlgebra
-
-from iwahori_hecke_algebra import IwahoriHeckeAlgebra, IwahoriHeckeAlgebraT
-from affine_nil_temperley_lieb import AffineNilTemperleyLiebTypeA
-lazy_import('sage.algebras.nil_coxeter_algebra', 'NilCoxeterAlgebra')
-
-lazy_import('sage.algebras.hall_algebra', 'HallAlgebra')
-
-lazy_import('sage.algebras.shuffle_algebra', 'ShuffleAlgebra')
