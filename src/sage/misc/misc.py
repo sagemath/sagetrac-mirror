43d7418b04a7e4f630c35789a0d5e94c89f8ebbf
Darij Grinberg <darijgrinberg@gmail.com>
2014-04-17 19:47:41 -0700

    a working doctest

--- src/sage/misc/misc.py
+++ src/sage/misc/misc.py
@@ -12,7 +12,15 @@ AUTHORS:
 
 TESTS:
 
-Check the fix from trac #8323::
+The following test, verifying that :trac:`16181` has been resolved, needs
+to stay at the beginning of this file so that its context is not
+poisoned by other tests::
+
+    sage: sage.misc.misc.inject_variable('a', 0)
+    sage: a
+    0
+
+Check the fix from :trac:`8323`::
 
     sage: 'name' in globals()
     False
