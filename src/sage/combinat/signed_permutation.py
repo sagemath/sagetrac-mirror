d7642e9b1154cc2db34c31cdae49a97188a26a9b
Sebastian Csar <sacsar@gmail.com>
2014-05-29 17:50:59 -0500

    Import permutation, add descent and negative entry statistics

--- src/sage/combinat/signed_permutation.py
+++ src/sage/combinat/signed_permutation.py
@@ -1,5 +1,6 @@
 from sage.structure.element import Element
 from sage.structure.parent import Parent
+from sage.combinat.permutation import Permutation
 import itertools
 
 class SignedPermutation(Element):
@@ -35,8 +36,47 @@ class SignedPermutation(Element):
         return repr(self.w)
 
     def abs(self):
-        """Returns a Permtuation object `w` such that `w(i)=|self(i)`"""
+        """Returns a Permtuation object `w` such that `w(i)=|self(i)|`"""
         return Permutation([abs(i) for i in self.w])
+
+    @lazy_attribute
+    def _descents(self):
+        """Calculates where the descents are"""
+        descents = []
+        if self.w[0] < 0:
+            descents.append(0)
+        for i in range(self.n):
+            a = self.w[i-1]
+            b = self.w[i]
+            if a > 0 and b < 0:
+                if a > b:
+                    descents.append(i)
+            elif a < 0 and b > 0:
+                if a < b:
+                    descents.append(i)
+        if self.w[-1] < 0:
+            descents.append(self.n)
+        return descents
+
+    def descents(self):
+        """Returns a list of positions of descents"""
+        return self._descents
+
+    def number_of_descents(self):
+        """Returns the number of descents of `self`"""
+        return len(self._descents)
+
+    @lazy_attribute
+    def _neg_entries(self):
+        return [i+1 for (i,j) in enumerate(self.w) if j < 0]
+    
+    def negative_entries(self):
+        """Returns list of `i` such that `self(i) < 0`"""
+        return self._neg_entries
+
+    def number_of_negative_entries(self):
+        """Returns the number of `i` such that `self(i) < 0`"""
+        return len(self._neg_entries)
         
 class SignedPermutations(Parent):
     r"""
