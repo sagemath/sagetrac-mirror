29b9dd1571be1116f5bf05d23d89f3e6d899e434
Nathann Cohen <nathann.cohen@gmail.com>
2014-06-16 14:36:26 +0200

    Additional Constructions

--- src/sage/combinat/designs/orthogonal_arrays.py
+++ src/sage/combinat/designs/orthogonal_arrays.py
@@ -902,6 +902,7 @@ def orthogonal_array(k,n,t=2,check=True,existence=False,who_asked=tuple()):
     from latin_squares import mutually_orthogonal_latin_squares
     from database import OA_constructions
     from block_design import projective_plane, projective_plane_to_OA
+    from orthogonal_arrays_recursive import find_recursive_construction
 
     # If k is set to None we find the largest value available
     if k is None:
@@ -986,6 +987,12 @@ def orthogonal_array(k,n,t=2,check=True,existence=False,who_asked=tuple()):
         OA = orthogonal_array(k+2,r,check=False)
         OA = OA_relabel(OA,k+2,r,matrix=[range(r)]*k+[range(u1)+[None]*(r-u1),range(u2)+[None]*(r-u2)])
         OA = wilson_construction(OA,k,r,m,2,[u1,u2],check=False)
+
+    elif may_be_available and find_recursive_construction(k,n,existence=True):
+        _OA_cache_set(k,n,True)
+        if existence:
+            return True
+        OA = find_recursive_construction(k,n)
 
     elif (t == 2 and transversal_design not in who_asked and
           transversal_design(k,n,existence=True,who_asked=who_asked+(orthogonal_array,)) is not Unknown):
