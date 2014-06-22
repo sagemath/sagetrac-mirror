57301d226c41224d874afbdb7317936579cd50a6
Travis Scrimshaw <tscrim at ucdavis.edu>
2014-06-21 22:53:18 -0700

    Simply copied over relevent functions to Semirings.Differential().
    This is not necessarily the correct, nor best place for it.

--- src/sage/categories/differential_algebras.py
+++ src/sage/categories/differential_algebras.py
@@ -71,6 +71,94 @@ class DifferentialAlgebras(CategoryWithAxiom_over_base_ring):
         from sage.algebras.differential_algebras.commutative_dga import CommutativeDGA
         return CommutativeDGA(degrees=(1,1,1,1,1), differential=(None, None, None, (((1,1,0,0,0), 1),), (((0,1,1,0,0), 1),)))
 
+    class ParentMethods:
+        def homology_algebra(self, n=None):
+            """
+            The homology of self as an algebra, up to but not including
+            total degree ``n``
+
+            INPUT:
+
+            - ``n`` - integer or ``None``. If ``None``, use one more than
+              the maximum of the degrees of the generators, relations,
+              and images of differentials
+
+            Return as a ``CommutativeDGA``.
+            """
+
+            # given a collection of homology generators in degrees less
+            # than d, look at degree d. find a set of expected linearly
+            # independent products of homology generators in that
+            # degree. lift and embed into the cycles, then project to
+            # homology. is the set of full rank, or are there
+            # dependencies? find a basis for the subvector space of
+            # homology spanned by these products, find dependencies among
+            # the remaining products to find relations, and then extend to
+            # a basis of the rest of homology to find new generators.
+
+            # Generators: list of generators.
+
+            # Relations: list of relations. Note that these are described
+            # in terms of the homology generators, not in terms of the
+            # generators for the original DGA.
+            from sage.misc.misc_c import prod
+
+            if n is None:
+                try:
+                    diffs = [self.differential(x) for x in self.gens()]
+                    n = 1 + max(self._degrees + self._relations
+                                + tuple([x.degree() for x in diffs if x != 0]))
+                except ValueError:
+                    # All lists are empty: no generators, etc.
+                    n = 1
+
+            p = self.base_ring().characteristic()
+            G = self.grading_group()
+            gens = []
+            relns = []
+            test = CommutativeDGA(self.base_ring(), degrees=())
+
+            # in characteristic p, pth powers of algebra generators are
+            # automatically homology generators.
+            #
+            # don't do this automatically? if an algebra generator is also
+            # a homology generator, we need to then remove its pth power
+            # from the list of generators, or add in a relation to
+            # compensate. it may be easier just to add the pth powers as
+            # they arise naturally.
+            #
+            # for now, leave this here to test things.
+            if p > 0:
+                gens = [x**p for x in self.gens() if x**p != 0]
+                test = CommutativeDGA(self.base_ring(),
+                                      degrees=[x.degree() for x in gens])
+
+            update = False
+            for d in group_range(1, n, group=G):
+                deg = G(d)
+                basis_exps = test.basis(deg)
+                basis = [prod(g**e for (g,e) in zip(gens, exp))
+                         for exp in basis_exps]
+                H_raw = self._raw_homology(deg)
+                H = self.homology(deg)
+
+                print d, basis, H
+
+                # under some circumstances, update either "gens" or
+                # "relns", and then update "test"
+                if False:
+                    gens.append(blah)
+                    update = True
+                if False:
+                    relns.append(blah)
+                    update = True
+                if update:
+                    test = CommutativeDGA(self.base_ring(),
+                                          degrees=[x.degree() for x in gens],
+                                          relations=relns)
+            pass
+
+
     WithBasis = LazyImport("sage.categories.differential_algebras_with_basis",
                            "DifferentialAlgebrasWithBasis", as_name="WithBasis")
 
