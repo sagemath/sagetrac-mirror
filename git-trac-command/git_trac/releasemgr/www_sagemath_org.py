a7d1787bfd40e9a4d0428285ff781f68112ae487
Volker Braun <vbraun.name@gmail.com>
2014-06-30 20:58:14 -0400

    Updated Sage version to 6.3.beta5

---
+++ git-trac-command/git_trac/releasemgr/www_sagemath_org.py
@@ -0,0 +1,37 @@
+"""
+Fab file for interaction with the sagemath.org server
+"""
+import os
+
+try:
+    import fabric
+    from fabric.api import env, run, sudo, put, settings, cd
+    from fabric.contrib.files import exists
+except ImportError:
+    # Fabric shoud be py3-compatible any time now, but not yet
+    # Evil hack to make importable in py3 tests
+    class AttrDict(dict):
+        def __init__(self, *args, **kwargs):
+            super(AttrDict, self).__init__(*args, **kwargs)
+            self.__dict__ = self
+    env = AttrDict()
+
+env.use_ssh_config = True
+env.user = 'root'
+env.hosts = ['www_sagemath_org']
+
+
+def upload_tarball(url):
+    """
+    Add tarball to http://sagemath.org/packages/upstream
+    """
+    if os.path.exists(url):        # is local file
+        put(url, os.path.join('/home/sagemath/upstream', os.path.basename(url)))
+    else:                          # should be a url
+        with cd('/home/sagemath/upstream'):
+            run('wget ' + url)
+    with cd('/www-data/sagemath-org/scripts'):
+        run('./mirror_upstream.py /home/sagemath/upstream')
+        run('./mirror-index.py')
+        run('./fix_permissions.sh')
+    run('/www-data/sagemath-org/go_live.sh')
