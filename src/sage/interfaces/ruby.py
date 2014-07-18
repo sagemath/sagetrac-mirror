dd0a85ede4b1b935ed07fc545dd834003a83ba09
Frédéric Chapoton <chapoton@math.univ-lyon1.fr>
2014-07-18 23:06:39 +0200

    trac #3538

--- src/sage/interfaces/ruby.py
+++ src/sage/interfaces/ruby.py
@@ -14,10 +14,10 @@ This is an expect interface to Ruby.
 #                  http://www.gnu.org/licenses/
 #*****************************************************************************
 
-import cPickle, os, time
-
+import cPickle
+import os
+import time
 from expect import Expect, ExpectElement, FunctionElement
- 
 from sage.structure.sage_object import dumps, loads, load
 
 
@@ -27,31 +27,35 @@ class Ruby(Expect):
 
     INPUT:
 
-        server -- (optional); if specified runs SAGE on a remote
+    - server -- (optional); if specified runs SAGE on a remote
                   machine with address.  You must have ssh keys setup
                   so you can login to the remote machine by typing
                   "ssh remote_machine" and no password, call
                   _install_hints_ssh() for hints on how to do that.
 
     EXAMPLES:
+
     We create an interface to a copy of \sage.  This copy of \sage runs
-    as an external process with its own memory space, etc.
-    
+    as an external process with its own memory space, etc. ::
+
         sage: s = Ruby()
 
-    Create the element 2 in our new copy of \sage, and cubeit.
+    Create the element 2 in our new copy of \sage, and cubeit. ::
+
         sage: a = s(2)
         sage: a^3
         8
 
-    Create a vector space of dimension $4$, and compute its generators:
+    Create a vector space of dimension $4$, and compute its generators::
+
         sage: V = s('QQ^4')
         sage: V.gens()
         ((1, 0, 0, 0), (0, 1, 0, 0), (0, 0, 1, 0), (0, 0, 0, 1))
 
     Note that V is a not a vector space, it's a wrapper around an object
     (which happens to be a vector space), in another running instance
-    of \sage.
+    of \sage. ::
+
         sage: type(V)
         <class 'sage.interfaces.ruby.RubyElement'>
         sage: V.parent()
@@ -60,13 +64,15 @@ class Ruby(Expect):
         (1, 0, 0, 0)
         sage: g.parent()
         Ruby
-        
+
     We can still get the actual parent by using the name attribute of g,
-    which is the variable name of the object in the child process.
+    which is the variable name of the object in the child process. ::
+
         sage: s('%s.parent()'%g.name())
         Vector space of dimension 4 over Rational Field
 
-    Note that the memory space is completely different. 
+    Note that the memory space is completely different. ::
+
         sage: x = 10
         sage: s('x = 5')
         5
@@ -76,13 +82,14 @@ class Ruby(Expect):
         5
 
     We can have the child interpreter itself make another child
-    \sage process, so now three copies of \sage are running:
+    \sage process, so now three copies of \sage are running::
+
         sage: s3 = s('Ruby()')
         sage: a = s3(10)
         sage: a
         10
 
-    This $a=10$ is in a subprocess of a subprocesses of your original \sage.        
+    This $a=10$ is in a subprocess of a subprocesses of your original \sage. ::
 
         sage: _ = s.eval('%s.eval("x=8")'%s3.name())
         sage: s3('"x"')
@@ -91,49 +98,50 @@ class Ruby(Expect):
         5
         sage: x
         10
-        
+
     The double quotes are needed because the call to s3 first evaluates
     its arguments using the s interpeter, so the call to s3 is passed
     \code{s('"x"')}, which is the string \code{"x"} in the s interpreter.
     """
-    def __init__(self, logfile   = None,
-                       python    = False,
-                       init_code = None,
-                       server    = None,
-                       server_tmpdir = None,
-                       remote_cleaner = True,
+    def __init__(self, logfile=None,
+                       python=False,
+                       init_code=None,
+                       server=None,
+                       server_tmpdir=None,
+                       remote_cleaner=True,
                        **kwds):
         command = "irb --noreadline"
         prompt = ">> "
-        
+
         Expect.__init__(self,
-                        name = 'ruby',
-                        prompt = prompt,
-                        command = command,
-                        restart_on_ctrlc = False,
-                        logfile = logfile,
-                        server = server,
-                        server_tmpdir = server_tmpdir,
-                        remote_cleaner = remote_cleaner,
+                        name='ruby',
+                        prompt=prompt,
+                        command=command,
+                        restart_on_ctrlc=False,
+                        logfile=logfile,
+                        server=server,
+                        server_tmpdir=server_tmpdir,
+                        remote_cleaner=remote_cleaner,
                         **kwds
                         )
 
     def cputime(self, t=None):
         """
         Return cputime since this Ruby subprocess was started.
-        
-        EXAMPLES:
-            sage: ruby.cputime()     # random output 
+
+        EXAMPLES::
+
+            sage: ruby.cputime()     # random output
             1.3530439999999999
             sage: ruby('factor(2^157-1)')
             852133201 * 60726444167 * 1654058017289 * 2134387368610417
-            sage: ruby.cputime()     # random output 
+            sage: ruby.cputime()     # random output
             1.6462939999999999
         """
-        s = self.eval('cputime(%s)'%t)
+        s = self.eval('cputime(%s)' % t)
         i = s.rfind('m')
         if i != -1:
-            s = s[i+1:-1]
+            s = s[i + 1:-1]
         return float(s)
 
     def trait_names(self):
@@ -145,18 +153,20 @@ class Ruby(Expect):
             pid = self._expect.pid
             if verbose:
                 if self.is_remote():
-                    print "Exiting spawned %s process (local pid=%s, running on %s)"%(self,pid,self._server)
+                    print "Exiting spawned %s process (local pid=%s, running on %s)" % (self, pid, self._server)
                 else:
-                    print "Exiting spawned %s process (pid=%s)."%(self, pid)
+                    print "Exiting spawned %s process (pid=%s)." % (self, pid)
             try:
-                for i in range(10):   # multiple times, since clears out junk injected with ._get, etc.
+                for i in range(10):   
+                    # multiple times, since clears out junk injected
+                    # with ._get, etc.
                     self._expect.sendline(chr(3))  # send ctrl-c
-                    self._expect.sendline('quit_sage(verbose=%s)'%verbose)
+                    self._expect.sendline('quit_sage(verbose=%s)' % verbose)
                     self._so_far(wait=0.2)
 
                 os.killpg(pid, 9)
                 os.kill(pid, 9)
-                
+
             except (RuntimeError, OSError), msg:
                 pass
 
@@ -165,14 +175,16 @@ class Ruby(Expect):
                 os.kill(pid, 9)
             except OSError:
                 pass
-            
+
             try:
                 self._expect.close(signal.SIGQUIT)
             except Exception:
                 pass
             self._expect = None
-        
+
     def _remote_tmpfile(self):
+        """
+        """
         try:
             return self.__remote_tmpfile
         except AttributeError:
@@ -180,18 +192,20 @@ class Ruby(Expect):
             return self.__remote_tmpfile
 
     def __call__(self, x):
+        """
+        """
         if isinstance(x, RubyElement) and x.parent() is self:
             return x
         if isinstance(x, str):
             return RubyElement(self, x)
 
         if self.is_local():
-            open(self._local_tmpfile(),'w').write(cPickle.dumps(x,2))
-            return RubyElement(self, 'cPickle.load(open("%s"))'%self._local_tmpfile())
+            open(self._local_tmpfile(), 'w').write(cPickle.dumps(x, 2))
+            return RubyElement(self, 'cPickle.load(open("%s"))' % self._local_tmpfile())
         else:
             open(self._local_tmpfile(),'w').write(dumps(x))   # my dumps is compressed by default
             self._send_tmpfile_to_server()
-            return RubyElement(self, 'loads(open("%s").read())'%self._remote_tmpfile())
+            return RubyElement(self, 'loads(open("%s").read())' % self._remote_tmpfile())
 
     def __reduce__(self):
         return reduce_load_Ruby, tuple([])
@@ -208,8 +222,9 @@ class Ruby(Expect):
         at the same time in a unified way.
 
         INPUT:
-            line -- input line of code
-            strip -- ignored
+
+        - line -- input line of code
+        - strip -- ignored
         """
         return Expect.eval(self, line)
 
@@ -217,29 +232,29 @@ class Ruby(Expect):
         """
         Set the variable var to the given value.
         """
-        cmd = '%s=%s'%(var,value)
+        cmd = '%s=%s' % (var, value)
         out = self.eval(cmd)
         if 'Traceback' in out:
-            raise TypeError, "Error executing code in SAGE\nCODE:\n\t%s\nSAGE ERROR:\n\t%s"%(cmd, out)
+            raise TypeError("Error executing code in SAGE\nCODE:\n\t%s\nSAGE ERROR:\n\t%s" % (cmd, out))
 
     def get(self, var):
         """
         Get the value of the variable var.
         """
-        s = self.eval('puts %s'%var)
+        s = self.eval('puts %s' % var)
         i = s.rfind('=>')
         if i != -1:
             return s[:i]
         return s
-        
+
     #def clear(self, var):
     #    """
     #    Clear the variable named var.
     #    """
     #    self.eval('del %s'%var)
-    
+
     def _contains(self, v1, v2):
-        return self.eval('%s in %s'%(v1,v2))
+        return self.eval('%s in %s' % (v1, v2))
 
     def _is_true_string(self, t):
         return t == "True"
@@ -248,6 +263,8 @@ class Ruby(Expect):
         ruby_console()
 
     def version(self):
+        """
+        """
         return eval(ruby_version())
 
     def _object_class(self):
@@ -255,6 +272,7 @@ class Ruby(Expect):
 
     def new(self, x):
         return RubyElement(self, x)
+
 
 class RubyElement(ExpectElement):
     def __getattr__(self, attrname):
@@ -265,7 +283,8 @@ class RubyElement(ExpectElement):
         """
         Return local copy of self.
 
-        EXAMPLE:
+        EXAMPLE::
+
             sage: sr = mq.SR(allow_zero_inversions=True)
             sage: F,s = sr.polynomial_system()
             sage: F == ruby(F)._sage_()
@@ -273,13 +292,14 @@ class RubyElement(ExpectElement):
         """
         P = self.parent()
         if P.is_remote():
-            P.eval('save(%s, "%s")'%(self.name(), P._remote_tmpfile()))
+            P.eval('save(%s, "%s")' % (self.name(), P._remote_tmpfile()))
             P._get_tmpfile_from_server(self)
             return load(P._local_tmp_file())
         else:
-            P.eval('save(%s, "%s")'%(self.name(), P._local_tmpfile()))
+            P.eval('save(%s, "%s")' % (self.name(), P._local_tmpfile()))
             return load(P._local_tmpfile())
-    
+
+
 class RubyFunction(FunctionElement):
     def __call__(self, *args, **kwds):
         P = self._obj.parent()
@@ -295,27 +315,27 @@ class RubyFunction(FunctionElement):
         else:
             callstr = '%s.%s()'%(self._obj._name, self._name)
         z = RubyElement(P, callstr)
-        
+
         return z
 
     def __repr__(self):
         return str(self.eval('%s.%s'%(self._obj._name, self._name)))
 
-                            
 
 ruby = Ruby()
+
 
 def reduce_load_ruby():
     return ruby
 
+
 def reduce_load_element(s):
-    return ruby('loads(%s)'%s)
+    return ruby('loads(%s)' % s)
 
 
-import os
 def ruby_console():
     os.system('sage')
 
+
 def ruby_version():
     return ruby('version()')
-
