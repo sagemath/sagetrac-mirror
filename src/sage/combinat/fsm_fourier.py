e0a73a727d97eae94affa88f963e160929db130b
Clemens Heuberger <clemens.heuberger@aau.at>
2014-08-13 12:15:50 +0000

    Fourier: T

--- src/sage/combinat/fsm_fourier.py
+++ src/sage/combinat/fsm_fourier.py
@@ -36,6 +36,8 @@ class FSMFourier(Transducer):
 
         - ``period`` -- least common multiple of the periods.
 
+        - ``T`` -- eigenvector matrix.
+
         EXAMPLES:
 
         -   Binary sum of digits::
@@ -51,7 +53,7 @@ class FSMFourier(Transducer):
             ....:     f(0) == 0],
             ....:     f, n, 2)
             sage: FSMFourier(T)._fourier_coefficient_data_()
-            FourierCoefficientData(c=1, periods=[1], period=1)
+            FourierCoefficientData(c=1, periods=[1], period=1, T=[1])
 
         -   Artificial example with two final components of periods `2`
             and `3`, respectively::
@@ -64,19 +66,109 @@ class FSMFourier(Transducer):
             ....:     initial_states=[0],
             ....:     final_states=[0, 1, 2, -3, -2, -1])
             sage: T._fourier_coefficient_data_()
-            FourierCoefficientData(c=2, periods=[3, 2], period=6)
+            (
+                          [        1/7           1           1         1/5           1           1]
+                          [          0           0           0         2/5          -2           0]
+                          [          0           0           0         2/5           2           0]
+                          [        2/7           2           2           0           0           0]
+                          [        2/7    -2*zeta6 2*zeta6 - 2           0           0           0]
+            2, [3, 2], 6, [        2/7 2*zeta6 - 2    -2*zeta6           0           0           0]
+            )
         """
-        from sage.rings.arith import lcm
         import collections
+        import operator
+
+        from sage.matrix.constructor import matrix
+        from sage.modules.free_module import VectorSpace
+        from sage.rings.arith import lcm
+        from sage.rings.integer_ring import ZZ
+        from sage.rings.number_field.number_field import CyclotomicField
+        from sage.structure.sage_object import SageObject
 
         FourierCoefficientData = collections.namedtuple(
             "FourierCoefficientData",
-            ["c", "periods", "period"])
-        components = self.final_components()
-        periods = [c.graph().period()
-                   for c in components]
-        period = lcm(periods)
+            ["c", "periods", "period", "T"])
+
+        positions = dict((state.label(), j)
+                         for j, state in enumerate(self.iter_states()))
+        q = len(self.input_alphabet)
+
+        class FCComponent(SageObject):
+            """Hold a final component and associated data."""
+            def __init__(self, fsm):
+                self.fsm = fsm
+                self.period = fsm.graph().period()
+                self.n_states = len(self.fsm.states())
+
+            def eigenvectors(self, M, components, common_period):
+                nrows = sum(c.n_states for c in components if c != self)
+                mask = matrix(
+                    nrows, M.ncols(),
+                    [standard_basis[positions[state.label()]]
+                     for other in components
+                     if other != self
+                     for state in other.fsm.iter_states()])
+
+                def eigenvector(j):
+                    eigenvalue = q * alpha**(j * common_period / self.period)
+                    S = matrix.block(
+                        [[M - eigenvalue*matrix.identity(M.nrows())],
+                         [mask]],
+                        subdivide=False)
+                    kernel = S.right_kernel_matrix()
+                    assert kernel.nrows() == 1
+                    if j == 0:
+                        #normalize for positive eigenvector
+                        return kernel.row(0) / sum(kernel.row(0))
+                    else:
+                        return kernel.row(0)
+
+                return [eigenvector(j) for j in range(self.period)]
+
+
+        components = [FCComponent(c) for c in self.final_components()]
+        period = lcm([c.period for c in components])
+        field = CyclotomicField(period)
+        alpha = field.gen()
+        M = self.adjacency_matrix(entry=lambda t: 1)
+        standard_basis = VectorSpace(field, M.nrows()).basis()
+
+        right_eigenvectors = reduce(
+            operator.add,
+            [c.eigenvectors(M, components, period)
+             for c in components],
+            [])
+
+        left_eigenvectors = reduce(
+            operator.add,
+            [c.eigenvectors(M.transpose(), components, period)
+             for c in components],
+            [])
+
+        annihilated_by_left = matrix(left_eigenvectors).\
+            right_kernel_matrix().transpose()
+
+        T = matrix.block([[matrix.column(right_eigenvectors),
+                           annihilated_by_left]],
+                         subdivide=False)
+
+        assert T.is_square()
+        assert T.nrows() == M.nrows()
+        assert T.is_invertible()
+
+        check = T.inverse() * M * T
+        eigenvalues = [q * alpha**(j * period/c.period)
+                       for c in components
+                       for j in range(c.period)]
+        check_dont_care = check.submatrix(len(eigenvalues),
+                                          len(eigenvalues))
+        assert (matrix.block(
+                [[matrix.diagonal(eigenvalues), ZZ(0)],
+                 [ZZ(0), check_dont_care]],
+                     subdivide=False) - check).is_zero()
+
         return FourierCoefficientData(
             c=len(components),
-            periods=periods,
-            period=period)
+            periods=[c.period for c in components],
+            period=period,
+            T=T)
