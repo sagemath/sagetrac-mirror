bb643eef28320a2498ef37c132d128a3901ba637
Arvind Ayyer <arvind@math.iisc.ernet.in>
2014-08-15 17:16:43 +0530

    Initial implementation of stochastic_matrix package

--- src/sage/probability/all.py
+++ src/sage/probability/all.py
@@ -2,3 +2,4 @@ from random_variable import (
      DiscreteRandomVariable,
      DiscreteProbabilitySpace )
 
+from sage.probability.stochastic_matrices import *
--- src/sage/probability/stochastic_matrices.py
+++ src/sage/probability/stochastic_matrices.py
@@ -1,1 +1,46 @@
-:)
+r"""
+Stochastic Matrices
+"""
+#*****************************************************************************
+#  Copyright (C) 2014 Anne Schilling <anne at math.ucdavis.edu>
+#
+#  Distributed under the terms of the GNU General Public License (GPL)
+#                  http://www.gnu.org/licenses/
+#******************************************************************************
+
+#from sage.structure.element import Element
+from sage.structure.element_wrapper import ElementWrapper
+#from sage.matrix.matrix_rational_dense import Matrix_rational_dense
+
+class StochasticMatrix(ElementWrapper):
+    r"""
+ 
+    EXAMPLES::
+
+     """
+
+    def __init__(self,  M, side='row'):
+        """
+        EXAMPLES::
+
+            sage: C = sage.categories.examples.crystals.HighestWeightCrystalOfTypeA(n=4)
+            sage: C == Crystals().example(n=4)
+            True
+        """
+        assert M.is_square()
+
+        ElementWrapper.__init__(self,  M.parent(), M)
+
+
+#    def _repr_(self):
+#        """
+#        EXAMPLES::
+#
+#            ERROR here
+#            
+#        """
+#        return "A stochastic matrix: %s"%(self,)
+
+
+
+
