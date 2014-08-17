718704d5d6f96624430212952a0f48358a5f51d9
Paul Mercat <mercatp@yahoo.fr>
2014-08-17 23:40:33 +0200

    Correct a bug in automata_tools.h

--- src/sage/combinat/words/automata_tools.c
+++ src/sage/combinat/words/automata_tools.c
@@ -46,6 +46,6 @@ void TikZ (const char *data, const char *graph_name, double sx, double sy)
 	fclose(f);
 	if (verb)
 		printf("draw...\n");
-	sprintf(tamp, "dot %s -Gname -Tsvg > output%s%s.svg", name, time(NULL), clock());
+	sprintf(tamp, "dot %s -Gname -Tsvg > output%d%d.svg", name, time(NULL), clock());
 	system(tamp);
 }
