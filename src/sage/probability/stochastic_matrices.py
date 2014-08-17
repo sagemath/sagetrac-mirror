1630f0ee98a46db0f56e87a10ecd8bafda10a2fb
Arvind Ayyer <arvind@math.iisc.ernet.in>
2014-08-17 10:00:33 +0530

    Stationary distribution program written

--- src/sage/probability/stochastic_matrices.py
+++ src/sage/probability/stochastic_matrices.py
@@ -21,7 +21,22 @@ class StochasticMatrix(ElementWrapper):
      """
 
     def __init__(self,  M, side='row',normalized = False):
-        """
+        r"""
+        Converts a matrix of nonnegative real numbers to the class of (row/ column) stochastic matrices.
+        A matrix is said to be row (resp. column) stochastic if the sum of the
+        entries of each row (resp. column) is equal to 1.
+
+        INPUT:
+
+        - ``side`` -- (default: 'row') if set to ``row`` checks whether row sums
+          satisfy the condition; if set to ``column`` checks whether column sums
+          satisfy the condition.
+
+        - ``normalized`` -- if set to ``True``, checks that
+          the sums are equal to 1. When set to ``False`` (default), checks that
+          the row (resp. column) sums are all equal to some constant
+          possibly different from 1.
+
         EXAMPLES::
 
             sage: M = Matrix([[1/2,1/2],[1,0]])
@@ -36,26 +51,53 @@ class StochasticMatrix(ElementWrapper):
         Traceback (most recent call last):
         ...
         ValueError: Input matrix is not stochastic
+        sage: StochasticMatrix(M,side='column',normalized = True)
+        Traceback (most recent call last):
+        ...
+        ValueError: Input matrix is not stochastic
         sage: StochasticMatrix(M,side='column')
         [1 1]
         [2 2]
-
         """
         if not M.is_stochastic(side = side,normalized = normalized):
             raise ValueError("Input matrix is not stochastic")
-
+        self.side = side
         ElementWrapper.__init__(self,  M.parent(), M)
 
+    def __getattr__(self,name):
+        try:
+            return getattr(self.value,name)
+        except AttributeError:
+            return super(StochasticMatrix, self).__getattr__(name)
+        
+    def stationary_distribution(self, irreducible = True):
+        r"""
+        Returns the stationary distribution of the stochastic matrix
+        
+        EXAMPLES::
+        
+            sage: M = Matrix([[1/2,1/2],[1,0]])
+            sage: StochasticMatrix(M)
+            [1/2 1/2]
+            [  1   0]
 
-#    def _repr_(self):
-#        """
-#        EXAMPLES::
-#
-#            ERROR here
-#            
-#        """
-#        return "A stochastic matrix: %s"%(self,)
-
-
-
-
+        TESTS::
+            
+        """
+            
+        if self.side == 'row':
+            sums = map(sum, self.rows())[0]
+            K = (self.value-sums).kernel()
+        else:
+            sums = map(sum, self.columns())[0]
+            K = (self.value-sums).transpose().kernel()
+            
+        if not irreducible:
+            return K
+        else:
+            if K.dimension() > 1:
+                raise ValueError("The stochastic matrix is not irreducible")
+            else:
+                V = K.basis()[0]
+                return V/sum(V)
+                    
