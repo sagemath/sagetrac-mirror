824071cac93f90dbdc7b940a69d116d443db3bd1
Daniel Krenn <devel@danielkrenn.at>
2014-08-24 22:14:55 +0200

    Parent CombinatorialExpressionRing added

--- src/sage/combinat/combinatorial_expression.py
+++ src/sage/combinat/combinatorial_expression.py
@@ -1511,3 +1511,256 @@ class LabeledCartesianProduct(
 # multi set
 # sequence
 
+
+#*****************************************************************************
+# Parent: CombinatorialExpressionRing
+#*****************************************************************************
+
+
+class CombinatorialExpressionRing(
+    sage.structure.unique_representation.UniqueRepresentation,
+    sage.rings.ring.Ring):
+    """
+    A ring of combinatorial expressions.
+
+    INPUT:
+
+    - ``base`` -- the base ring.
+
+    OUTPUT:
+
+    A ring.
+
+    EXAMPLES::
+
+        sage: from sage.combinat.combinatorial_expression import (
+        ....:     CombinatorialExpressionRing)
+        sage: R = CombinatorialExpressionRing(SR); R
+        Combinatorial Expression Ring (over Symbolic Ring)
+        sage: R(var('z'))
+        z
+    """
+    def __init__(self, base):
+        """
+        See :class:`CombinatorialExpressionRing` for details.
+
+        TESTS::
+
+            sage: from sage.combinat.combinatorial_expression import (
+            ....:     CombinatorialExpressionRing)
+            sage: R = CombinatorialExpressionRing(SR); R
+            Combinatorial Expression Ring (over Symbolic Ring)
+            sage: type(R)
+            <class 'sage.combinat.combinatorial_expression.CombinatorialExpressionRing_with_category'>
+            sage: R.category()
+            Category of rings
+            sage: R.element_class
+            <class 'sage.combinat.combinatorial_expression.CombinatorialExpressionRing_with_category.element_class'>
+        """
+        if base != sage.symbolic.ring.SR:
+            raise NotImplementedError("%s not allowed as base ring." % (base,))
+        super(CombinatorialExpressionRing, self).__init__(base=base)
+
+
+    def _repr_(self):
+        """
+        Returns a representation string.
+
+        INPUT:
+
+        Nothing.
+
+        OUTPUT:
+
+        A string.
+
+        EXAMPLES::
+
+            sage: from sage.combinat.combinatorial_expression import (
+            ....:     CombinatorialExpressionRing)
+            sage: repr(CombinatorialExpressionRing(SR))  # indirect doctest
+            'Combinatorial Expression Ring (over Symbolic Ring)'
+        """
+        return "Combinatorial Expression Ring (over %s)" % (self.base_ring(),)
+
+
+    def base_ring(self):
+        """
+        Returns the base ring.
+
+        INPUT:
+
+        Nothing.
+
+        OUTPUT:
+
+        A ring.
+
+        EXAMPLES::
+
+            sage: from sage.combinat.combinatorial_expression import (
+            ....:     CombinatorialExpressionRing)
+            sage: CombinatorialExpressionRing(SR).base_ring()
+            Symbolic Ring
+        """
+        return self.base().base_ring()
+
+
+    Element = GenericBase
+
+    #------------------------------------------------------------------------
+
+    @lazy_attribute
+    def element_class_generic_empty(self):
+        return self.__make_element_class__(GenericEmpty)
+
+    @lazy_attribute
+    def element_class_unlabeled_empty(self):
+        return self.__make_element_class__(UnlabeledEmpty)
+
+    @lazy_attribute
+    def element_class_labeled_empty(self):
+        return self.__make_element_class__(LabeledEmpty)
+
+    #------------------------------------------------------------------------
+
+    @lazy_attribute
+    def element_class_generic_atom(self):
+        return self.__make_element_class__(GenericAtom)
+
+    @lazy_attribute
+    def element_class_unlabeled_atom(self):
+        return self.__make_element_class__(UnlabeledAtom)
+
+    @lazy_attribute
+    def element_class_labeled_atom(self):
+        return self.__make_element_class__(LabeledAtom)
+
+    #------------------------------------------------------------------------
+
+    @lazy_attribute
+    def element_class_generic_singleton(self):
+        return self.__make_element_class__(GenericSingleton)
+
+    @lazy_attribute
+    def element_class_unlabeled_singleton(self):
+        return self.__make_element_class__(UnlabeledSingleton)
+
+    @lazy_attribute
+    def element_class_labeled_singleton(self):
+        return self.__make_element_class__(LabeledSingleton)
+
+    #------------------------------------------------------------------------
+
+    def _get_element_class_(self, what, flavor):
+        return getattr(self, 'element_class_' + flavor + '_' + what)
+
+    #------------------------------------------------------------------------
+
+    def _from_base_ring_(self, data, flavor, size):
+        """
+        Converts an expression from the base ring to a combinatorial
+        expression.
+
+        INPUT:
+
+        - ``data`` -- an element of the base ring.
+
+        - ``flavor`` -- a string representing a flavor.
+
+        - ``size`` -- an integer or ``None``
+
+        OUTPUT:
+
+        An combinatorial expression element.
+
+        TESTS::
+
+            sage: from sage.combinat.combinatorial_expression import (
+            ....:     CombinatorialExpressionRing)
+            sage: R = CombinatorialExpressionRing(SR)
+            sage: z = R._from_base_ring_(var('z'), 'labeled', size=1); z
+            z
+            sage: type(z)
+            <class 'sage.combinat.combinatorial_expression.LabeledAtom_with_category'>
+            sage: z.size()
+            1
+            sage: e = R(SR(1)); e  # indirect doctest
+            1
+            sage: type(e)
+            <class 'sage.combinat.combinatorial_expression.GenericEmpty_with_category'>
+            sage: e.size()
+            0
+            sage: y = R(var('y'), size=2); y  # indirect doctest
+            y
+            sage: type(y)
+            <class 'sage.combinat.combinatorial_expression.GenericSingleton_with_category'>
+            sage: y.size()
+            2
+        """
+
+        # at the moment only self.base_ring() == SR is allowed (see __init__)
+
+        if size is None:
+            if data.is_numeric():
+                size = 0
+            else:
+                size = 1
+
+        if data.operator() is None:
+            if size == Integer(0):
+                return self._get_element_class_('empty', flavor)(
+                    self, data)
+            elif size == Integer(1):
+                return self._get_element_class_('atom', flavor)(
+                    self, data)
+            elif size > Integer(1):
+                return self._get_element_class_('singleton', flavor)(
+                    self, data, size=size)
+            else:
+                raise ValueError('Wrong size %s' % (size,))
+        else:
+            raise NotImplementedError('At the moment only expressions without '
+                                      'operators can be converted to '
+                                      'combinatorial expressions directly.')
+
+
+    def _element_constructor_(self, *args, **kwargs):
+
+        unlabeled = kwargs.pop('unlabeled', None)
+        labeled = kwargs.pop('labeled', None)
+
+        if unlabeled:
+            flavor = 'unlabeled'
+        elif labeled:
+            flavor = 'labeled'
+        else:
+            flavor = 'generic'
+
+        size = kwargs.pop('size', None)
+
+        if not args:
+            raise NotImplementedError('TODO')
+        if len(args) > 1:
+            raise ValueError('Too many input arguments given.')
+        data = args[0]
+
+        try:
+            P = data.parent()
+        except AttributeError:
+            data = self.base_ring()(data)
+
+        if data in self.base_ring():
+            return self._from_base_ring_(data, flavor, size)
+
+        if isinstance(data, GenericBase):
+            raise NotImplementedError('TODO')
+
+        raise NotImplementedError('Cannot convert or decode given '
+                                  'data %s' % (data,))
+
+
+
+    def _an_element_(self):
+        return GenericAtom(self, self.base_ring().an_element())
+
