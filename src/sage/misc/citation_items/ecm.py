8b5814a50d44ef86817ea642f140c6c3937cdf90
Martin Raum <martin@raum-brothers.eu>
2014-08-25 15:21:02 +0200

    Update ECM bibtex code.

--- src/sage/misc/citation_items/ecm.py
+++ src/sage/misc/citation_items/ecm.py
@@ -10,11 +10,20 @@ from sage.misc.citation_items.citation_item import CitationItem
 class ECM_CitationItem( CitationItem ):
     _name = "ECM"
 
+    ## based on reference given by Paul Zimmermann via email
     _bibtex = r"""
-@Misc{software-ecm,
-    Author = {Charron, T. and Daminelli, N. and Granlund, Torbjorn and Leyland, P., and Zimmermann, Paul}, 
-    Title = {{P. The ECMNET Project}},
-    Howpublished = {{\url{http://www.loria.fr/∼zimmerma/ecmnet/}}},
+@InProceedings{software-ecm,
+  author = 	 {Zimmermann, Paul and Dodson, Bruce},
+  title = 	 {20 years of {ECM}},
+  booktitle =	 {Proceedings of the 7th Algorithmic Number Theory Symposium
+                  (ANTS VII)},
+  pages =	 {525--542},
+  year =	 2006,
+  editor =	 {Hess, F. and Pauli, S. and Pohst, M.},
+  volume =	 4076,
+  series =	 {{LNCS}},
+  address =	 {Berlin Heidelberg},
+  publisher =	 {{Springer}}
 }
     """
 
