9ff8d05ef5fe8e7d36f7ca03772a0bcec911aed5
bransingh <bransingh@gmail.com>
2014-10-05 11:09:01 +0530

    new file:   src/sage/algebras/lie_superalgebras.py

---
+++ src/sage/algebras/lie_superalgebras.py
@@ -0,0 +1,24 @@
+from math import*
+from cartan_type import CartanType
+class LieSuperalgebras()
+def even_odd(self)
+if self._cartan_type != other._cartan_type:
+         return degree==0
+
+	 else:
+		degree==1
+
+def degree(self,x):
+
+    if self.x is even:
+
+               0
+    else:
+
+               1
+
+
+def super_bracket(x, y):
+    
+
+    return x*y -(-1)^{degree(x)*degree(y)} y*x
