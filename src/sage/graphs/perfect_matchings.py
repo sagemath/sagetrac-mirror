3d62f2c58fc66c4e91826b2b5e27409dcb58f651
Arvind Ayyer <arvind@math.iisc.ernet.in>
2014-10-16 15:34:35 +0530

    imported perfect_matchings.py in all.py

--- src/sage/graphs/perfect_matchings.py
+++ src/sage/graphs/perfect_matchings.py
@@ -22,11 +22,30 @@ Methods
 #                         http://www.gnu.org/licenses/
 #*****************************************************************************
 
+from sage.misc.decorators import options
+from sage.misc.cachefunc import cached_method
+
+
 def perfect_matchings(G):
     r"""
-    Computes all perfect matchings of the graph G
+    Computes all perfect matchings or dimer coverings or 1-factors of the graph G
 
+    INPUT:
 
+    * ``G`` - a graph with an even number of vertices
+
+    EXAMPLES::
+
+    sage: G = graphs.GridGraph([3,2])
+    sage: perfect_matchings(G)
+    [[((1, 1), (2, 1)), ((1, 0), (2, 0)), ((0, 0), (0, 1))],
+    [((2, 0), (2, 1)), ((1, 0), (1, 1)), ((0, 0), (0, 1))],
+    [((2, 0), (2, 1)), ((0, 1), (1, 1)), ((0, 0), (1, 0))]]
+    sage: G = graphs.GridGraph([3,3])
+    sage: perfect_matchings(G)
+    Traceback (most recent call last)
+    ...
+    ValueError: there is no perfect matching for a graph with an odd number of vertices
 
     """
     n = G.num_verts()
@@ -41,7 +60,7 @@ def perfect_matchings(G):
     N = G.neighbors(g)
     PP = []
     for h in N:
-        H = copy(G)
+        H = G.copy()
         H.delete_vertices([g,h])
         P = perfect_matchings(H)
         for p in P:
