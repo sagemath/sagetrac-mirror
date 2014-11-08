65069da4d3ef945572af04f1b7c1df2ca362f1c9
Andrew Mathas <andrew.mathas@gmail.com>
2014-11-08 17:44:19 +1100

    Adding of seminormal representations

--- src/sage/algebras/iwahori_hecke_algebras/iwahori_hecke_algebra_representations.py
+++ src/sage/algebras/iwahori_hecke_algebras/iwahori_hecke_algebra_representations.py
@@ -1,3 +1,31 @@
+r"""
+Implements representations of a Iwahori-Hecke algebra as
+:class:`CombinatorialFreeModules`, including cell representations and in types A, B and,
+more generally, `G(r,1,n)`, the Murphy and seminormal forms of the Specht
+modules.
+
+<Paragraph description>
+
+AUTHORS:
+
+- Andrew Mathas (2014-12-03): initial version
+
+EXAMPLES::
+
+<Lots and lots of examples>
+"""
+
+#*****************************************************************************
+#    Copyright (C) 2014 Andrew Mathas <andrew.mathas@sydney.edu.au>
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation, either version 2 of the License, or
+#    (at your option) any later version. See http://www.gnu.org/licenses/
+#    for more details.
+#*****************************************************************************
+
+
 from sage.algebras.algebra import is_Algebra
 from sage.categories.action import Action
 from sage.combinat.free_module import CombinatorialFreeModule
@@ -59,6 +87,8 @@ class IwahoriHeckeAlgebraRepresentation(CombinatorialFreeModule):
         sage: SeminormalRepresentation([3,2,2], prefix='v').an_element()
         3*v(1,2,7/3,5/4,6) + 2*v(1,3,7/2,5/4,6) + 2*v(1,4,7/2,5/3,6)
     """
+    def __init__(self, R, basis_keys, prefix, q1,q2, left_module=True):
+        pass
 
     def hecke_algebra_action(self, H):
         r"""
@@ -87,34 +117,6 @@ class IwahoriHeckeAlgebraRepresentation(CombinatorialFreeModule):
         trace=0
         for t in self._basis_keys:
             trace+=self(t).T(*arg).coefficient(t)
-        return trace
-
-    def tau_character(self, *arg):
-        r"""
-        Return the trace of `tau*T_w` acting on the representation. Here `tau`
-        is the automorphsim of the Specht module that sends the basis element
-        `f_t` to `f_{t'}`, where `t'` is the tableau conjugate to `t` and `w` is
-        the permutation corresponding to the  (reduced) expression `*arg`.
-
-        In fact, if `w` is a minimal length coset representative then this
-        character value is zero unless `w` belongs to the 
-        conjugacy class of the symmetric group that splits when restricted to
-        the alternating group.
-
-        EXAMPLES
-
-            sage: SeminormalRepresentation([2,2]).tau_character(1)
-            0
-            sage: SeminormalRepresentation([2,2]).tau_character(1,2)
-            (((-1 - 2*q^2 - q^4)*r3)*I)/(-q - 2*q^3 - q^5)
-            sage: SeminormalRepresentation([2,2]).tau_character(2,1)
-            (((1 + 2*q^2 + q^4)*r3)*I)/(-q - 2*q^3 - q^5)
-        """
-        if self._shape<> self._shape.conjugate():
-            return 0
-        trace=0
-        for t in self._basis_keys:
-            trace+=self(t.conjugate()).T(*arg).coefficient(t)
         return trace
 
     @cached_method
@@ -235,9 +237,24 @@ class IwahoriHeckeAlgebraAction(Action):
         """
         return v.action(a)
 
+
+###############################################################################
+##       Kazhdan-Lusztig cell representations of Iwahori-Hecke algebras      ##
+###############################################################################
+
+class LeftCellRepresentationOfHeckeAlgebra(IwahoriHeckeAlgebraRepresentation)
+    pass
+
+class RightCellRepresentationOfHeckeAlgebra(IwahoriHeckeAlgebraRepresentation)
+    pass
+
+###############################################################################
+##     Seminormal representations of (cyclotomic) Iwahori-Hecke algebras     ##
+###############################################################################
+
 class SeminormalRepresentation_generic(IwahoriHeckeAlgebraRepresentation):
 
-    @staticmethod
+    @staticmetho
     def __classcall__(cls, shape, prefix='f'):
         """
         Magic to allow class to accept a list (which is not hashable) instead
@@ -362,7 +379,7 @@ class SeminormalRepresentation_generic(IwahoriHeckeAlgebraRepresentation):
 
 class SeminormalRepresentation(SeminormalRepresentation_generic):
 
-    def __init__(self, shape, prefix):
+    def __init__(self, shape, prefix='v'):
         r"""
         Initialisation of a seminormal representation of the symmetric group
         indexed by the partition `shape` and where the basis elements are
@@ -450,7 +467,7 @@ class SeminormalRepresentation(SeminormalRepresentation_generic):
 
 class SeminormalRepresentation_orthogonal(SeminormalRepresentation_generic):
 
-    def __init__(self, shape, prefix):
+    def __init__(self, shape, prefix='y'):
         r"""
         Initialisation of a seminormal representation of the symmetric group
         indexed by the partition `shape` and where the basis elements are
@@ -594,7 +611,7 @@ class SeminormalRepresentation_murphy(SeminormalRepresentation_generic):
 
 class SeminormalRepresentation_alternating(SeminormalRepresentation_generic):
 
-    def __init__(self, shape, prefix):
+    def __init__(self, shape, prefix='a'):
         r"""
         Initialisation of a seminormal representation of the symmetric group
         indexed by the partition `shape` and where the basis elements are
@@ -695,5 +712,131 @@ class SeminormalRepresentation_alternating(SeminormalRepresentation_generic):
                 coeff*=-self.I*self._quantum_integer_root(-d+1)*self._quantum_integer_root(-d-1)
             return self.sum_of_terms([(tab,denom), (rtab,coeff)],distinct=True)
 
-class MurphySpechtModule(IwahoriHeckeAlgebraRepresentation):
-    pass
+    def tau_character(self, *arg):
+        r"""
+        Return the trace of `tau*T_w` acting on the representation. Here `tau`
+        is the automorphsim of the Specht module that sends the basis element
+        `f_t` to `f_{t'}`, where `t'` is the tableau conjugate to `t` and `w` is
+        the permutation corresponding to the  (reduced) expression `*arg`.
+
+        In fact, if `w` is a minimal length coset representative then this
+        character value is zero unless `w` belongs to the 
+        conjugacy class of the symmetric group that splits when restricted to
+        the alternating group.
+
+        EXAMPLES
+
+            sage: SeminormalRepresentation([2,2]).tau_character(1)
+            0
+            sage: SeminormalRepresentation([2,2]).tau_character(1,2)
+            (((-1 - 2*q^2 - q^4)*r3)*I)/(-q - 2*q^3 - q^5)
+            sage: SeminormalRepresentation([2,2]).tau_character(2,1)
+            (((1 + 2*q^2 + q^4)*r3)*I)/(-q - 2*q^3 - q^5)
+        """
+        if self._shape<> self._shape.conjugate():
+            return 0
+        trace=0
+        for t in self._basis_keys:
+            trace+=self(t.conjugate()).T(*arg).coefficient(t)
+        return trace
+
+class SpechtModuleWithMurphyBasis(SeminormalRepresentation_generic):
+
+    def __init__(self, shape, prefix='m'):
+        r"""
+        Initialisation of a seminormal representation of the symmetric group
+        indexed by the partition `shape` and where the basis elements are
+        labelled by `prefix`.
+
+        EXAMPLES::
+
+            sage: SeminormalRepresentation([3,2,1])    # indirect doctest
+            SeminormalRepresentation([3,2,1])
+        """
+        # We need to build the base ring that splits our module. It needs to
+        # contain the inverses and square roots of the quantum integers [k]_q,
+        # for 0\le k\le n. To achieve this we start with the ring of Laurent
+        # polynomials and successively extend this ring by throwing in the
+        # square roots that we need. At the end we throw in a square of -1. We
+        # don't use the ComplexField() because this seems to drag along with it
+        # unwanted precision when printing.
+        self._shape=shape
+        self._prefix=prefix
+        self._generic_seminormalform=SeminormalRepresentation_murphy(shape)
+        self._generic_Murphy=lambda t: self._generic_seminormalform().gen().T( *t.reduced_word())
+        Fq=FractionField(PolynomialRing(IntegerRing(),'q')) 
+        self.q=Fq.gen()
+        roots=['r%d'%(d+1) for d in range(0,shape.size())] # names for the square roots
+        roots[0]='I'
+        poly_ring=PolynomialRing(Fq,roots)
+        relations=[poly_ring.gen(0)**2+1]
+        self.__quantum_integer={0:0, 1:1}       # dictionary of positive quantum integers
+        self.__quantum_integer_root={0:0, 1:1}  # dictionary of square roots of positive quantum integers
+        qint=1                                  # a quantum integer in waiting
+        for k in range(1,shape.size()):
+            qint+=self.q**(2*k)                 # now equal to 1+q^2+...+q^2k = [k+1]_{q^2}
+            self.__quantum_integer[k+1]=qint    # we remember these because we need them later
+            relations.append(poly_ring.gen(k)**2-qint)
+        F=poly_ring.quotient(poly_ring.ideal(relations),roots)
+        for k in range(1,shape.size()):
+            self.__quantum_integer_root[k+1]=F.gen(k)
+        self.I=F.gen(0)                         # I=sqrt(-1). We don't use CC as we want issues with precision
+        CombinatorialFreeModule.__init__(self,F,shape.standard_tableaux(), prefix='m')
+
+    @cached_method
+    def _T_on_basis(self,tab,r):
+        r"""
+        Compute the action of `T_r` on the basis element `f_tab`, where the action
+        of `T_r` is as follows:
+
+        .. MATH::
+
+           f_t T_r = \frac{-q^{-1}}{[d]}f_t + \alpha_d f_s
+
+        where $d=c_r(t)-c_{r+1}(t)$  is the axial distance from $r$ to $r+1$ in `t` and
+
+        .. MATH::
+
+           \alpha_d = \begin{cases}
+                         \frac{\sqrt{-1}\sqrt{[d+1]}\sqrt{[d-1]}}{[d]},&\text{if }d>0,\\
+                        -\frac{\sqrt{-1}\sqrt{[-d+1]}\sqrt{[-d-1]}}{[d]},&\text{if }d<0.
+           \end{cases}
+
+         The whole point of choosing this particular basis is that if the module
+         is indexed by a self-conjugate partition then it splits when it is
+         restricted to the alternating Hecke algebra.
+
+         EXAMPLES::
+
+             sage: rep=SeminormalRepresentation([3,2,1])
+             sage: rep([[1,2,3],[4,5],[6]]).T(1)   # indirect doctest
+             q*f(1,2,3/4,5/6)
+             sage: rep([[1,2,3],[4,5],[6]]).T(1,2)
+             q^2*f(1,2,3/4,5/6)
+             sage: rep([[1,2,3],[4,5],[6]]).T(1,2,3)
+             (q^2/(-q-q^3-q^5))*f(1,2,3/4,5/6) + (q^2*r2*r4*I/(1+q^2+q^4))*f(1,2,4/3,5/6)
+        """
+        rtab=tab.symmetric_group_action_on_entries( Permutation((r,r+1)) )
+        d=tab.content(r)-tab.content(r+1)
+        if rtab.is_standard():
+            if d>0: 
+                # tab dominates rtab => m_tab T_r = m_{rtab}
+                return self.monomial(rtab)
+            else:
+                # rtab dominates tab => m_tab T_r = (q_1+q_2)m_{rtab} + q_1q_2 m_tab
+                return self.sum_of_terms([(tab,self.parent()._q_prod), (rtab,rtab,self.parent()._qsum)],distinct=True)
+        else:
+           # r and r+1 are in the same column so we compute via the generic
+           # seminormal form using the fact the transition matrix between the
+           # Murphy basis the SeminormalRepresentation_murphy basis is unitriangular
+           specht=self.parent()
+           mtabr=specht.zero()
+           generic_prod=specht._generic_Murphy(t)
+           while generic_prod != self._generic_seminormalform().zero():
+                (s,c) = generic_prod.trailing_item(index_cmp)
+                generic_prod -= c * specht._generic_Murphy(s)
+                mtabr += specht._specialise_coeff(c) * self.parent().monomial(s)
+
+           return mtabr
+
+
