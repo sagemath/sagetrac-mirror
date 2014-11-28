eb483ef804fcdf46d23d73c11f2a9f3d8d0291e0
Nicolas M. Thiéry <nthiery@users.sf.net>
2014-11-28 21:54:02 +0100

    Update of Agregation courses

--- src/doc/en/thematic_tutorials/media/gauss.py
+++ src/doc/en/thematic_tutorials/media/gauss.py
@@ -1,37 +1,31 @@
 import time
 
-def gauss(m, maxi=None):
+def gauss(m):
     m = copy(m)
     n = m.nrows()
-    if maxi is None:
-        maxi=n
-    for i in range(maxi):
+    for i in range(n):
         # Recherche du pivot
         for j in range(i,n):
             if m[j,i]:
                 m.swap_rows(i,j)
-                m.rescale_row(i, ~m[i,i])
                 break
         if not m[i,i]:
             raise ValueError("non invertible matrix")
 
         for j in range(i+1, n):
-            m.add_multiple_of_row(j, i, -m[j,i])
+            m.add_multiple_of_row(j, i, -m[j,i]/m[i,i])
     return m
 
-def matrice_inversible(n, corps=QQ):
+def data(n, corps=QQ):
+    # Renvoie une matrice inversible
     while True:
         m = random_matrix(corps, n, n)
         if m.det():
             return m
 
-def hilbert(n):
-    return matrix(QQ, n, lambda i,j: 1/QQ(i+j+1))
-
-def temps(f, n, input=input):
-    m = input(n)
+def temps(f, n, data=data, corps=QQ):
+    print n
+    m = data(n, corps)
     debut = time.time()
     f(m)
-    t = time.time() - debut
-    print n, t
-    return t
+    return time.time() - debut
