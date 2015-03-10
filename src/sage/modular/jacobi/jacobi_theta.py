c584f9b20c095e6b03050b9d2dbcab7c7404eef7
Frédéric Chapoton <chapoton@math.univ-lyon1.fr>
2015-03-10 22:08:51 +0100

    trac #14244 details

--- src/sage/modular/jacobi/jacobi_theta.py
+++ src/sage/modular/jacobi/jacobi_theta.py
@@ -1,19 +1,13 @@
-
-
-
-
-
-def _jacobi_theta(self, Vec, Max=10):
+def _jacobi_theta(self, vec, max=10):
     """
     Compute the theta series as a power series in the variable given
-    in var_str (which defaults to '`q`'), up to the specified precision
+    in ``var_str`` (which defaults to `q`), up to the specified precision
     `O(q^max)`.
 
+    EXAMPLES:
     """
     from sage.modular.jacobi.jacobi_form import Jacobi_Form
-    JF = Jacobi_Form(self,Vec,Max)
-    return JF.Fourier_expansion()
+    return Jacobi_Form(self, vec, max).Fourier_expansion()
 
 
-def __jacobi_theta(self,Vec, Max):
-    return _jacobi_theta(self,Vec,Max)
+__jacobi_theta = _jacobi_theta
--- src/sage/quadratic_forms/quadratic_form__jacobi_theta.py
+++ src/sage/quadratic_forms/quadratic_form__jacobi_theta.py
@@ -1,24 +1,28 @@
 """
 Jacobi Theta Series of Quadratic Forms
 
+AUTHOR:
+
 Andrew Fiori
-
 """
-
 from sage.modular.jacobi.jacobi_theta import __jacobi_theta
-from sage.misc.misc import cputime, verbose
 
-def bilinear_form(self,x,y):
-    return self(x+y)-self(x)-self(y)
 
-def jacobi_theta(self, Vec, Max=10):
+def bilinear_form(self, x, y):
+    """
+    Return the associated bilinear form of a quadratic form.
+
+    EXAMPLES:
+    """
+    return self(x + y) - self(x) - self(y)
+
+
+def jacobi_theta(self, vec, max=10):
     """
     Compute the theta series as a power series in the variable given
-    in var_str (which defaults to '`q`'), up to the specified precision
+    in ``var_str`` (which defaults to `q`), up to the specified precision
     `O(q^max)`.
 
+    EXAMPLES:
     """
-    return __jacobi_theta(self,Vec,Max)
-
-
-
+    return __jacobi_theta(self, vec, max)
