97428512e64745ce4726dba61433a29ccf5479cd
Nathann Cohen <nathann.cohen@gmail.com>
2015-03-31 14:51:32 +0200

    My new changessssss

--- src/sage/graphs/all.py
+++ src/sage/graphs/all.py
@@ -1,5 +1,5 @@
 from sage.misc.lazy_import import lazy_import
-
+print " HEyyyyyyyyyyyyyyyyyyyyyyy"
 lazy_import("sage.graphs.graph_generators", "graphs")
 lazy_import("sage.graphs.digraph_generators", "digraphs")
 lazy_import("sage.graphs.hypergraph_generators", "hypergraphs")
