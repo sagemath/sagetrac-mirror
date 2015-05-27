b1e06b98811bad393e3c4288a95a04a4d40d980b
Frédéric Chapoton <chapoton@math.univ-lyon1.fr>
2015-05-27 20:00:16 +0200

    now with bad INPUT format

--- src/sage/combinat/horrible.py
+++ src/sage/combinat/horrible.py
@@ -3,7 +3,7 @@ just an horrible python file
 
 will all possible problems
 
-starting with unicode hé hö ù 
+starting with unicode hé hö ù 羊
 without unicode declaration
 
 trailing whitespaces  
@@ -18,3 +18,13 @@ def plant():
     And even old style raise syntax
     """
     raise ValueError, 'not good at all'
+
+def pear(n):
+    """
+    And now with a bad format for the input block
+
+    INPUT::
+
+    - n -- anything
+    """
+    pass
