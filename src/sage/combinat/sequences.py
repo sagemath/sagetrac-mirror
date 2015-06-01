06dfe6bbe6147f3a8c6ef89859d60cf7ef66f5d0
Daniel Krenn <devel@danielkrenn.at>
2015-06-01 10:46:39 +0200

    add sequences.py

---
+++ src/sage/combinat/sequences.py
@@ -0,0 +1,28 @@
+r"""
+Common Sequences
+
+
+AUTHORS:
+
+- Daniel Krenn (2015-05-22): initial version
+
+ACKNOWLEDGEMENT:
+
+- Daniel Krenn is supported by the Austrian Science Fund (FWF): P 24644-N26.
+
+Functions and methods
+---------------------
+
+"""
+#*****************************************************************************
+#       Copyright (C) 2015 Daniel Krenn <dev@danielkrenn.at>
+#
+#  Distributed under the terms of the GNU General Public License (GPL)
+#  as published by the Free Software Foundation; either version 2 of
+#  the License, or (at your option) any later version.
+#                http://www.gnu.org/licenses/
+#*****************************************************************************
+
+import itertools
+import sage
+
