d19d0d0e35afbdf899498a34e8aeaf4c39486358
Frédéric Chapoton <chapoton@math.univ-lyon1.fr>
2015-06-12 18:39:33 +0200

    trac #14974 triple colon

--- src/sage/combinat/horrible.py
+++ src/sage/combinat/horrible.py
@@ -1,12 +1,12 @@
 """
 just an horrible python file
 
-will all possible problems
+will all possible problems for detection by the patchbot
 
 starting with unicode hé hö ù 羊
 without unicode declaration
 
-trailing whitespaces  
+trailing whitespaces in this line  
 
 old style doctest continuation::
 
@@ -16,6 +16,10 @@ old style doctest continuation::
 def plant():
     """
     And even old style raise syntax
+
+    and some triple colon
+
+    TRIPLE:::
     """
     raise ValueError, 'not good at all'
 
