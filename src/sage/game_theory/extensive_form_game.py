c0eb2d41177193bd1ac69a778766adc9d8097a2e
Hannah Lorrimore <324b3d5a1a764755953b101a3c4ed767@compute.c.sage-math-inc.internal>
2015-06-16 14:21:09 +0000

    Creating an empty extensive form game file

---
+++ src/sage/game_theory/extensive_form_game.py
@@ -0,0 +1,11 @@
+"""
+This file will contain a class for extensive form games.
+"""
+def hannahfunction(x, y):
+    """
+    this will do something::
+    
+        sage: hannahfunction(4, 7)
+        13
+    """
+    return x + y + 2
