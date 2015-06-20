9bd5a6c022714821e9a4dd65ef27ed41bbcdf8c8
Hannah Lorrimore <324b3d5a1a764755953b101a3c4ed767@compute.c.sage-math-inc.internal>
2015-06-20 09:46:57 +0000

    Temporarily removed tests from EFG class with the intention on putting them back as they're required. More tests added to Node Class

--- src/sage/game_theory/extensive_form_game.py
+++ src/sage/game_theory/extensive_form_game.py
@@ -4,125 +4,32 @@ This file will contain a class for extensive form games.
 
 class ExtensiveFormGame():
     """
-
-    Class to be able to build a tree. To do this we first need to build nodes::
-
-        sage: leaf_1 = Leaf((2, 3))
-        sage: leaf_2 = Leaf((0, 0))
-        sage: leaf_3 = Leaf((1, 1))
-        sage: leaf_4 = Leaf((3, 2))
-        sage: celine_1 = Node({'Sports': leaf_1, 'Comedy': leaf_2})
-        sage: celine_2 = Node({'Sports': leaf_3, 'Comedy': leaf_4})
-        sage: root = Root({'Sports': celine_1, 'Comedy': celine_2})
-
-
-    We then want to assign the nodes to players::
-
-        sage: root.player = Player('Vince')
-        sage: celine_1.player = Player('Celine')
-        sage: celine_2.player = Player('Celine')
-
-    We see that :code:`celine_1` has the following attributes::
-
-        sage: celine_1.actions
-        ['Sports, 'Comedy']
-        sage: celine_1.children
-        [leaf_1, leaf_2]
-        sage: celine_1.player
-        Celine
-
-    We can then create the game::
-
-        sage: g1 = ExtensiveFormGame(root)
-        sage: g1
-        An extensive form game...
-
-    This creates an underlying tree object as well as a dictionary (or something) that keeps track of all the information::
-
-        sage: g.tree
-        The game tree...  # This is just a sage tree object
-
-    After creating the game, :code:`celine_1` has the following extra attributes:
-
-        sage: celine_1.actions
-        ['Sports, 'Comedy']
-        sage: celine_1.children
-        [leaf_1, leaf_2]
-        sage: celine_1.parent
-        Vince
-        sage: celine_1.player
-        True
-        sage: celine_1.is_complete
-        True
-
-    If we try to create a tree from a single node that is not a root we get an error::
-
-        sage: g1 = ExtensiveFormGame(celine_1)
-        ERROR
-
-    We can also create a game without specifying the children/parents of each node but by passing a tree with all the structural information.
-
-
-        sage: leaf_1 = Leaf((2, 3))
-        sage: leaf_2 = Leaf((0, 0))
-        sage: leaf_3 = Leaf((1, 1))
-        sage: leaf_4 = Leaf((3, 2))
-        sage: celine_1 = Node(['Sports', 'Comedy'])
-        sage: celine_2 = Node(['Sports', 'Comedy'])
-        sage: root = Root(['Sports', 'Comedy'])
-        sage: tree = Graph({root:[celine_1, celine_2], celine_1:[leaf_1, leaf_2], celine_2:[leaf_3, leaf_4]})
-
-    We need to specify the players::
-
-        sage: root.player = Player('Vince')
-        sage: celine_1.player = Player('Celine')
-        sage: celine_2.player = Player('Celine')
-
-    Our nodes now have the following information::
-
-        sage: celine_1.actions
-        ['Sports, 'Comedy']
-        sage: celine_1.player
-        True
-
-    The tree now contains all the structural information and the nodes contain the other information::
-
-        sage: g2 = ExtensiveFormGame(tree)
-        sage: g2
-        An extensive form game...
-
-    After creating the game the nodes have all the information::
-
-        sage: celine_1.actions
-        ['Sports, 'Comedy']
-        sage: celine_1.children
-        [leaf_1, leaf_2]
-        sage: celine_1.parent
-        Vince
-        sage: celine_1.player
-        True
-        sage: celine_1.is_complete
-        True
-
-    We see that these two trees although created differently are equal::
-
-        sage: g1 == g2
-        True
-
-    If we try to create a tree from a root but the root was generated with no specified children, we also get an error:
-
-        sage: root = Root(['Sports', 'Comedy'])
-        sage: g1 = ExtensiveFormGame(root)
-        ERROR
-
     """
 
 class Node():
-    def __init__(self, input):
+    def __init__(self, input, name = 'False'):
         """
+        Node input can be read to determine actions of node and children of node.
+        sage: child_1 = Leaf((0, 1))
+        sage: child_2 = Leaf((1, 0))
+        sage: mothernode = Node({'Action1': child_1, 'Action2'L child_2})
+        sage: mothernode.actions
+        ['Action1', 'Action2']
+        sage: mothernode.children
+        [child_1, child_2]
 
+        If we then create a second node, who has :code:`mothernode` as one of it's children, then the parent of :code:`mothernode` will be set to that node::
+        sage: sisternode = Node()
+        sage: grandmothernode = Node({'ActionA':mothernode, 'ActionB':sisternode})
+        sage: mothernode.parent
+        grandmothernode
+
+        We can also set names for the Node, which will then be returned instead::
+        sage: grandmothernode = Node({'ActionA':mothernode, 'ActionB':sisternode}, 'Node A')
+        sage: mothernode.parent
+        Node A
         """
-        self.player = False
+        #self.player = False
         
     def attributes():
         """
@@ -131,7 +38,11 @@ class Node():
             sage: laura_1.attributes 
             The node has the following attributes. Actions: False. Children: False. Parent: False. Player: False. 
         """
-
+    def changetype(type):
+        """ 
+        Will possibly be a function to change the type of a node to a root or a leaf, though specifics need to be worked out
+        How will this alter the game?
+        """
 class Leaf():
     def __init__(self, payoffs):
         """
