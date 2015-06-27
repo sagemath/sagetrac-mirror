dcc47a37d19bbd7c151988ce610d0a4ca7a1d8af
Frédéric Chapoton <chapoton@math.univ-lyon1.fr>
2015-06-27 20:43:37 +0200

    trac #14974 adding a test for pdf doc and for handling of utf8 file by file

---
+++ src/sage/combinat/horrible2.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+r"""
+just another horrible python file
+
+will some possible problems for detection by the patchbot
+
+starting with unicode hé hö ù 羊
+
+but with unicode declaration, so that it should be ok
+
+but with pdf errors : `\QQ` is good but `\Q` is not good
+"""
