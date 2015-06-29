1240e779cc9b11dccb373e9fb4b8ba10ed3b1418
Martin R. Albrecht <martinralbrecht@googlemail.com>
2015-06-29 14:01:46 +0100

    update copyright statement to Sage standard

--- src/sage/libs/openf4.pyx
+++ src/sage/libs/openf4.pyx
@@ -37,23 +37,14 @@ TESTS::
 
 """
 
-
-#*****************************************************************************
-#
-#                               Sage
-#
+#*****************************************************************************#
 #       Copyright (C) 2015 Antoine Joux and Vanessa Vitse
-#       Copyright (C) 2015 Titouan COLADON
+#       Copyright (C) 2015 Titouan Coladon
 #
-#  Distributed under the terms of the GNU General Public License (GPL)
-#
-#    This code is distributed in the hope that it will be useful,
-#    but WITHOUT ANY WARRANTY; without even the implied warranty of
-#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
-#    General Public License for more details.
-#
-#  The full text of the GPL is available at:
-#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 2 of the License, or
+# (at your option) any later version.
 #                  http://www.gnu.org/licenses/
 #*****************************************************************************
 
