3d3d135685131dbb70e2ea89eaa48e3e4d8c1347
Nadia Lafreniere <nadia.l@lacim.ca>
2015-09-08 17:09:51 -0700

    Modified location of Lyndon word, added deprecation

--- src/sage/combinat/all.py
+++ src/sage/combinat/all.py
@@ -129,7 +129,7 @@ from set_partition_ordered import OrderedSetPartition, OrderedSetPartitions
 from subset import Subsets
 #from subsets_pairwise import PairwiseCompatibleSubsets
 from necklace import Necklaces
-from lyndon_word import LyndonWord, LyndonWords, StandardBracketedLyndonWords
+from words.lyndon_word import LyndonWord, LyndonWords, StandardBracketedLyndonWords
 from dyck_word import DyckWords, DyckWord
 from sloane_functions import sloane
 
