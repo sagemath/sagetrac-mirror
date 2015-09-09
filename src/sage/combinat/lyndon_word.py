b2ee0a8fea04c4a50bac775baf934e005f9de88b
Nadia Lafreniere <nadia.l@lacim.ca>
2015-09-08 22:09:27 -0700

    trying to fix deprecation

--- src/sage/combinat/lyndon_word.py
+++ src/sage/combinat/lyndon_word.py
@@ -5,10 +5,10 @@ This module has been moved to sage.combinat.words. It should be called from ther
 """
 
 from sage.misc.lazy_import import lazy_import
-lazy_import(sage.combinat.words.LyndonWord, deprecation=19150)
+lazy_import('sage.combinat.words.lyndon_word', '*', deprecation=19150)
 
 """
 TESTS::
 
-sage: fromm sage.combinat.lyndon_word import LyndonWord
+sage: fromm sage.combinat.lyndon_word import *
 """
