a4c5eff09cbf130a393dd69f0d35516417e3ab52
Nadia Lafreniere <nadia.l@lacim.ca>
2015-09-11 22:08:41 -0400

    Moving doctests in lyndon_word.py + editing the list of modules for documentation (reviewer's suggestions)

--- src/sage/combinat/lyndon_word.py
+++ src/sage/combinat/lyndon_word.py
@@ -2,13 +2,11 @@
 Lyndon Word
 
 This module has been moved to sage.combinat.words. It should be called from there.
+
+TESTS::
+
+    sage: from sage.combinat.lyndon_word import LyndonWord
 """
 
 from sage.misc.lazy_import import lazy_import
 lazy_import('sage.combinat.words.lyndon_word', '*', deprecation=19150)
-
-"""
-TESTS::
-
-sage: from sage.combinat.lyndon_word import *
-"""
