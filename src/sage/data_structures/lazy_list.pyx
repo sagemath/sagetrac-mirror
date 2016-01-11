eef095586371e5a516d8d8a71ec46cf640e06291
Daniel Krenn <git@danielkrenn.at>
2016-01-11 19:15:34 +0100

    simplify and extend __repr__

--- src/sage/data_structures/lazy_list.pyx
+++ src/sage/data_structures/lazy_list.pyx
@@ -108,6 +108,14 @@ empty_lazy_list.start = 0
 empty_lazy_list.stop = 0
 empty_lazy_list.step = 1
 empty_lazy_list.cache = []
+empty_lazy_list.name = 'lazy list'
+empty_lazy_list.opening_delimiter = '['
+empty_lazy_list.separator = ', '
+empty_lazy_list.more = '...'
+empty_lazy_list.closing_delimiter = ']'
+empty_lazy_list.preview = 3
+#empty_lazy_list = lazy_list_generic(initial_values=[],
+#                                    start=0, stop=0, step=1)
 
 
 def lazy_list(data=None, initial_values=None, start=None, stop=None, step=None,
@@ -270,7 +278,11 @@ cdef class lazy_list_generic(object):
         sage: l[200]
         1229
     """
-    def __init__(self, cache=None, start=None, stop=None, step=None):
+    def __init__(self, cache=None, master=None,
+                 start=None, stop=None, step=None,
+                 name=None, separator=None, more=None,
+                 opening_delimiter=None, closing_delimiter=None,
+                 preview=None):
         r"""
         No check is performed on input and bad input can result in a Sage crash.
         You are advised to use the function :func:`lazy_list` instead. The only
@@ -305,9 +317,19 @@ cdef class lazy_list_generic(object):
             lazy list ['haha', 1]
         """
         self.cache = [] if cache is None else cache
+        self.master = master
+
         self.start = 0 if start is None else start
         self.stop = PY_SSIZE_T_MAX if stop is None else stop
         self.step = 1 if step is None else step
+
+        self.name = 'lazy list' if name is None else name
+        self.opening_delimiter = '[' if opening_delimiter is None else opening_delimiter
+        self.separator = ', ' if separator is None else separator
+        self.more = '...' if more is None else more
+        self.closing_delimiter = ']' if closing_delimiter is None else closing_delimiter
+        self.preview = 3 if preview is None else preview
+
 
     def start_stop_step(self):
         r"""
@@ -472,32 +494,22 @@ cdef class lazy_list_generic(object):
         cdef Py_ssize_t num_elts = 1 + (self.stop-self.start-1) / self.step
         cdef Py_ssize_t length = len(self.cache)
 
-        if (length <= self.start + 3*self.step and
+        if (length <= self.start + self.preview*self.step and
             num_elts != length / self.step):
-            self._fit(self.start + 3*self.step)
+            self._fit(self.start + self.preview*self.step)
             num_elts = 1 + (self.stop-self.start-1) / self.step
 
-        if num_elts == 0:
-            return "lazy list []"
-
-        if num_elts == 1:
-            return "lazy list [{!r}]".format(self.get(0))
-
-        if num_elts == 2:
-            return "lazy list [{!r}, {!r}]".format(
-                    self.get(0),
-                    self.get(1))
-
-        if num_elts == 3:
-            return "lazy list [{!r}, {!r}, {!r}]".format(
-                self.get(0),
-                self.get(1),
-                self.get(2))
-
-        return "lazy list [{!r}, {!r}, {!r}, ...]".format(
-                self.get(0),
-                self.get(1),
-                self.get(2))
+        cdef str s = self.name
+        if s:
+            s += ' '
+        s += self.opening_delimiter
+        cdef list E = list('{!r}'.format(self.get(n))
+                           for n in xrange(min(num_elts, self.preview)))
+        if num_elts > self.preview:
+            E.append(self.more)
+        s += self.separator.join(E)
+        s += self.closing_delimiter
+        return s
 
     def __reduce__(self):
         r"""
