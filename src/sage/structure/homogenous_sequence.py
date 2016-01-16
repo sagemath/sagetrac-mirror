cb45f06e91a5a075d1d674d04f16928a6d6b6d45
Daniel Krenn <git@danielkrenn.at>
2016-01-16 16:38:55 +0100

    create homogenous_sequence

---
+++ src/sage/structure/homogenous_sequence.py
@@ -0,0 +1,28 @@
+r"""
+Homogenous Sequences
+
+An immutable sequence with elements of a common guaranteed
+universe. The elements are evaluated lazily.
+
+Various
+=======
+
+AUTHORS:
+
+- Daniel Krenn (2016)
+
+
+Classes and Methods
+===================
+"""
+
+# *****************************************************************************
+# Copyright (C) 2016 Daniel Krenn <dev@danielkrenn.at>
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 2 of the License, or
+# (at your option) any later version.
+# http://www.gnu.org/licenses/
+# *****************************************************************************
+
