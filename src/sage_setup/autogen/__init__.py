ba82534a637847807ab79975f7b5fc0b4c0e0b07
Erik M. Bray <erik.bray@lri.fr>
2016-05-27 16:12:38 +0200

    interpreters.py was quite long, so converted it to a package and broke the original module up into multiple logically separate submodules.
    Some of the doctests may still be broken due to the need to
    update a few imports; will do this in a later commit.

--- src/sage_setup/autogen/__init__.py
+++ src/sage_setup/autogen/__init__.py
@@ -5,8 +5,8 @@ def autogen_all():
     """
     Regenerate the automatically generated files of the Sage library.
     """
-    import pari
+    from . import pari
     pari.rebuild()
 
-    import interpreters
+    from . import interpreters
     interpreters.rebuild(os.path.join(SAGE_SRC, "sage", "ext", "interpreters"))
