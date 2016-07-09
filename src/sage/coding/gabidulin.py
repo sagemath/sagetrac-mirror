40a3d3727bad1de2d68fd84fa4abc13dad759d77
arpitdm <arpitdm@gmail.com>
2016-07-09 10:18:19 +0530

    Refactored code based on comments 3, 4 and 5. Added support for vector_to_matrix and matrix_to_vector representations of codewords. Added methods for rank_weight of a codeword and rank_distance between two codewords.

--- src/sage/coding/gabidulin.py
+++ src/sage/coding/gabidulin.py
@@ -1,4 +1,6 @@
 from sage.matrix.constructor import matrix
+from sage.matrix.matrix import is_Matrix
+from sage.structure.element import is_Vector
 from sage.matrix.constructor import column_matrix
 from sage.rings.finite_rings.finite_field_constructor import GF
 from sage.modules.free_module_element import vector
@@ -15,61 +17,46 @@ class GabidulinCode(SageObject):
     _registered_encoders = {}
     _registered_decoders = {}
     
-    def __init__(self, absolute_field, relative_field, length, dimension, linearly_independent_elements=None):
-        if not absolute_field.is_finite() or not absolute_field.is_field():
-            raise ValueError("absolute_field must be a finite field")
-        if not relative_field.is_finite() or not relative_field.is_field():
-            raise ValueError("relative_field must be a finite field")
-        p = relative_field.characteristic()
-        q = relative_field.order()
-        s = log(q, p)
-        m = log(absolute_field.order(), q)
-        sm = log(absolute_field.order(), p)
-        if not s.divides(sm):
-            raise ValueError("relative_field must be a subfield of the absolute_field")
-        self._prime_field = relative_field.base_ring()
-        self._relative_field = relative_field
-        self._relative_field_power = s
-        self._absolute_field = absolute_field
-        self._absolute_field_power = sm
+    def __init__(self, absolute_field, relative_field, length, dimension, evaluation_points=None):
+ 
+        FE = RelativeFiniteFieldExtension(absolute_field, relative_field)
+        self._relative_finite_field_extension = FE
+        m = absolute_field.degree()/relative_field.degree()
+        self._m = m
+        V = VectorSpace(relative_field, m)
+        self._vector_space = V
+        
         if not length <= m or length not in ZZ or length < 1:
-            raise ValueError("length of the code must be a positive integer less than or equal to the absolute_field_power which is %d" % m )
+            raise ValueError("length of the code must be a positive integer less than or equal to \
+                    the absolute_field_power which is %d" % m )
         self._length = length
         
         if not dimension <= length or dimension not in ZZ or dimension < 1:
-            raise ValueError("dimension of the code must be a positive integer less than or equal to the length of the code which is %d" % length )
+            raise ValueError("dimension of the code must be a positive integer less than or equal \
+                    to the length of the code which is %d" % length )
         self._dimension = dimension
 
-#       V = VectorSpace(relative_field, m) is a vector space of dimension m over Fq. ``basis`` is the basis of Fqm over Fp.
-#       Fqm over Fq is a vector space of dimension m. ``linearly_independent_elements`` is a set of n elements belonging to Fqm.
-#       ``FE.relative_field_representation(linearly_independent_elements[i])`` gives elements belonging to Fq.
-#       And any n such elements are linearly independent over V. Thus, we generate g_0, g_1,...., g_{n-1}
-        V = VectorSpace(relative_field, m)
-        self._vector_space = V
-        FE = RelativeFiniteFieldExtension(absolute_field, relative_field)
-        self._relative_finite_field_extension = FE
-        
-        if linearly_independent_elements is None:
+        if evaluation_points is None:
             basis = FE.absolute_field_basis()
-            linearly_independent_elements = []
+            evaluation_points = []
             for i in range(length):
-                linearly_independent_elements.append(basis[i])
+                evaluation_points.append(basis[i])
         else:
-            if not len(linearly_independent_elements) == length:
+            if not len(evaluation_points) == length:
                 raise ValueError("The number of elements in the set should be equal to the length of the code")
             for i in range(length):
-                if not linearly_independent_elements[i] in absolute_field:
+                if not evaluation_points[i] in absolute_field:
                     raise ValueError("Element does not belong to absolute field")
             basis = []
-            for i in range(len(linearly_independent_elements)):
-                basis.append(FE.relative_field_representation(linearly_independent_elements[i]))
+            for i in range(len(evaluation_points)):
+                basis.append(FE.relative_field_representation(evaluation_points[i]))
             if V.linear_dependence(basis):
                 raise ValueError("The elements provided are not linearly independent")
-        self._evaluation_points = linearly_independent_elements
+        self._evaluation_points = evaluation_points
 
     def _repr_(self):
         return "[%s, %s, %s] Linear Gabidulin Code over %s" \
-                % (self.length(), self.dimension(), \
+                % (self.length(), self.dimension(),
                 self.minimum_distance(), self.base_field())
 
     def _latex_(self):
@@ -79,12 +66,11 @@ class GabidulinCode(SageObject):
 
     def __eq__(self, other):
         return isinstance(other, GabidulinCode) \
-                and self.absolute_field() == other.absolute_field() \
-                and self.relative_field() == other.relative_field() \
+                and self.relative_finite_field_extension().absolute_field() == other.relative_finite_field_extension().absolute_field() \
+                and self.relative_finite_field_extension().relative_field() == other.relative_finite_field_extension().relative_field() \
                 and self.length() == other.length() \
                 and self.dimension() == other.dimension() \
-                and self.basis() == other.basis() \
-                and self.evaluation_points() == self.evaluation_points()
+                and self.evaluation_points() == other.evaluation_points() \
                 
     def minimum_distance(self):
         return self.length() - self.dimension() + 1
@@ -93,26 +79,47 @@ class GabidulinCode(SageObject):
         eval_pts = self.evaluation_points()
         n = self.length()
         k = self.dimension()
-        coefficient_matrix = matrix(self.base_field(), n - 1, n, \
-                lambda i,j: pow(eval_pts[j], pow(self.relative_field().order(), -n + k + 1 + i)))
+        Fqm = self.base_field()
+        q = self.relative_finite_field_extension().relative_field().order()
+        coefficient_matrix = matrix(Fqm, n - 1, n, lambda i,j: pow(eval_pts[j], pow(q, -n + k + 1 + i)))
         solution_space = coefficient_matrix.right_kernel()
         parity_eval_pts = solution_space.random_element()
         return parity_eval_pts
  
     def dual_code(self):
         parity_eval_pts = self.parity_evaluation_points()
-        return GabidulinCode(self.absolute_field(), self.relative_field(), \
+        return GabidulinCode(self.relative_finite_field_extension().absolute_field(), 
+                self.relative_finite_field_extension().relative_field(),
                 self.length(), self.length() - self.dimension(), parity_eval_pts)
 
     def parity_check_matrix(self):
         E = GabidulinGeneratorMatrixEncoder(self.dual_code())
         return E.generator_matrix()
 
+    def rank_weight(self, codeword):
+        E = GabidulinPolynomialEvaluationEncoder(self)
+        if is_Matrix(codeword):
+            return codeword.rank()
+        elif is_Vector(codeword):
+            codeword_matrix = E.vector_to_matrix(codeword)
+            return codeword_matrix.rank()
+
+    def rank_distance(self, a, b):
+        E = GabidulinPolynomialEvaluationEncoder(self)
+        if is_Vector(a):
+            a = E.vector_to_matrix(a)
+        if is_Vector(b):
+            b = E.vector_to_matrix(b)
+        return (a - b).rank()
+
     def length(self):
         return self._length
     
     def dimension(self):
         return self._dimension
+    
+    def m(self):
+        return self._m
 
     def vector_space(self):
         return self._vector_space
@@ -120,23 +127,12 @@ class GabidulinCode(SageObject):
     def relative_finite_field_extension(self):
         return self._relative_finite_field_extension
 
+    def generator_matrix(self):
+        E = GabidulinGeneratorMatrixEncoder(self)
+        return E.generator_matrix()
+
     def base_field(self):
-        return self._absolute_field
-
-    def prime_field(self):
-        return self._prime_field
-
-    def absolute_field(self):
-        return self._absolute_field
-
-    def relative_field(self):
-        return self._relative_field
-
-    def absolute_field_power(self):
-        return self._absolute_field_power
-
-    def relative_field_power(self):
-        return self._relative_field_power
+        return self.relative_finite_field_extension().absolute_field()
 
     def evaluation_points(self):
         return self._evaluation_points
@@ -166,7 +162,7 @@ class GabidulinPolynomialEvaluationEncoder(Encoder):
         return isinstance(other, GabidulinPolynomialEvaluationEncoder) \
                 and self.code() == other.code()
 
-    def encode(self, p):
+    def encode(self, p, form="vector"):
         M = self.message_space()
         if p not in M:
             raise ValueError("The value to encode must be in %s" % M)
@@ -176,13 +172,57 @@ class GabidulinPolynomialEvaluationEncoder(Encoder):
         eval_pts = C.evaluation_points()
         sigma = self.sigma()
         codeword = []
-        elem = C.absolute_field().zero()
+        coefficients = p.coefficients()
+        degree = p.degree()
+        elem = C.relative_finite_field_extension().absolute_field().zero()
+        sigma_compositions = []
         for i in range(len(eval_pts)):
-            for j in range(p.degree()):
-                elem += p.coefficients()[j]*(pow(sigma(eval_pts[i]), j))
+            for j in range(degree):
+                if j == 0:
+                    sig = eval_pts[i]
+                    sigma_compositions.append(sig)
+                for k in range(j):
+                    sig = sigma(sig)
+                    sigma_compositions.append(sig)
+                elem += coefficients[j]*sigma_compositions[i]
             codeword.append(elem)
-            elem = C.absolute_field().zero()
-        return vector(codeword)
+            elem = C.relative_finite_field_extension().absolute_field().zero()
+        if form == "vector":
+            return vector(codeword)
+        elif form == "matrix":
+            return self.vector_to_matrix(vector(codeword))
+        else:
+            return ValueError("The argument 'form' takes only either 'vector' or 'matrix' as valid input")
+
+    def vector_to_matrix(self, codeword_vector):
+        C = self.code()
+        n = C.length()
+        FE = C.relative_finite_field_extension()
+        Fqm = FE.absolute_field()
+        Fq = FE.relative_field()
+        if not is_Vector(codeword_vector):
+            raise TypeError("Input must be a vector")
+        if not len(codeword_vector) == n:
+            raise ValueError("Codeword vector must be of length %d" % n)   # Does not check if vector belongs to code
+        codeword_matrix = matrix(Fq, C.m(), n, lambda i,j: FE.relative_field_representation(codeword_vector[j])[i])
+        return codeword_matrix
+
+    def matrix_to_vector(self, codeword_matrix):
+        C = self.code()
+        n = C.length()
+        m = C.m()
+        FE = self.relative_finite_field_extension()
+        Fqm = FE.absolute_field()
+        codeword_vector = []
+        if not is_Matrix(codeword_matrix):
+            raise TypeError("Input must be a matrix")
+        if not codeword_matrix.ncols() == n:
+            raise ValueError("Number of columns of matrix must be %d" % n)
+        if not codeword_matrix.nrows() == m:
+            raise ValueError("Number of columns of matrix must be %d" % m)  #Does not check if matrix belongs to code
+        for i in range(codeword_matrix.ncols()):
+            codeword_vector.append(FE.absolute_field_representation(codeword_matrix.column(i)))
+        return vector(codeword_vector)
 
     def sigma(self):
         return self._sigma
@@ -209,4 +249,5 @@ class GabidulinGeneratorMatrixEncoder(Encoder):
     def generator_matrix(self):
         C = self.code()
         eval_pts = C.evaluation_points()
-        return matrix(C.base_field(), C.dimension(), C.length(), lambda i,j: pow(eval_pts[j], pow(C.relative_field().order(), i)))
+        q = C.relative_finite_field_extension().relative_field().order()
+        return matrix(C.base_field(), C.dimension(), C.length(), lambda i,j: pow(eval_pts[j], pow(q, i)))
