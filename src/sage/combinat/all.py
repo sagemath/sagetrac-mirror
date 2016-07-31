18d1465952e88fe8aa6fbec964d924d774050ca0
Frédéric Chapoton <chapoton@math.univ-lyon1.fr>
2016-07-31 10:04:47 +0200

    trac 20014 relative import is now needed

--- src/sage/combinat/all.py
+++ src/sage/combinat/all.py
@@ -70,7 +70,7 @@ from .partition_algebra import SetPartitionsAk, SetPartitionsPk, SetPartitionsTk
 from .diagram_algebras import PartitionAlgebra, BrauerAlgebra, TemperleyLiebAlgebra, PlanarAlgebra, PropagatingIdeal
 
 #BDiagram
-from bdiagram import BDiagrams, BDiagram, BDiagramHopfAlgebra
+from .bdiagram import BDiagrams, BDiagram, BDiagramHopfAlgebra
 
 #Descent algebra
 from .descent_algebra import DescentAlgebra
