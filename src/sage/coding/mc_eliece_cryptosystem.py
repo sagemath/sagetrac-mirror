6ecc97859b4fe328acbe2ed92bb55f31b514f1cd
Fangan DOSSO <yssouffangan@gmail.com>
2016-08-26 23:29:44 +0200

    adds a first commit as backup

---
+++ src/sage/coding/mc_eliece_cryptosystem.py
@@ -0,0 +1,42 @@
+# -*- coding: utf-8 -*-
+
+from sage.matrix.constructor import matrix, random_matrix
+from sage.misc.prandom import shuffle
+from sage.coding.linear_code import LinearCode
+from sage.coding.channel_constructions import StaticErrorRateChannel
+
+class McElieceCryptosystem:
+
+    def __init__(self, code, encoder, decoder):
+        G = encoder.generator_matrix()
+        S = random_matrix(code.base_field(), code.dimension(), code.dimension())
+        while S.is_singular():
+            S = random_matrix(code.base_field(), code.dimension(), code.dimension())
+        sigma = range(0, code.length())
+        shuffle(sigma)
+        P = matrix(code.base_field(), code.length(), code.length())
+        for i in sigma:
+            P[i,sigma[i]] = 1
+        G_pub = S*G*P
+        C_pub = LinearCode(G_pub)
+        E_pub = C_pub.encoder()  
+        self.pubkey = E_pub, decoder.decoding_radius()
+        self.privkey = (S.inverse(), P.inverse(), decoder)
+    
+    def encrypt(self, m):
+	E_pub, t = self.pubkey
+        c = E_pub.encode(m)
+        ambient = E_pub.code().ambient_space()
+        channel = StaticErrorRateChannel(ambient, t)
+        y = channel.transmit(c)
+        return y
+    
+    def decrypt(self, y):
+        S_inv, P_inv, decoder = self.privkey
+        y = y * P_inv
+        m = decoder.decode_to_message(y)
+        m = m * S_inv
+        return m
+    
+    
+    
