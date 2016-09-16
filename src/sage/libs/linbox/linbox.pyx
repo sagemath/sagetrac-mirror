549e9e86d14d9f73cf0375be65f80f1191fa52c9
Charles Bouillaguet <charles.bouillaguet@univ-lille1.fr>
2016-09-16 11:04:46 +0200

    got rid of Givaro's horrible SpyInteger workaround

--- src/sage/libs/linbox/linbox.pyx
+++ src/sage/libs/linbox/linbox.pyx
@@ -24,35 +24,20 @@ cdef extern from "givaro/zring.h":
     cdef cppclass GivaroIntegerRing "Givaro::ZRing<Givaro::Integer>":
         GivaroIntegerRing()
 
-# for c++ debugging purposes
-cdef extern from "<iostream>" namespace "std":
-    cdef cppclass ostream:
-        ostream() except +
-        ostream& write (char* s, int n) except +
-    cdef ostream cout
+#### various flavors of integers
+cdef extern from "gmpxx.h":
+    cdef cppclass mpz_class:
+        mpz_class()
+        mpz_class(mpz_t a)
 
-
+# LinBox integers are actualy Givaro Integers, 
+# which are themselves thin wrappers around GMPXX's mpz_class, 
+# which are themsemves thin wrappers around GMP's mpz_t...
 cdef extern from "givaro/givinteger.h" namespace "Givaro":
-    #cdef cppclass mpz_class:
-    #    mpz_class()
-    #    mpz_class(mpz_t a)
-
-    # LinBox integers are actualy Givaro Integers, 
-    # which are themselves thin wrappers around GMPXX's mpz_class, 
-    # which are themsemves thin wrappers around GMP's mpz_t...
-
-    # the Givaro version currently bundled with sage has no constructor of Givaro::Integer from mpz_t....
-    # Later Givaro version do have it. In the meanwhile, we have to use the horrible SpyInteger hack.
     cdef cppclass GivaroInteger "Givaro::Integer":
         GivaroInteger()
-        # GivaroInteger(const mpz_class &a)    --- in more recent Givaro
-        void output "print" (ostream &)
-
-    cdef cppclass LinBoxSpyInteger "LinBox::SpyInteger":   # necessary evil
-        mpz_t get_mpz(GivaroInteger &t)                    #    for now
-
-#### ZZ[X] elements
-ctypedef vector[GivaroInteger] LinBox_ZZX_element
+        GivaroInteger(const mpz_class &a)
+        mpz_ptr get_mpz()
 
 #### sparse matrices mod small prime
 cdef extern from "linbox/matrix/sparse-matrix.h"  namespace "LinBox":
@@ -72,14 +57,15 @@ cdef extern from "linbox/vector/vector.h" namespace "LinBox":
 #### dense integer matrices
 cdef extern from "linbox/matrix/dense-matrix.h" namespace "LinBox":
     cdef cppclass BlasMatrix[Ring]:
-        BlasMatrix(const GivaroIntegerRing &F, int &nrows, int &ncols)
-        void setEntry (size_t i, size_t j, const GivaroInteger & x)
-        const GivaroInteger getEntry (size_t i, size_t j)
-        #void write(ostream)
+        BlasMatrix(const GivaroIntegerRing &F, int &nrows, int &ncols) # fixME Ring
+        void setEntry (size_t i, size_t j, const GivaroInteger & x)    # fixME Ring
+        const GivaroInteger getEntry (size_t i, size_t j)              # fixME Ring
 
+#### wrapping this up
 ctypedef BlasMatrix[GivaroIntegerRing] DenseMatrixZZ
 ctypedef DenseVector[ModIntField] DenseVectorGFp
 ctypedef SparseMatrix[ModIntField] SparseMatrixGFp
+ctypedef vector[GivaroInteger] LinBox_ZZX_element
 
 
 ######### algorithms
@@ -97,13 +83,13 @@ cdef extern from "linbox/solutions/methods.h" namespace "LinBox":
 ctypedef unsigned long & long_ref; 
 
 cdef extern from "linbox/solutions/rank.h" namespace "LinBox":
-    cdef long_ref rank(unsigned long &, const SparseMatrixGFp&, const Method&) 
-    cdef long_ref rank(unsigned long &, const DenseMatrixZZ&) 
+    cdef unsigned long & rank(unsigned long &, const SparseMatrixGFp&, const Method&) 
+    cdef unsigned long & rank(unsigned long &, const DenseMatrixZZ&) 
 
 ctypedef vector[ModIntFieldElement] vec_mod_int
 
 cdef extern from "linbox/solutions/solve.h" namespace "LinBox":
-    cdef long_ref solve(DenseVectorGFp &, SparseMatrixGFp&, const DenseVectorGFp&, const Method&) 
+    cdef unsigned long & solve(DenseVectorGFp &, SparseMatrixGFp&, const DenseVectorGFp&, const Method&) 
 
 cdef extern from "linbox/solutions/det.h" namespace "LinBox":
     cdef GivaroInteger & det(GivaroInteger &, DenseMatrixZZ &) 
@@ -140,20 +126,15 @@ cdef class Linbox_matrix_modn_sparse:
     cdef size_t rank(self, SparseAlgorithm algorithm):
         cdef unsigned long r
         cdef SparseMatrixGFp *M = <SparseMatrixGFp *> self._M
-
         rank(r, dereference(M), SparseEliminationTraits())
         return r
 
     cdef void solve(self, c_vector_modint **x, c_vector_modint *b, SparseAlgorithm algorithm):
         cdef SparseMatrixGFp *M = <SparseMatrixGFp *> self._M
         cdef SparseEliminationTraits method = SparseEliminationTraits()
-        
         cdef ModIntField F = M.field()
         cdef DenseVectorGFp B = DenseVectorGFp(F, self.ncols)
         cdef DenseVectorGFp X = DenseVectorGFp(F, self.nrows)
-
-        #B = DenseVectorGFp(F, self.ncols)
-        #X = DenseVectorGFp(F, self.nrows)
 
         # scatter b into a dense vector
         cdef size_t i
@@ -185,9 +166,9 @@ cdef class Linbox_integer_dense:
     cdef set(self, size_t nrows, size_t ncols, mpz_t ** matrix):
         cdef size_t i, j
         cdef GivaroIntegerRing *ZZ = new GivaroIntegerRing()
-        cdef LinBoxSpyInteger spy   # this crap just have static methods
-        cdef GivaroInteger t, u
         cdef DenseMatrixZZ *M = new DenseMatrixZZ(dereference(ZZ), nrows, ncols)    
+        cdef mpz_class foo
+        cdef GivaroInteger bar 
 
         self.nrows = nrows
         self.ncols = ncols
@@ -197,9 +178,9 @@ cdef class Linbox_integer_dense:
         # the various wrappers around mpz_t.
         for i in range(nrows):
             for j in range(ncols):
-                mpz_set(spy.get_mpz(t), matrix[i][j])
-                M.setEntry(i, j, t)
-
+                foo = mpz_class(matrix[i][j])
+                bar = GivaroInteger(foo)
+                M.setEntry(i, j, bar)
         self._M = M
 
     cdef size_t rank(self):
@@ -211,40 +192,34 @@ cdef class Linbox_integer_dense:
     cdef det(self):
         cdef DenseMatrixZZ *M = <DenseMatrixZZ *> self._M
         cdef GivaroInteger d
-        cdef Integer z  # Sage Integer
-        cdef LinBoxSpyInteger spy   # this crap just have static methods
-
-        det(d, dereference(M)) # we get a LinBox integer, and we need to convert it to a Sage integer..
-        z = Integer()
-        mpz_set(z.value, spy.get_mpz(d));
+        det(d, dereference(M))
+        # this produced a LinBox integer; convert it to a Sage integer
+        cdef Integer z = Integer()
+        mpz_set(z.value, d.get_mpz());
         return z
 
     cdef minpoly(self):
         cdef DenseMatrixZZ *M = <DenseMatrixZZ *> self._M
         cdef LinBox_ZZX_element pM
-        cdef LinBoxSpyInteger spy   # this crap just have static methods
 
         minpoly(pM, dereference(M))
         # make a list of sage integers from the vector of LinBox integers
         l = []
         for i in range(pM.size()):
             k = Integer()
-            mpz_set(k.value, spy.get_mpz(pM[i]))
+            mpz_set(k.value, pM[i].get_mpz())
             l.append(k)
         return l
     
     cdef charpoly(self):
         cdef DenseMatrixZZ *M = <DenseMatrixZZ *> self._M
         cdef LinBox_ZZX_element pM = LinBox_ZZX_element()
-        cdef LinBoxSpyInteger spy   # this crap just have static methods
-
-        # print("going in")
+        
         charpoly(pM, dereference(M))
-        # print("gone out")
         # make a list of sage integers from the vector of LinBox integers
         l = []
         for i in range(pM.size()):
             k = Integer()
-            mpz_set(k.value, spy.get_mpz(pM[i]))
+            mpz_set(k.value, pM[i].get_mpz())
             l.append(k)
         return l
