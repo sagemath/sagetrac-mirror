c73c232d9ea0dbcb6bcd19479e62c4cd2375770c
Martin Rubey <martin.rubey@tuwien.ac.at>
2016-10-25 08:18:44 +0200

    not sure, but I think this needs to set texinfo_documents, too

--- src/doc/en/reference/conf.py
+++ src/doc/en/reference/conf.py
@@ -52,6 +52,24 @@ latex_elements['preamble'] += r'''
 \makeatother
 '''
 
+# Grouping the document tree into texinfo files. List of tuples
+# (source start file, target name, title, author, dir_entry,
+# description, category).
+
+# for example, with target name = 'name', sphinx produces
+# @setfilename name.info
+# ...
+# @settitle title
+# ...
+# @dircategory category
+# @direntry
+# * dir_entry: (name.info). description
+# @end direntry
+
+texinfo_documents = [
+('index', name, title, u'The Sage Development Team', name, title, 'SageMath')
+]
+
 #Ignore all .rst in the _sage subdirectory
 exclude_patterns = exclude_patterns + ['_sage']
 
