45b05be1c609322ffd570486f7c81087443c73dd
Rusydi H. Makarim <rusydi.hasan@gmail.com>
2016-11-02 01:49:20 +0100

    adapt docstring to the behaviour of change_ring()

--- src/sage/rings/polynomial/multi_polynomial_sequence.py
+++ src/sage/rings/polynomial/multi_polynomial_sequence.py
@@ -1091,12 +1091,12 @@ class PolynomialSequence_generic(Sequence_generic):
 
     def change_ring(self, base_ring=None, names=None, order=None):
         """
-        Return a PolynomialSequence defined over a new polynomial ring isomorphic
-        to ``self.ring()``.
+        Return a ``PolynomialSequence`` with its elements defined over a new polynomial ring
+        determined by parameters ``base_ring``, ``names``, and ``order``.
 
         The term ordering of the new polynomial ring is given by the parameter ``order``
-        whereas ``names`` defines the variable names. Each polynomial in ``self`` will be
-        mapped to its isomorphic image in the new polynomial ring.
+        whereas ``names`` defines the variable names. The coefficients of polynomials in ``self``
+        are converted to ``base_ring`` elements.
 
         INPUT:
 
