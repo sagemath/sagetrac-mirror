808897820dc98055ae22034688d47c84f9dec66f
pportilla <p.portilla89@gmail.com>
2016-12-03 21:13:10 +0100

    silly mistake on the lazy imports

--- src/sage/geometry/all.py
+++ src/sage/geometry/all.py
@@ -26,9 +26,9 @@ from . import toric_plotter
 from .hyperbolic_space.all import *
 
 lazy_import('sage.geometry.ribbon_graph', 'RibbonGraph')
-lazy_import('sage.geometry.ribbon_graph', 'TatGraph')
-lazy_import('sage.geometry.ribbon_graph', 'safewalk')
-lazy_import('sage.geometry.ribbon_graph', 'check_tat_property')
-lazy_import('sage.geometry.ribbon_graph', 'bipartite_tat_graph')
+lazy_import('sage.geometry.tat_graph', 'TatGraph')
+lazy_import('sage.geometry.tat_graph', 'safewalk')
+lazy_import('sage.geometry.tat_graph', 'check_tat_property')
+lazy_import('sage.geometry.tat_graph', 'bipartite_tat_graph')
 lazy_import('sage.geometry.hyperplane_arrangement.arrangement', 'HyperplaneArrangements')
 lazy_import('sage.geometry.hyperplane_arrangement.library', 'hyperplane_arrangements')
