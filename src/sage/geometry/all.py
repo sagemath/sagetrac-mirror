fd0f53283e42682778ad3bfb3b55985a17539786
pportilla <p.portilla89@gmail.com>
2016-12-03 21:09:39 +0100

    this is by baldursigurds

--- src/sage/geometry/all.py
+++ src/sage/geometry/all.py
@@ -25,5 +25,9 @@ from .hyperbolic_space.all import *
 from .voronoi_diagram import VoronoiDiagram
 
 lazy_import('sage.geometry.ribbon_graph', 'RibbonGraph')
+lazy_import('sage.geometry.ribbon_graph', 'TatGraph')
+lazy_import('sage.geometry.ribbon_graph', 'safewalk')
+lazy_import('sage.geometry.ribbon_graph', 'check_tat_property')
+lazy_import('sage.geometry.ribbon_graph', 'bipartite_tat_graph')
 lazy_import('sage.geometry.hyperplane_arrangement.arrangement', 'HyperplaneArrangements')
 lazy_import('sage.geometry.hyperplane_arrangement.library', 'hyperplane_arrangements')
