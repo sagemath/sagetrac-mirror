ba840c6934228dc3c35e706f7e79f6ea22ac9f58
Erik M. Bray <erik.bray@lri.fr>
2016-12-13 17:37:10 +0000

    A couple enhancements to find_extra_files:
    * The "special files" argument now accepts fnmatch wildcards, as well
      as sub-directories--in this was find_extra_files can be used more or
      less analogously to the distutils package_data argument, on a per-
      package basis.
    * The cythonized_dir argument is now optional, and cythonized is not
      searched if it is not specified.
    
    This makes find_extra_files mostly superfluous to package_data, but allows
    the use of a single interface for finding/specifying all of sage's data
    files to setup().

--- src/sage_setup/find.py
+++ src/sage_setup/find.py
@@ -12,7 +12,13 @@ Recursive Directory Contents
 #*****************************************************************************
 
 
+import collections
+import itertools
 import os
+
+from os.path import join, relpath, normpath, dirname
+
+from fnmatch import fnmatch
 
 
 def find_python_sources(src_dir, modules=('sage',)):
@@ -74,7 +80,8 @@ def find_python_sources(src_dir, modules=('sage',)):
     return python_packages, python_modules
 
 
-def find_extra_files(packages, src_dir, cythonized_dir, special_filenames=[]):
+def find_extra_files(packages, src_dir, cythonized_dir=None,
+                     special_filenames=[]):
     """
     Find all extra files which should be installed.
 
@@ -87,12 +94,13 @@ def find_extra_files(packages, src_dir, cythonized_dir, special_filenames=[]):
 
     INPUT:
 
-    - ``packages`` -- a list of Python packages to be considered
+    - ``packages`` -- a list of Python packages to be considered, or a single
+      package name
 
     - ``src_dir`` -- the directory where to look for source files
 
     - ``cythonized_dir`` -- the directory where the Cython-generated
-      files are
+      files are; may be ommitted when adding non-Cython-generated files
 
     - ``special_filenames`` -- a list of filenames to be installed from
       ``src_dir``
@@ -105,23 +113,53 @@ def find_extra_files(packages, src_dir, cythonized_dir, special_filenames=[]):
         [('sage/modular/arithgroup',
           ['.../src/sage/modular/arithgroup/farey.pxd', ...farey_symbol.h...])]
     """
-    data_files = []
+
+    # `Destination-directory: source-files` mapping
+    data_files = collections.defaultdict(list)
+
+    if isinstance(packages, str):
+        packages = [packages]
+
+    def find_files(base_dir, pkg_dir, exts, patterns=[], descend=True):
+        src_dir = normpath(relpath(join(base_dir, pkg_dir)))
+
+        for cur_dir, dirs, filenames in os.walk(src_dir):
+            for filename in filenames:
+                filename = join(relpath(cur_dir, src_dir), filename)
+                if (filename.endswith(exts) or
+                        any(fnmatch(filename, pat) for pat in patterns)):
+                    # (destination directory, filename) tuple
+                    yield (normpath(join(pkg_dir, dirname(filename))),
+                           normpath(join(src_dir, filename)))
+
+            if not descend:
+                # Don't check in directories below the immediate package
+                # directory
+                break
+
+            # Don't descend into sub-packages as each sub-package must be
+            # listed individually--only descend into non-sub-package
+            # directories
+            for idx, dir in reversed(list(enumerate(dirs))):
+                if os.path.exists(join(cur_dir, dir,  '__init__.py')):
+                    del dirs[idx]
 
     for package in packages:
-        dir = package.replace('.', os.path.sep)
-        sdir = os.path.join(src_dir, dir)
-        cydir = os.path.join(cythonized_dir, dir)
+        pkg_dir = package.replace('.', os.path.sep)
 
-        files = [os.path.join(sdir, f) for f in os.listdir(sdir)
-                if f.endswith((".pxd", ".pxi")) or f in special_filenames]
-        if os.path.isdir(cydir):  # Not every directory contains Cython files
-            files += [os.path.join(cydir, f) for f in os.listdir(cydir)
-                    if f.endswith(".h")]
+        files = find_files(src_dir, pkg_dir, ('.pxd', '.pxi'),
+                           special_filenames)
 
-        if files:
-            data_files.append((dir, files))
+        if cythonized_dir is not None:
+            cythonized_files = find_files(cythonized_dir, pkg_dir, '.h',
+                                          descend=False)
+        else:
+            cythonized_files = []
 
-    return data_files
+        for dst, src in itertools.chain(files, cythonized_files):
+            data_files[dst].append(src)
+
+    return sorted(data_files.items())
 
 
 def installed_files_by_module(site_packages, modules=('sage',)):
@@ -178,7 +216,7 @@ def installed_files_by_module(site_packages, modules=('sage',)):
                 module_dir = '.'.join(dirpath.split(os.path.sep))
                 for filename in filenames:
                     base, ext = os.path.splitext(filename)
-                    filename = os.path.join(dirpath, filename)
+                    filename = join(dirpath, filename)
                     if base == '__init__':
                         add(module_dir, filename)
                     else:
