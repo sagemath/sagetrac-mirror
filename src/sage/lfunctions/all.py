eb2006785b5c456048ba4773fd58be652a409d5c
Ralf Stephan <ralf@ark.in-berlin.de>
2017-04-11 17:18:10 +0200

    16477: implement Dirichlet series
    Also-by: Jonathan Hanke

--- src/sage/lfunctions/all.py
+++ src/sage/lfunctions/all.py
@@ -1,5 +1,6 @@
 from sage.misc.lazy_import import lazy_import as _lazy_import_
 
+_lazy_import_('sage.lfunctions.dirichlet_series', ['dirichlet_L', 'dirichlet_series', 'DirichletSeries'])
 _lazy_import_("sage.lfunctions.dokchitser", "Dokchitser")
 _lazy_import_("sage.lfunctions.sympow", "sympow")
 _lazy_import_("sage.lfunctions.zero_sums", "LFunctionZeroSum")
