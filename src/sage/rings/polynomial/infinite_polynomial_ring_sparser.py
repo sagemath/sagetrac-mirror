8e8686149d95f58930a54467f1c5c8c42360a358
Daniel Krenn <git@danielkrenn.at>
2017-07-22 22:09:13 +0200

    header

---
+++ src/sage/rings/polynomial/infinite_polynomial_ring_sparser.py
@@ -0,0 +1,33 @@
+r"""
+Infinite Multivariate Polynomial Ring with Sparse Exponents
+
+Various
+=======
+
+AUTHORS:
+
+- Daniel Krenn (2017)
+
+ACKNOWLEDGEMENT:
+
+- Daniel Krenn is supported by the
+  Austrian Science Fund (FWF): P 24644-N26.
+
+Classes and Methods
+===================
+"""
+# *****************************************************************************
+# Copyright (C) 2017 Daniel Krenn <dev@danielkrenn.at>
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 2 of the License, or
+# (at your option) any later version.
+# http://www.gnu.org/licenses/
+# *****************************************************************************
+from __future__ import print_function
+from __future__ import absolute_import
+
+from six import iteritems
+from six import itervalues
+from six import string_types
