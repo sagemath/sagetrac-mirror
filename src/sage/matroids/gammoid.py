66ffb54fd76032983d0f6c4c4215ae0798e70a64
Zachary Gershkoff <zgersh2@math.lsu.edu>
2017-08-17 23:42:42 +0000

    Uploading changes, even though there's a bug in _prune_vertices()
    Ground set elements are being removed when they shouldn't.

--- src/sage/matroids/gammoid.py
+++ src/sage/matroids/gammoid.py
@@ -7,9 +7,9 @@ sets of vertices of `D`. Say a subset `X` of `E` is in a collection `I` if
 where `E` is the ground set of a matroid and `I` is its independent sets.
 
 Some authors use a reverse convention, where instead of a set `T` of roots,
-they have a starting set `S` that is linked into subsets of `E`. The gammoids
-in this class have the vertices `T` at the end of the directed paths, not the
-beginning.
+they have a starting set `S` that is linked into subsets of `E`. Here we use
+the convention that the vertices `T` are at the end of the directed paths,
+not the beginning.
 """
 from __future__ import absolute_import
 #*****************************************************************************
@@ -24,13 +24,14 @@ from __future__ import absolute_import
 from .matroid import Matroid
 from .utilities import sanitize_contractions_deletions, setprint_s
 
+from sage.graphs.digraph import DiGraph
 from .minor_matroid import MinorMatroid
 
 from copy import copy, deepcopy
 
 class Gammoid(Matroid):
     r"""
-    Gammoid
+    The gammoid class.
 
     INPUT:
 
@@ -66,7 +67,18 @@ class Gammoid(Matroid):
 
     def __init__(self, D, roots, groundset=None):
         """
-        Documentation will be under class definition.
+        See class definition for full documentation.
+
+        EXAMPLES::
+
+            sage: from sage.matroids.gammoid import Gammoid
+            sage: edgedict = {1:[2], 2:[3], 4:[1,5], 5:[2,3,8], 6:[4,7], 7:[5,8]}
+            sage: D = DiGraph(edgedict)
+            sage: M = Gammoid(D, roots=[1,2,3])
+            sage: N1 = Gammoid(D, groundset=range(1,8), roots=[1,2,3])
+            sage: N2 = M.delete(8)
+            sage: N1 == N2
+            True
         """
         self._roots = frozenset(roots)
         vertices = frozenset(D.vertices())
@@ -80,7 +92,8 @@ class Gammoid(Matroid):
             if not self._groundset.issubset(vertices):
                 raise ValueError("ground set must be a subset of the vertices")
 
-        self._D = copy(D)
+        # discard edge labels
+        self._D = DiGraph(D.edges(labels=False))
         self._prune_vertices()
         self._G = self._D.copy(immutable=True)
         self._rootv = self._D.add_vertex()
@@ -106,11 +119,106 @@ class Gammoid(Matroid):
         """
         vertices_c = self._roots.union(self._groundset)
         extra_sources = set(self._D.sources()).difference(vertices_c)
-        extra_sinks = set(self._D.sources()).difference(vertices_c)
+        extra_sinks = set(self._D.sinks()).difference(vertices_c)
         while extra_sources or extra_sinks:
             self._D.delete_vertices(set(extra_sources).union(extra_sinks))
             extra_sources = set(self._D.sources()).difference(vertices_c)
-            extra_sinks = set(self._D.sources()).difference(vertices_c)
+            extra_sinks = set(self._D.sinks()).difference(vertices_c)
+
+    def __hash__(self):
+        r"""
+        Return an invariant of the matroid.
+
+        This function is called when matroids are added to a set. It is very
+        desirable to override it so it can distinguish matroids on the same
+        groundset, which is a very typical use case!
+
+        .. WARNING::
+
+            This method is linked to __richcmp__ (in Cython) and __cmp__ or
+            __eq__/__ne__ (in Python). If you override one, you should (and in
+            Cython: MUST) override the other!
+
+        EXAMPLES::
+
+            sage: from sage.matroids.gammoid import Gammoid
+            sage: D = digraphs.TransitiveTournament(5)
+            sage: M1 = Gammoid(D, roots=[3,4])
+            sage: M2 = Gammoid(D, roots=[2,3])
+            sage: hash(M1) == hash(M2)
+            False
+            sage: N1 = M1.delete(2)
+            sage: N2 = Gammoid(D, roots=[3,4], groundset=[0,1,3,4])
+            sage: hash(N1) == hash(N2)
+            True
+            sage: D.delete_edge(0,4)
+            sage: M3 = Gammoid(D, roots=[3,4])
+            sage: hash(M1) == hash(M3)
+            False
+        """
+        return hash((self._G, self._groundset, self._roots))
+
+    def __eq__(self, other):
+        """
+        Compare two matroids.
+
+        For two graphic matroids to be equal, all attributes of the underlying
+        graphs must be equal.
+
+        INPUT:
+
+        - ``other`` -- A matroid.
+
+        OUTPUT:
+
+        ``True`` if ``self`` and ``other`` have the same digraph, roots, and
+        ground set; ``False`` otherwise.
+
+        EXAMPLES::
+
+            sage: from sage.matroids.gammoid import Gammoid
+            sage: D = digraphs.TransitiveTournament(5)
+            sage: M1 = Gammoid(D, roots=[3,4])
+            sage: M2 = Gammoid(D, roots=[2,3])
+            sage: M1 == M2
+            False
+            sage: N1 = M1.delete(2)
+            sage: N2 = Gammoid(D, roots=[3,4], groundset=[0,1,3,4])
+            sage: N1 == N2
+            True
+        """
+        if not isinstance(other, Gammoid):
+            return False
+        # The roots are implied by self._D
+        return (self._D == other._D and self._groundset == other._groundset)
+
+    def __ne__(self, other):
+        """
+        Compare two matroids.
+
+        INPUT:
+
+        - ``other`` -- A matroid.
+
+        OUTPUT:
+
+        ``False`` if ``self`` and ``other`` have the same digraphs, roots, and
+        ground set; ``True`` otherwise.
+
+        EXAMPLES::
+
+        sage: from sage.matroids.gammoid import Gammoid
+        sage: edgedict = {1:[2], 2:[3], 4:[1,5], 5:[2,3,8], 6:[4,7], 7:[5,8]}
+        sage: D = DiGraph(edgedict)
+        sage: M = Gammoid(D, roots=[1,2,3])
+        sage: D.relabel([1,2,3,4,5,6,7,'a'])
+        sage: N = Gammoid(D, roots=[1,2,3])
+        sage: M == N
+        False
+        sage: M.delete(8) == N.delete('a')
+        True
+        """
+        return (not self == other)
 
     def digraph(self):
         """
@@ -131,6 +239,18 @@ class Gammoid(Matroid):
     def digraph_plot(self):
         """
         Plot the graph with color-coded vertices.
+
+        Vertices that are elements but not roots will be shown as blue. Vertices that
+        are roots but not elements are red. Vertices that are both are pink, and vertices
+        that are neither are grey.
+
+        EXAMPLES::
+
+            sage: from sage.matroids.gammoid import Gammoid
+            sage: D = digraphs.TransitiveTournament(4)
+            sage: M = Gammoid(D, roots=[2,3])
+            sage: M.digraph_plot()
+            Graphics object consisting of 11 graphics primitives
         """
         self._inter = frozenset(self._roots.intersection(self._groundset))
         self._buckets = frozenset(self._roots.difference(self._inter))
@@ -160,7 +280,6 @@ class Gammoid(Matroid):
             2
             sage: M.rank(M.groundset())
             2
-
         """
         source = self._D.add_vertex()
         for x in X:
@@ -189,6 +308,15 @@ class Gammoid(Matroid):
     def _repr_(self):
         """
         Returns a string representation of the matroid.
+
+        EXAMPLES:
+
+            sage: from sage.matroids.gammoid import Gammoid
+            sage: edgelist = [(0, 4), (0, 5), (1, 0), (1, 4), (2, 0),
+            ....: (2, 1), (2, 3), (2, 6), (3, 4), (3, 5), (4, 0), (5, 2), (6, 5)]
+            sage: D = DiGraph(edgelist)
+            sage: M = Gammoid(D, roots=[4,5,6]); M
+            Gammoid of rank 3 on 7 elements
         """
         self._mrank = str(self._rank(self._groundset))
         self._elts = str(len(self._groundset))
@@ -210,7 +338,20 @@ class Gammoid(Matroid):
         OUTPUT:
 
         If there are contractions, a MinorMatroid. If not, a Gammoid.
+
+        EXAMPLES::
+
+            sage: from sage.matroids.gammoid import Gammoid
+            sage: edgedict = {1:[2], 2:[3], 4:[1,5], 5:[2,3,8], 6:[4,7], 7:[5,8]}
+            sage: D = DiGraph(edgedict)
+            sage: M = Gammoid(D, roots=[1,2,3]); M
+            Gammoid of rank 3 on 8 elements
+            sage: N1 = M.delete([3,4])
+            sage: N1.groundset()
+            frozenset({1, 2, 5, 6, 7, 8})
         """
+        # Unlike TransversalMatroid, we don't check for coloops to delete
+        # because that would mean altering the roots
         if deletions:
             new_groundset = self.groundset().difference(deletions)
             N = Gammoid(self.digraph(), self._roots, new_groundset)
