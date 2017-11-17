7eb3f6baf494e1288428700b11d70efd64e3d3a6
Erik M. Bray <erik.bray@lri.fr>
2017-11-17 13:58:41 +0000

    Adds a context manager that can save and restore the state of the atexit module.
    This functionality is currently only needed by the test runner (see
    DocTestTask) but is non-trivial on Python 3 since the list of registered
    atexit handler functions is no longer exposed to Python.
    
    This is perhaps the first example we have in sage of a Python 3-only extension
    module.  This establishes a convention that module names ending with '_py3'
    should only be built on Python 3.  Typically these should also begin with an
    underscore to emphasize that they are an implementation detail.  They can then
    be conditionally imported from a 'public' module of the same name without the
    _*_py3.

---
+++ src/sage/misc/_context_py3.pyx
@@ -0,0 +1,75 @@
+# -*- encoding: utf-8 -*-
+
+"""Internal module with C implementations of some context managers."""
+
+
+__all__ = ['restore_atexit']
+
+
+import atexit
+
+
+from cpython.ref cimport PyObject
+
+
+ctypedef struct atexit_callback:
+    PyObject* func
+    PyObject* args
+    PyObject* kwargs
+
+
+ctypedef struct atexitmodule_state:
+    atexit_callback** atexit_callbacks
+    int ncallbacks
+    int callback_len
+
+
+cdef extern from "Python.h":
+    void* PyModule_GetState(object module)
+
+
+cdef class restore_atexit:
+    cdef int _clear
+    cdef list _exithandlers
+
+    def __init__(self, clear=False):
+        self._clear = clear
+        self._exithandlers = []
+
+    def __enter__(self):
+        self.copy_state()
+        if self._clear:
+            atexit._clear()
+
+        return self
+
+    def __exit__(self, *exc):
+        """
+        Restore the original atexit module state by first clearing it, then
+        re-registering all the saved callbacks.
+        """
+
+        atexit._clear()
+
+        for callback in self._exithandlers:
+            atexit.register(callback[0], *callback[1], **callback[2])
+
+        del self._exithandlers[:]
+
+    cdef copy_state(self):
+        cdef atexitmodule_state* state
+        cdef atexit_callback callback
+        cdef int idx
+        cdef object kwargs
+
+        state = <atexitmodule_state*>PyModule_GetState(atexit)
+
+        for idx in range(state.ncallbacks):
+            callback = state.atexit_callbacks[idx][0]
+            if callback.kwargs:
+                kwargs = <object>callback.kwargs
+            else:
+                kwargs = {}
+            self._exithandlers.append((<object>callback.func,
+                                       <object>callback.args,
+                                       kwargs))
