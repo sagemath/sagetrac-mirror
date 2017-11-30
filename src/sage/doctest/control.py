d9c93a7f8ce56096b0e021a8555107eb9f700b29
Erik M. Bray <erik.bray@lri.fr>
2017-11-30 14:15:08 +0000

    Various fixes intended to ensure that opened files are closed explictly where necessary in order to avoid ResourceWarnings.
    Some of these *might* not be necessary, but it was easiest just to be
    consistent.  Note that DocTestWorker.kill() now calls sage_results_output,
    which ensures that the DocTestWorker.outtmpfile is closed.  Thus we call
    kill() on every worker explicitly started in the tests, just to be sure.

--- src/sage/doctest/control.py
+++ src/sage/doctest/control.py
@@ -366,19 +366,34 @@ class DocTestController(SageObject):
         # write to the actual standard output, regardless of
         # redirections.
         if options.serial:
-            real_stdout = os.fdopen(os.dup(sys.stdout.fileno()), "a")
+            self._real_stdout = os.fdopen(os.dup(sys.stdout.fileno()), "w")
+            self._close_stdout = True
         else:
             # Parallel mode: no special tricks needed
-            real_stdout = sys.stdout
+            self._real_stdout = sys.stdout
+            self._close_stdout = False
 
         if self.logfile is None:
-            self.logger = real_stdout
+            self.logger = self._real_stdout
         else:
-            self.logger = Logger(real_stdout, self.logfile)
+            self.logger = Logger(self._real_stdout, self.logfile)
 
         self.stats = {}
         self.load_stats(options.stats_path)
         self._init_warn_long()
+
+    def __del__(self):
+        if getattr(self, 'logfile', None) is not None:
+            try:
+                self.logfile.close()
+            except OSError:
+                pass
+
+        if getattr(self, '_close_stdout', False):
+            try:
+                self._real_stdout.close()
+            except OSError:
+                pass
 
     def _init_warn_long(self):
         """
