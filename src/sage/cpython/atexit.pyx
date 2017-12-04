0d4c02d28233f78894015fbe4c8af1c4de1a4b38
Erik M. Bray <erik.bray@lri.fr>
2017-12-04 11:22:52 +0000

    Minor cleanup

--- src/sage/cpython/atexit.pyx
+++ src/sage/cpython/atexit.pyx
@@ -1,6 +1,6 @@
 # -*- encoding: utf-8 -*-
 
-"""A home for miscellaneous context managers."""
+"""Utilities for interfacing with the standard library's atexit module."""
 
 #*****************************************************************************
 #       Copyright (C) 2017 Erik M. Bray <erik.bray@lri.fr>
@@ -17,8 +17,6 @@ from __future__ import absolute_import
 
 import atexit
 
-import six
-
 
 __all__ = ['restore_atexit']
 
@@ -33,7 +31,7 @@ cdef class restore_atexit(object):
     - ``clear`` (`bool`, default `False`) -- if `True`, clear already
       registered atexit handlers upon entering the context.
 
-    EXAMPLES::
+    EXAMPLES:
 
     For this example we will wrap the entire example with
     ``restore_atexit(True)`` so as to start with a fresh atexit module state
@@ -41,21 +39,21 @@ cdef class restore_atexit(object):
 
     Note that the function ``atexit._run_exitfuncs()`` runs all registered
     handlers, and then clears the list of handlers, so we can use it to test
-    manipulation of the ``atexit`` state.
+    manipulation of the ``atexit`` state::
 
-    sage: import atexit
-    sage: from sage.cpython.atexit import restore_atexit
-    sage: def handler(*args, **kwargs):
-    ....:     print((args, kwargs))
-    sage: with restore_atexit(clear=True):
-    ....:     atexit._run_exitfuncs()  # Should be none registered
-    ....:     atexit.register(handler, 1, 2, c=3)
-    ....:     with restore_atexit():
-    ....:         atexit._run_exitfuncs()  # Run just registered handler
-    ....:     atexit._run_exitfuncs()  # Handler should be run again
-    <function handler at 0x...>
-    ((1, 2), {'c': 3})
-    ((1, 2), {'c': 3})
+        sage: import atexit
+        sage: from sage.cpython.atexit import restore_atexit
+        sage: def handler(*args, **kwargs):
+        ....:     print((args, kwargs))
+        sage: with restore_atexit(clear=True):
+        ....:     atexit._run_exitfuncs()  # Should be none registered
+        ....:     atexit.register(handler, 1, 2, c=3)
+        ....:     with restore_atexit():
+        ....:         atexit._run_exitfuncs()  # Run just registered handler
+        ....:     atexit._run_exitfuncs()  # Handler should be run again
+        <function handler at 0x...>
+        ((1, 2), {'c': 3})
+        ((1, 2), {'c': 3})
     """
 
     cdef list _exithandlers
@@ -76,10 +74,10 @@ cdef class restore_atexit(object):
         _set_exithandlers(self._exithandlers)
 
 
+# These are helper functions for implementing restore_atexit.  The only reason
+# they're broken out from the class itself is because these require very
+# different implementations on Python 2 and 3
 IF PY_MAJOR_VERSION == 2:
-    # These are helper functions for implementing restore_atexit.  The only
-    # reason they're broken out from the class itself is because these require
-    # very different implements on Python 2 and 3
     def _get_exithandlers():
         """Return list of exit handlers registered with the atexit module."""
         return atexit._exithandlers[:]
@@ -100,6 +98,10 @@ IF PY_MAJOR_VERSION == 2:
 ELSE:
     from cpython.ref cimport PyObject
 
+    # Internal structures defined in the CPython source in
+    # Modules/atexitmodule.c and subject to (but unlikely to) change.  Watch
+    # https://bugs.python.org/issue32082 for a request to (eventually)
+    # re-expose more of the atexit module's internals to Python
     ctypedef struct atexit_callback:
         PyObject* func
         PyObject* args
@@ -116,7 +118,7 @@ ELSE:
         void* PyModule_GetState(object module)
 
 
-    cdef inline list _get_exithandlers():
+    def _get_exithandlers():
         """Return list of exit handlers registered with the atexit module."""
         cdef atexitmodule_state* state
         cdef atexit_callback callback
@@ -143,7 +145,7 @@ ELSE:
         return exithandlers
 
 
-    cdef inline _set_exithandlers(list exithandlers):
+    def _set_exithandlers(list exithandlers):
         """
         Replace the list of exit handlers registered with the atexit module
         with a new list.
@@ -155,6 +157,6 @@ ELSE:
             atexit.register(callback[0], *callback[1], **callback[2])
 
 
-    cdef inline _clear_exithandlers():
+    def _clear_exithandlers():
         """Clear the atexit module of all registered exit handlers."""
         atexit._clear()
