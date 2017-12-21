22a45f392e6e18d174830b2d0d11916410804e5d
Marcelo Forets <mforets@gmail.com>
2017-12-21 16:50:48 +0100

    24212: Fresnel integrals

--- src/sage/functions/all.py
+++ src/sage/functions/all.py
@@ -4,7 +4,8 @@ from sage.misc.lazy_import import lazy_import
 
 lazy_import('sage.functions.piecewise_old', 'Piecewise')   # deprecated
 lazy_import('sage.functions.piecewise', 'piecewise')
-lazy_import('sage.functions.error', ['erf', 'erfc', 'erfi', 'erfinv'])
+lazy_import('sage.functions.error', ['erf', 'erfc', 'erfi', 'erfinv',
+    'fresnel_sin', 'fresnel_cos'])
 
 from .trig import ( sin, cos, sec, csc, cot, tan,
                    asin, acos, atan,
@@ -25,8 +26,8 @@ from .other import ( ceil, floor, gamma, psi, factorial, beta, binomial,
                     abs_symbolic, sqrt, log_gamma,
                     gamma_inc, incomplete_gamma, gamma_inc_lower,
                     arg, real_part, real, frac,
-                    imag_part, imag, imaginary, conjugate, cases,
-                    complex_root_of)
+                    imag_part, imag, imaginary, conjugate,
+                    cases, complex_root_of)
 
 from .log import (exp, exp_polar, log, ln, polylog, dilog, lambert_w, harmonic_number)
 
