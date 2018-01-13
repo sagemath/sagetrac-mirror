9aaf77896d9806d92fe2630f28bb5652320fb8f4
Victor Porton <porton@narod.ru>
2018-01-14 01:46:58 +0200

    one more testing example

--- src/sage/relations/binary.py
+++ src/sage/relations/binary.py
@@ -43,6 +43,8 @@ def BinaryRelation(x_range, y_range, points=[]):
         {(2, 1)}
         sage: r2.compose(r1)
         {(0, 2)}
+        sage: r1.union(r2)
+        {(0, 1), (1, 2)}
 
     """
     return FiniteBinaryRelation(x_range, y_range, points)
