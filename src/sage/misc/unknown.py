f916205b0bcab16501053a70fa317c7b3b415aa8
Ralf Stephan <ralf@ark.in-berlin.de>
2018-01-26 15:57:11 +0100

    24345: Disallow boolean operations with Unknown

--- src/sage/misc/unknown.py
+++ src/sage/misc/unknown.py
@@ -1,6 +1,46 @@
 """
 The Unknown truth value
 
+The ``Unknown`` object is used in Sage in several places as return value
+in addition to ``True`` and ``False``, in order to signal uncertainty
+about or inability to compute the result. ``Unknown`` can be identified
+using ``is``, or by catching :class:`UnknownError` from a boolean operation.
+
+EXAMPLES::
+
+    sage: [Unknown is val for val in [True, False, Unknown]]
+    [False, False, True]
+
+    sage: def func(n): return [True, False, Unknown][n]
+    sage: def verbose(n):
+    ....:    try:
+    ....:        if func(n):
+    ....:            return 'yes'
+    ....:        else:
+    ....:            return 'no'
+    ....:    except UnknownError:
+    ....:        return 'maybe'
+    sage: [verbose(n) for n in range(3)]
+    ['yes', 'no', 'maybe']
+
+.. WARNING::
+
+    Unless PEP 335 is accepted, in the following cases,
+    ``and``, ``not`` and ``or`` fail or return a somewhat wrong value::
+
+        sage: not Unknown         # should return Unknown
+        Traceback (most recent call last):
+        ...
+        UnknownError: Unknown does not evaluate in boolean context
+        sage: Unknown and False   # should return False
+        Traceback (most recent call last):
+        ...
+        UnknownError: Unknown does not evaluate in boolean context
+        sage: Unknown or False    # should return Unknown
+        Traceback (most recent call last):
+        ...
+        UnknownError: Unknown does not evaluate in boolean context
+
 AUTHORS:
 
 - Florent Hivert (2010): initial version.
@@ -11,42 +51,36 @@ from sage.structure.sage_object import SageObject
 from sage.structure.unique_representation import UniqueRepresentation
 from sage.structure.richcmp import richcmp_method, rich_to_bool
 
+class UnknownError(TypeError):
+    """
+    Raised whenever :class:`Unknown` is used in a boolean operation.
+
+    EXAMPLES::
+
+        sage: not Unknown
+        Traceback (most recent call last):
+        ...
+        UnknownError: Unknown does not evaluate in boolean context
+    """
+    pass
 
 @richcmp_method
 class UnknownClass(UniqueRepresentation, SageObject):
     """
+    The Unknown truth value
+
+    The ``Unknown`` object is used in Sage in several places as return value
+    in addition to ``True`` and ``False``, in order to signal uncertainty
+    about or inability to compute the result. ``Unknown`` can be identified
+    using ``is``, or by catching :class:`UnknownError` from a boolean
+    operation.
+
     TESTS::
 
         sage: TestSuite(Unknown).run()
     """
     def __init__(self):
         """
-        The ``Unknown`` truth value
-
-        EXAMPLES::
-
-            sage: l = [False, Unknown, True]
-            sage: for a in l: print([a and b for b in l])
-            [False, False, False]
-            [Unknown, Unknown, Unknown]
-            [False, Unknown, True]
-
-            sage: for a in l: print([a or b  for b in l])
-            [False, Unknown, True]
-            [False, Unknown, True]
-            [True, True, True]
-
-        .. WARNING::
-
-            Unless PEP 335 is accepted, in the following cases,
-            ``and``, ``not`` and ``or`` return a somewhat wrong value::
-
-                sage: not Unknown         # should return Unknown
-                True
-                sage: Unknown and False   # should return False
-                Unknown
-                sage: Unknown or False    # should return Unknown
-                False
         """
 
     def _repr_(self):
@@ -60,17 +94,20 @@ class UnknownClass(UniqueRepresentation, SageObject):
 
     def __bool__(self):
         """
-        When evaluated in a boolean context ``Unknown()`` is evalutated into
-        ``False``.
+        When evaluated in a boolean context ``Unknown()`` raises an error.
 
         EXAMPLES::
 
             sage: bool(Unknown)
-            False
+            Traceback (most recent call last):
+            ...
+            UnknownError: Unknown does not evaluate in boolean context
             sage: not Unknown
-            True
+            Traceback (most recent call last):
+            ...
+            UnknownError: Unknown does not evaluate in boolean context
         """
-        return False
+        raise UnknownError('Unknown does not evaluate in boolean context')
 
     __nonzero__ = __bool__
 
@@ -94,7 +131,9 @@ class UnknownClass(UniqueRepresentation, SageObject):
         Compare with::
 
             sage: Unknown and False    # should return False
-            Unknown
+            Traceback (most recent call last):
+            ...
+            UnknownError: Unknown does not evaluate in boolean context
         """
         if other is False:
             return False
@@ -114,14 +153,18 @@ class UnknownClass(UniqueRepresentation, SageObject):
             sage: Unknown | False
             Unknown
             sage: Unknown | Unknown
-            Unknown
+            Traceback (most recent call last):
+            ...
+            UnknownError: Unknown does not evaluate in boolean context
             sage: Unknown | True
             True
 
         Compare with::
 
             sage: Unknown or False    # should return Unknown
-            False
+            Traceback (most recent call last):
+            ...
+            UnknownError: Unknown does not evaluate in boolean context
         """
         if other:
             return True
@@ -144,7 +187,9 @@ class UnknownClass(UniqueRepresentation, SageObject):
         Compare with::
 
             sage: not Unknown  # should return Unknown
-            True
+            Traceback (most recent call last):
+            ...
+            UnknownError: Unknown does not evaluate in boolean context
         """
         return self
 
@@ -168,7 +213,7 @@ class UnknownClass(UniqueRepresentation, SageObject):
         if other is self:
             return rich_to_bool(op, 0)
         if not isinstance(other, bool):
-            return NotImplemented
+            return Unknown
         if other:
             return rich_to_bool(op, -1)
         else:
