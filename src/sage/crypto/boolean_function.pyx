b29c0e5e40f24e16c21eb318bd9fa3eca16efb1c
Friedrich Wiemer <friedrich.wiemer@rub.de>
2018-02-16 12:11:57 +0100

    add a comment to remember that this is only a hack

--- src/sage/crypto/boolean_function.pyx
+++ src/sage/crypto/boolean_function.pyx
@@ -499,6 +499,7 @@ cdef class BooleanFunction(SageObject):
         bitset_init(anf, (1<<self._nvariables))
         bitset_copy(anf, self._truth_table)
         reed_muller(anf.bits, ZZ(anf.limbs).exact_log(2))
+        # TODO: this is a hack, introduced in #24748, because BooleanPolynomialRing leaks memory
         from sage.rings.polynomial.pbori import BooleanPolynomialRing_constructor
         R = BooleanPolynomialRing_constructor(self._nvariables,"x")
         G = R.gens()
@@ -963,6 +964,7 @@ cdef class BooleanFunction(SageObject):
             {0}
         """
         # NOTE: this is a toy implementation
+        # TODO: this is a hack, introduced in #14549, because BooleanPolynomialRing leaks memory
         from sage.rings.polynomial.polynomial_ring_constructor import BooleanPolynomialRing_constructor
         R = BooleanPolynomialRing_constructor(self._nvariables,'x')
         G = R.gens()
