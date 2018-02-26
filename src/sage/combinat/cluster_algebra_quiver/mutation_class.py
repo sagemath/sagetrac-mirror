bf0e9502653e856c5201f86ec3b99a406adf190e
Frédéric Chapoton <chapoton@math.univ-lyon1.fr>
2018-02-26 13:54:11 +0100

    trac 22381 do not introduce arrows between frozen vertices

--- src/sage/combinat/cluster_algebra_quiver/mutation_class.py
+++ src/sage/combinat/cluster_algebra_quiver/mutation_class.py
@@ -11,29 +11,30 @@ AUTHORS:
 - Christian Stump
 """
 
-#*****************************************************************************
+# ****************************************************************************
 #       Copyright (C) 2011 Gregg Musiker <musiker@math.mit.edu>
 #                          Christian Stump <christian.stump@univie.ac.at>
 #
 #  Distributed under the terms of the GNU General Public License (GPL)
 #                  http://www.gnu.org/licenses/
-#*****************************************************************************
+# ****************************************************************************
 from __future__ import print_function
 from six.moves import range
 
 import time
-from sage.groups.perm_gps.partn_ref.refinement_graphs import *
+from sage.groups.perm_gps.partn_ref.refinement_graphs import search_tree, get_orbits
 from sage.rings.all import ZZ, infinity
 from sage.graphs.all import DiGraph
 from sage.combinat.cluster_algebra_quiver.quiver_mutation_type import _edge_list_to_matrix
 
+
 def _principal_part( mat ):
     """
-    Returns the principal part of a matrix.
+    Return the principal part of a matrix.
 
     INPUT:
 
-    - ``mat`` - a matrix with at least as many rows as columns
+    - ``mat`` -- a matrix with at least as many rows as columns
 
     OUTPUT:
 
@@ -50,17 +51,19 @@ def _principal_part( mat ):
         [1 2]
         [3 4]
     """
-    n, m = mat.ncols(), mat.nrows()-mat.ncols()
+    n, m = mat.ncols(), mat.nrows() - mat.ncols()
     if m < 0:
         raise ValueError('The input matrix has more columns than rows.')
     elif m == 0:
         return mat
     else:
-        return mat.submatrix(0,0,n,n)
+        return mat.submatrix(0, 0, n, n)
 
-def _digraph_mutate( dg, k, n, m ):
+
+def _digraph_mutate(dg, k, n, m):
     """
-    Returns a digraph obtained from dg with n+m vertices by mutating at vertex k.
+    Return a digraph obtained from ``dg`` with ``n+m`` vertices by
+    mutating at vertex ``k``.
 
     INPUT:
 
@@ -76,49 +79,64 @@ def _digraph_mutate( dg, k, n, m ):
         [(0, 1, (1, -1)), (2, 1, (1, -1)), (2, 3, (1, -1))]
         sage: _digraph_mutate(dg,2,4,0).edges()
         [(0, 1, (1, -1)), (1, 2, (1, -1)), (3, 2, (1, -1))]
+
+    TESTS::
+
+        sage: B = matrix(3,[0,1,-1])
+        sage: Q = ClusterQuiver(B)
+        sage: dg = Q.digraph()
+        sage: _digraph_mutate(dg,0,1,2).edges()
+        [(0, 1, (1, -1)), (2, 0, (1, -1))]
     """
-    edges = dict( ((v1,v2),label) for v1,v2,label in dg._backend.iterator_in_edges(dg,True) )
-    in_edges = [ (v1,v2,edges[(v1,v2)]) for (v1,v2) in edges if v2 == k ]
-    out_edges = [ (v1,v2,edges[(v1,v2)]) for (v1,v2) in edges if v1 == k ]
-    in_edges_new = [ (v2,v1,(-label[1],-label[0])) for (v1,v2,label) in in_edges ]
-    out_edges_new = [ (v2,v1,(-label[1],-label[0])) for (v1,v2,label) in out_edges ]
+    edges = {(v1, v2): label
+             for v1, v2, label in dg._backend.iterator_in_edges(dg, True)}
+    in_edges = [(v1, v2, edges[(v1, v2)]) for (v1, v2) in edges if v2 == k]
+    out_edges = [(v1, v2, edges[(v1, v2)]) for (v1, v2) in edges if v1 == k]
+    in_edges_new = [(v2, v1, (-label[1], -label[0]))
+                    for (v1, v2, label) in in_edges]
+    out_edges_new = [(v2, v1, (-label[1], -label[0]))
+                     for (v1, v2, label) in out_edges]
     diag_edges_new = []
     diag_edges_del = []
 
-    for (v1,v2,label1) in in_edges:
-        for (w1,w2,label2) in out_edges:
-            l11,l12 = label1
-            l21,l22 = label2
-            if (v1,w2) in edges:
-                diag_edges_del.append( (v1,w2,edges[(v1,w2)]) )
-                a,b = edges[(v1,w2)]
-                a,b = a+l11*l21, b-l12*l22
-                diag_edges_new.append( (v1,w2,(a,b)) )
-            elif (w2,v1) in edges:
-                diag_edges_del.append( (w2,v1,edges[(w2,v1)]) )
-                a,b = edges[(w2,v1)]
-                a,b = b+l11*l21, a-l12*l22
-                if a<0:
-                    diag_edges_new.append( (w2,v1,(b,a)) )
-                elif a>0:
-                    diag_edges_new.append( (v1,w2,(a,b)) )
+    for (v1, _, label1) in in_edges:
+        if v1 > n:
+            continue
+        l11, l12 = label1
+        for (_, w2, label2) in out_edges:
+            if w2 > n:
+                continue
+            l21, l22 = label2
+            if (v1, w2) in edges:
+                diag_edges_del.append((v1, w2, edges[(v1, w2)]))
+                a, b = edges[(v1, w2)]
+                a, b = a + l11 * l21, b - l12 * l22
+                diag_edges_new.append((v1, w2, (a, b)))
+            elif (w2, v1) in edges:
+                diag_edges_del.append((w2, v1, edges[(w2, v1)]))
+                a, b = edges[(w2, v1)]
+                a, b = b + l11 * l21, a - l12 * l22
+                if a < 0:
+                    diag_edges_new.append((w2, v1, (b, a)))
+                elif a > 0:
+                    diag_edges_new.append((v1, w2, (a, b)))
             else:
-                a,b = l11*l21,-l12*l22
-                diag_edges_new.append( (v1,w2,(a,b)) )
+                a, b = l11 * l21, -l12 * l22
+                diag_edges_new.append((v1, w2, (a, b)))
 
     del_edges = in_edges + out_edges + diag_edges_del
     new_edges = in_edges_new + out_edges_new + diag_edges_new
-    new_edges += [ (v1,v2,edges[(v1,v2)]) for (v1,v2) in edges if not (v1,v2,edges[(v1,v2)]) in del_edges ]
+    new_edges += [(v1, v2, edges[(v1, v2)])
+                  for (v1, v2) in edges
+                  if not (v1, v2, edges[(v1, v2)]) in del_edges]
 
     dg_new = DiGraph()
-    for v1,v2,label in new_edges:
-        dg_new._backend.add_edge(v1,v2,label,True)
-    if dg_new.order() < n+m:
-        dg_new_vertices = [ v for v in dg_new ]
-        for i in [ v for v in dg if v not in dg_new_vertices ]:
-            dg_new.add_vertex(i)
+    dg_new.add_vertices(range(n + m))
+    for v1, v2, label in new_edges:
+        dg_new._backend.add_edge(v1, v2, label, True)
 
     return dg_new
+
 
 def _matrix_to_digraph( M ):
     """
@@ -134,9 +152,11 @@ def _matrix_to_digraph( M ):
     n = M.ncols()
 
     dg = DiGraph(sparse=True)
-    for i,j in M.nonzero_positions():
-        if i >= n: a,b = M[i,j],-M[i,j]
-        else: a,b = M[i,j],M[j,i]
+    for i, j in M.nonzero_positions():
+        if i >= n:
+            a, b = M[i, j], -M[i, j]
+        else:
+            a, b = M[i, j], M[j, i]
         if a > 0:
             dg._backend.add_edge(i,j,(a,b),True)
         elif i >= n:
@@ -145,6 +165,7 @@ def _matrix_to_digraph( M ):
         for i in [ index for index in range(M.nrows()) if index not in dg ]:
             dg.add_vertex(i)
     return dg
+
 
 def _dg_canonical_form( dg, n, m ):
     """
@@ -188,6 +209,7 @@ def _dg_canonical_form( dg, n, m ):
                 index += 1
     dg._backend.relabel( iso, True )
     return iso, orbits
+
 
 def _mutation_class_iter( dg, n, m, depth=infinity, return_dig6=False, show_depth=False, up_to_equivalence=True, sink_source=False ):
     """
@@ -287,6 +309,7 @@ def _mutation_class_iter( dg, n, m, depth=infinity, return_dig6=False, show_dept
             nr += ' ' * (10-len(nr))
             print("Depth: %s found: %s Time: %.2f s" % (dc, nr, timer2 - timer))
 
+
 def _digraph_to_dig6( dg, hashable=False ):
     """
     Returns the dig6 and edge data of the digraph dg.
@@ -312,6 +335,7 @@ def _digraph_to_dig6( dg, hashable=False ):
     if hashable:
         D = tuple( sorted( D.items() ) )
     return (dig6,D)
+
 
 def _dig6_to_digraph( dig6 ):
     """
@@ -344,6 +368,7 @@ def _dig6_to_digraph( dig6 ):
             dg.set_edge_label( edge[0],edge[1], (1,-1) )
     return dg
 
+
 def _dig6_to_matrix( dig6 ):
     """
     Return the matrix obtained from the dig6 and edge data.
@@ -367,6 +392,7 @@ def _dig6_to_matrix( dig6 ):
     """
     dg = _dig6_to_digraph(dig6)
     return _edge_list_to_matrix(dg.edges(), list(range(dg.order())), [])
+
 
 def _dg_is_sink_source( dg, v ):
     """
@@ -392,6 +418,7 @@ def _dg_is_sink_source( dg, v ):
     in_edges = [ edge for edge in dg._backend.iterator_in_edges([v],True) ]
     out_edges = [ edge for edge in dg._backend.iterator_out_edges([v],True) ]
     return not ( in_edges and out_edges )
+
 
 def _graph_without_edge_labels(dg,vertices):
     """
@@ -430,6 +457,7 @@ def _graph_without_edge_labels(dg,vertices):
             dg._backend.del_edge(u,v,l,True)
     return [a for a in edge_partition if a != []], edge_labels
 
+
 def _has_two_cycles( dg ):
     """
     Returns True if the input digraph has a 2-cycle and False otherwise.
@@ -448,6 +476,7 @@ def _has_two_cycles( dg ):
         if (w,v) in edge_set:
             return True
     return False
+
 
 def _is_valid_digraph_edge_set( edges, frozen=0 ):
     """
