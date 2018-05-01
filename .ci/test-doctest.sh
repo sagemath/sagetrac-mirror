751479648b73b21e18a148e95d833d8a0a5cc68d
Julian Rüth <julian.rueth@fsfe.org>
2018-05-01 18:59:30 +0200

    Run --new doctests to get faster failures

--- .ci/test-doctest.sh
+++ .ci/test-doctest.sh
@@ -2,7 +2,7 @@
 
 # This script gets called from CI to run doctests in the sagemath build
 
-# Usage: ./test-doctest.sh IMAGE-NAME
+# Usage: ./test-doctest.sh IMAGE-NAME --new|--short|--long
 
 # ****************************************************************************
 #       Copyright (C) 2018 Julian Rüth <julian.rueth@fsfe.org>
@@ -16,8 +16,26 @@
 
 set -ex
 
+case "$2" in
+    --new)
+        # Try to go back to the latest commit by the release manager
+        git reset `git log --author release@sagemath.org -1 --format=%H` || true
+        export DOCTEST_PARAMETERS="--new"
+        ;;
+    --short)
+        # TODO: Upgrade this with https://trac.sagemath.org/ticket/25270
+        export DOCTEST_PARAMETERS="--all"
+        ;;
+    --long)
+        export DOCTEST_PARAMETERS="--long"
+        ;;
+    *)
+        exit 1
+        ;;
+esac
+
 # Run tests once, and then try the failing files twice to work around flaky doctests.
-docker run --entrypoint sh -e DOCTEST_PARAMETERS "$1" -c 'sage -tp --all $DOCTEST_PARAMETERS ||
-                                                          sage -tp --all $DOCTEST_PARAMETERS --failed ||
-                                                          sage -tp --all $DOCTEST_PARAMETERS --failed'
+docker run --entrypoint sh -e DOCTEST_PARAMETERS "$1" -c 'sage -tp $DOCTEST_PARAMETERS ||
+                                                               sage -tp --failed $DOCTEST_PARAMETERS ||
+                                                               sage -tp --failed $DOCTEST_PARAMETERS'
 
