59e3542dee947723e47f62d14f8a7f2a538a5633
Simon Brandhorst <sbrandhorst@web.de>
2018-06-08 21:38:15 +0200

    ..

--- src/sage/quadratic_forms/genera/genus.py
+++ src/sage/quadratic_forms/genera/genus.py
@@ -1520,7 +1520,7 @@ class Genus_Symbol_p_adic_ring(object):
         r"""
         Return the locally automorphous square classes at this prime.
 
-        See [CS]§9.6
+        See [CS]_ 9.6
 
         EXAMPLES::
 
@@ -2289,6 +2289,7 @@ class GenusSymbol_global_ring(object):
 
     def rank(self):
         r"""
+        The rank of a representative of this genus.
         """
         return self._local_symbols[0].rank()
 
