00caa3631fff3ea1f3bee700ad9c4da9a48a0642
Dario Asprone <aspronedario@gmail.com>
2018-08-01 10:57:26 +0200

    Reduced fingerprint creation overhead

--- src/sage/graphs/weisfeiler_lehman/weisfeiler_lehman.cpp
+++ src/sage/graphs/weisfeiler_lehman/weisfeiler_lehman.cpp
@@ -10,9 +10,11 @@
 #include <algorithm>
 #include <tuple>
 #include <cstring>
+#include <chrono>
 #include <exception>
 #include <cassert>
 namespace wl{
+        static unsigned long long t_e;
         template<typename T>
         class AdjMatrix{
                 public:
@@ -274,6 +276,7 @@ namespace wl{
         
         using ColorClass = std::unordered_set<std::pair<int,int>, IntPair_Hash>;
         using FingerprintMap = std::unordered_map<AdjMatrix<int>::VectorView, int, VectorView_Hash>;
+        using Fingerprint = std::map<int, int>;
         int initColorClasses(queue<ColorClass>& cc, const AdjMatrix<int>& am){
             int n = am.size();
             unordered_map<int, ColorClass> temp;
@@ -288,37 +291,38 @@ namespace wl{
             }
             return c;
         }
-        void initFingerprint(FingerprintMap& fingerprint, const AdjMatrix<int>& am, int maxVertex, int offset, int limit, int* currentTuple, unordered_set<int>& used_vertices){
+        void initFingerprint(FingerprintMap& fingerprints, const AdjMatrix<int>& am, int maxVertex, int offset, int limit, int* currentTuple, unordered_set<int>& used_vertices){
             int n = am.size();
             if(offset == limit){ //If a subgraph is completed
                 for(const auto& i: used_vertices){
                     for(const auto& j: used_vertices){
                         AdjMatrix<int>::VectorView subgraphView(am, am.getCanonicalOrdering(currentTuple, limit, n, i,j), limit, {i,j});
-                        fingerprint[subgraphView] = 0;
+                        auto& f = fingerprints[subgraphView];
+                        if(f == 0) f = fingerprints.size();
                     }
                 }
             }else{            
                 for(int k = maxVertex; k < n; k++){
                     currentTuple[offset] = k;
                     bool inserted = used_vertices.insert(k).second;
-                    initFingerprint(fingerprint, am, k, offset+1, limit, currentTuple, used_vertices);
+                    initFingerprint(fingerprints, am, k, offset+1, limit, currentTuple, used_vertices);
                     if(inserted) used_vertices.erase(k);
                 }
             }
         }
-        void createFingerprint(FingerprintMap& fingerprint, const AdjMatrix<int>& am, int i, int j, int maxVertex, int offset, int limit, int* currentTuple, bool usedI=false, bool usedJ=false){
+        void createFingerprint(FingerprintMap& fingerprints, Fingerprint& fingerprint, const AdjMatrix<int>& am, int i, int j, int maxVertex, int offset, int limit, int* currentTuple, bool usedI=false, bool usedJ=false){
             if(offset > limit-2 && !usedI && !usedJ) return;
             if((maxVertex > i && !usedI) || (maxVertex > j && !usedJ)) return;
             int n = am.size();
             if(offset == limit){ //If a subgraph is completed
                 if(!usedI || !usedJ) return;
                 AdjMatrix<int>::VectorView subgraphView(am, am.getCanonicalOrdering(currentTuple, limit, n, i,j), limit, {i,j});
-                fingerprint[subgraphView]++;
+                fingerprint[fingerprints[subgraphView]]++;
                 if(i > 8000) subgraphView.printVectorView();
             }else{            
                 for(int k = maxVertex; k < n; k++){
                     currentTuple[offset] = k;
-                    createFingerprint(fingerprint, am, i, j, k, offset+1, limit, currentTuple, k == i?true:usedI, (k == j && (i!=j || usedI))?true:usedJ);
+                    createFingerprint(fingerprints, fingerprint, am, i, j, k, offset+1, limit, currentTuple, k == i?true:usedI, (k == j && (i!=j || usedI))?true:usedJ);
                 }
             }
             
@@ -339,8 +343,7 @@ namespace wl{
                 el.first.printVectorView();
                 cout << endl << endl;
             }
-        }
-        
+        }    
         unordered_map<int, vector<pair<int,int>>> k_WL(const std::vector<GraphNode>& v, int k, bool hasVertexLabels){
             //for(const auto& el: v){
                 //cout << "IDX: " << el.idx << " || COLOR: " << el.color << endl;
@@ -357,31 +360,34 @@ namespace wl{
             initColorClasses(color_classes, adjMatrix);
             queue<ColorClass> new_color_classes;
             unordered_set<int> used_vertices;
+            map<int,int> fingerprint;
             bool finished = false;
             while(!finished){
                     finished = true;
-                    FingerprintMap fingerprint;
+                    FingerprintMap fingerprint_map;
                     int* tempVector = new int[k+1];
-                    unordered_map<Tuple<int>, ColorClass> fingerprintsDB;
-                    initFingerprint(fingerprint, adjMatrix, 0, 0, k+1, tempVector, used_vertices);
+                    unordered_map<vector<int>, ColorClass, IntVector_Hash> fingerprintsDB;
+                    initFingerprint(fingerprint_map, adjMatrix, 0, 0, k+1, tempVector, used_vertices);
                     int c = 0;
                     while(!color_classes.empty()){
                         auto cc = color_classes.front();
                         color_classes.pop();
                         fingerprintsDB.clear();
                         //printFingerprint(fingerprint);
-                        
                         for(const auto& edge: cc){
-                            int s = clearFingerprint(fingerprint);
-                            createFingerprint(fingerprint, adjMatrix, edge.first, edge.second, 0, 0, k+1, tempVector);
-                            assert(s == (int)fingerprint.size());
-                            Tuple<int> t(s);
-                            int c = 0;
+                            /*std::chrono::steady_clock::time_point begin = std::chrono::steady_clock::now();
+                            std::chrono::steady_clock::time_point end= std::chrono::steady_clock::now();
+                            t_e += std::chrono::duration_cast<std::chrono::nanoseconds> (end - begin).count();
+                            */
+                            createFingerprint(fingerprint_map, fingerprint, adjMatrix, edge.first, edge.second, 0, 0, k+1, tempVector);
+                            vector<int> t(2*fingerprint.size());
+                            int idx = 0;
                             for(const auto& el: fingerprint){
-                                t[c++] = el.second;
+                                    t[idx++] = el.first;
+                                    t[idx++] = el.second;
                             }
-                            t.rehash();
-                            fingerprintsDB[t].insert(edge);
+                            fingerprintsDB[std::move(t)].insert(edge);
+                            fingerprint.clear();
                         }
                         int oldc = c;
                         for(auto& color_class: fingerprintsDB){
@@ -412,6 +418,7 @@ namespace wl{
                     //cout << "    (" << el2.first << ", " << el2.second << ")" << endl;
                 //}
             //}
+            cout << t_e << endl;
             return result;
         }
 
