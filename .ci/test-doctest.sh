666dffe87c202926e698dfededfc75a36285cf16
Julian Rüth <julian.rueth@fsfe.org>
2018-08-23 02:50:51 +0200

    Fix entrypoint for testing
    so we can inject scripts in a more standard way and without mkfifo foo.

--- .ci/test-doctest.sh
+++ .ci/test-doctest.sh
@@ -16,37 +16,25 @@
 
 set -ex
 
-# The image's entrypoints might not accept arbitrary shell scripts, so we feed
-# our commands into the container through a FIFO. (Note that we want the
-# entrypoints to run and not override the entrypoint to be sh as we might need
-# the setup steps performed by the entrypoint.)
-mkfifo stdin
-# Pipe everything that comes into the FIFO into our image (without closing the FIFO)
-(while true; do cat stdin; done) | docker run -i "$1" bash &
-# When this script exits the background job won't die, so the docker container
-# is going to keep running. Make sure the entitre process tree goes away.
-trap "exit" INT TERM
-trap "kill 0" EXIT
+SETUP=":"
 
 case "$2" in
     --new)
-        echo 'git reset `git log --author release@sagemath.org -1 --format=%H`' > stdin
-        export DOCTEST_PARAMETERS="--long --new"
+        SETUP='git reset `git log --author release@sagemath.org -1 --format=%H`'
+        DOCTEST_PARAMETERS="--long --new"
         ;;
     --short)
-        export DOCTEST_PARAMETERS="--short --all"
+        DOCTEST_PARAMETERS="--short --all"
         ;;
     --long)
-        export DOCTEST_PARAMETERS="--long --all"
+        DOCTEST_PARAMETERS="--long --all"
         ;;
     *)
         exit 1
         ;;
 esac
 
-# Run tests once, and then try the failing files twice to work around flaky doctests.
-echo <<EOF > stdin
-sage -tp $DOCTEST_PARAMETERS ||
-sage -tp --failed $DOCTEST_PARAMETERS ||
-sage -tp --failed $DOCTEST_PARAMETERS
-EOF
+docker run "$1" "$SETUP; \
+sage -tp $DOCTEST_PARAMETERS || \
+sage -tp --failed $DOCTEST_PARAMETERS || \
+sage -tp --failed $DOCTEST_PARAMETERS"
