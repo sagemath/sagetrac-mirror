5c5f09a5db193a160c0dc7ee9329efb7a4390c9b
Matthew Lancellotti <mareoraft@gmail.com>
2018-09-15 14:59:29 -0400

    tests passing

--- src/sage/combinat/skew_partition.py
+++ src/sage/combinat/skew_partition.py
@@ -166,7 +166,7 @@ from sage.graphs.digraph import DiGraph
 from sage.matrix.matrix_space import MatrixSpace
 
 from sage.combinat.combinat import CombinatorialElement
-from sage.combinat.partition import Partitions, _Partitions
+from sage.combinat.partition import Partition, Partitions, _Partitions, is_weakly_decreasing
 from sage.combinat.tableau import Tableaux
 from sage.combinat.composition import Compositions
 
@@ -742,6 +742,280 @@ class SkewPartition(CombinatorialElement):
         """
         return SkewPartition([x.conjugate() for x in self])
 
+    def is_symmetric(sp):
+        r""" A SkewPartition is *symmetric* if its inner and outer shapes are symmetric.
+
+        Return ``True`` if and only if the :class:`SkewPartition` ``sp`` is equal to its own conjugate.
+
+        EXAMPLES::
+
+            sage: SkewPartition([[], []]).is_symmetric()
+            True
+            sage: SkewPartition([[1], []]).is_symmetric()
+            True
+            sage: SkewPartition([[4, 3, 3, 1], [1]]).is_symmetric()
+            True
+            sage: SkewPartition([[4, 3, 3, 1], [1, 1]]).is_symmetric()
+            False
+            sage: SkewPartition([[5, 3, 3, 1], [2, 2]]).is_symmetric()
+            False
+
+        ..  SEEALSO::
+
+            :meth:`Partition.is_symmetric`
+        """
+        return sp == sp.conjugate()
+
+    def is_linked(sp):
+        r"""
+        A skew-shape ``sp`` is a *skew-linked diagram* if both the row-shape and column-shape of `sp` are partitions.
+
+        EXAMPLES:
+
+        Both row shape and column shape are valid::
+
+            sage: SkewPartition([[2, 1], [1]]).is_linked()
+            True
+
+        Valid row shape but invalid column shape::
+
+            sage: SkewPartition([[3, 2], [1]]).is_linked()
+            False
+
+        The empty skew-shape corresponds to empty (valid) partitions::
+
+            sage: SkewPartition([[], []]).is_linked()
+            True
+
+        ..  SEEALSO::
+
+            :meth:`row_lengths`, :meth:`column_lengths`
+        """
+        return is_weakly_decreasing(sp.row_lengths()) and is_weakly_decreasing(sp.column_lengths())
+
+    def right(sp, row_index):
+        r""" Given a SkewPartition and a 0-based row index, return the 0-based column index of the *rightmost* cell in the corresponding row.  (Section 2.1 of [LLMS2013]_)
+
+        EXAMPLES::
+
+            sage: SkewPartition([[4, 1], [2]]).right(0)
+            3
+
+            sage: SkewPartition([[4, 1], [2]]).right(1)
+            0
+
+        An input index that is out of the bounds of the skew partition will throw an error::
+
+            sage: SkewPartition([[4, 1], [2]]).right(2)
+            Traceback (most recent call last):
+            ...
+            IndexError: list index out of range
+
+        An in-bounds index where no cells exist will return ``None``::
+
+            sage: SkewPartition([[2, 1, 1, 1], [1, 1]]).right(1) == None
+            True
+
+        TESTS::
+
+            sage: sp = SkewPartition([[1], []])
+            sage: sp.right(0)
+            0
+
+            sage: sp = SkewPartition([[2], []])
+            sage: sp.right(0)
+            1
+
+            sage: sp = SkewPartition([[1, 1], []])
+            sage: sp.right(0)
+            0
+
+            sage: sp = SkewPartition([[2], [1]])
+            sage: sp.right(0)
+            1
+
+            sage: sp = SkewPartition([[2], [2]])
+            sage: sp.right(0) == None
+            True
+
+        ..  SEEALSO::
+
+            :meth:`left`, :meth:`top`, :meth:`bottom`
+        """
+        # first check to make sure the cell exists
+        if sp.row_lengths()[row_index] == 0:
+            return None
+        outer_row_lengths = sp.outer().to_list()
+        outer_row_length = outer_row_lengths[row_index]
+        col_index = outer_row_length - 1
+        return col_index
+
+    def left(sp, row_index):
+        r""" Given a SkewPartition and a 0-based row index, return the 0-based column index of the *leftmost* cell in the corresponding row.  (Section 2.1 of [LLMS2013]_)
+
+        EXAMPLES::
+
+            sage: SkewPartition([[4, 1], [2]]).left(0)
+            2
+
+            sage: SkewPartition([[4, 1], [2]]).left(1)
+            0
+
+        An input index that is out of the bounds of the skew partition will throw an error::
+
+            sage: SkewPartition([[4, 1], [2]]).left(2)
+            Traceback (most recent call last):
+            ...
+            IndexError: list index out of range
+
+        An in-bounds index where no cells exist will return ``None``::
+
+            sage: SkewPartition([[2, 1, 1, 1], [1, 1]]).left(1) == None
+            True
+
+        TESTS::
+
+            sage: sp = SkewPartition([[1], []])
+            sage: sp.left(0)
+            0
+
+            sage: sp = SkewPartition([[2], []])
+            sage: sp.left(0)
+            0
+
+            sage: sp = SkewPartition([[2], [1]])
+            sage: sp.left(0)
+            1
+
+            sage: sp = SkewPartition([[2], [2]])
+            sage: sp.left(0) == None
+            True
+
+        ..  SEEALSO::
+
+            :meth:`right`, :meth:`top`, :meth:`bottom`
+        """
+        # first check to make sure the cell exists
+        if sp.row_lengths()[row_index] == 0:
+            return None
+        outer_row_lengths = sp.outer().to_list()
+        inner_row_lengths = sp.inner().to_list()
+        if (row_index in range(len(outer_row_lengths))) and (row_index not in range(len(inner_row_lengths))):
+            inner_row_length = 0
+        else:
+            inner_row_length = inner_row_lengths[row_index]
+        col_index = inner_row_length
+        return col_index
+
+    def top(sp, col_index):
+        r""" Given a SkewPartition and a 0-based column index, return the 0-based row index of the *topmost* cell in the corresponding column.  (Section 2.1 of [LLMS2013]_)
+
+        EXAMPLES::
+
+            sage: SkewPartition([[4, 1, 1], [2]]).top(0)
+            2
+
+        An in-bounds col_index where no cells exist will return ``None``::
+
+            sage: SkewPartition([[4, 1, 1], [2]]).top(1) == None
+            True
+
+            sage: SkewPartition([[4, 1, 1], [2]]).top(2)
+            0
+
+            sage: SkewPartition([[4, 1, 1], [2]]).top(3)
+            0
+
+        A col_index that is out-of-bounds of the skew partition will throw an error::
+
+            sage: SkewPartition([[4, 1, 1], [2]]).top(4)
+            Traceback (most recent call last):
+            ...
+            IndexError: list index out of range
+
+        TESTS::
+
+            sage: sp = SkewPartition([[1], []])
+            sage: sp.top(0)
+            0
+
+            sage: sp = SkewPartition([[2], []])
+            sage: sp.top(0)
+            0
+
+            sage: sp = SkewPartition([[1, 1], []])
+            sage: sp.top(0)
+            1
+
+            sage: sp = SkewPartition([[1, 1], [1]])
+            sage: sp.top(0)
+            1
+
+            sage: sp = SkewPartition([[2], [2]])
+            sage: sp.top(0) == None
+            True
+
+        ..  SEEALSO::
+
+            :meth:`right`, :meth:`left`, :meth:`bottom`
+        """
+        return sp.conjugate().right(col_index)
+
+    def bottom(sp, col_index):
+        r""" Given a SkewPartition and a 0-based column index, return the 0-based row index of the *bottommost* cell in the corresponding column.  (Section 2.1 of [LLMS2013]_)
+
+        EXAMPLES::
+
+            sage: SkewPartition([[4, 1, 1], [2]]).bottom(0)
+            1
+
+        An in-bounds col_index where no cells exist will return ``None``::
+
+            sage: SkewPartition([[4, 1, 1], [2]]).bottom(1) == None
+            True
+
+            sage: SkewPartition([[4, 1, 1], [2]]).bottom(2)
+            0
+
+            sage: SkewPartition([[4, 1, 1], [2]]).bottom(3)
+            0
+
+        A col_index that is out-of-bounds of the skew partition will throw an error::
+
+            sage: SkewPartition([[4, 1, 1], [2]]).bottom(4)
+            Traceback (most recent call last):
+            ...
+            IndexError: list index out of range
+
+        TESTS::
+
+            sage: sp = SkewPartition([[1], []])
+            sage: sp.bottom(0)
+            0
+
+            sage: sp = SkewPartition([[2], []])
+            sage: sp.bottom(0)
+            0
+
+            sage: sp = SkewPartition([[1, 1], []])
+            sage: sp.bottom(0)
+            0
+
+            sage: sp = SkewPartition([[1, 1], [1]])
+            sage: sp.bottom(0)
+            1
+
+            sage: sp = SkewPartition([[2], [2]])
+            sage: sp.bottom(0) == None
+            True
+
+
+        ..  SEEALSO::
+
+            :meth:`right`, :meth:`left`, :meth:`top`
+        """
+        return sp.conjugate().left(col_index)
+
     def outer_corners(self):
         """
         Return a list of the outer corners of ``self``.
@@ -1084,6 +1358,78 @@ class SkewPartition(CombinatorialElement):
                             newstring = (i + 1, j)
                         G.add_edge(string, newstring)
         return G
+
+    def k_boundary_to_partition(sp, k=None, check=True):
+        r""" Given a ``k``-boundary ``sp`` (`k`-boundaries are a specific type of skew-shape), output the original partition whose `k`-boundary is ``sp``.
+
+        (For the definition of `k`-boundary, see Section 2.2 of [LLMS2013]_)
+
+        If ``check`` is set to ``True``, the program will assert that the skew-shape really is a `k`-boundary.
+
+        TODO: test
+
+        EXAMPLES::
+
+            sage: SkewPartition([[3, 2, 1], [2, 1]]).k_boundary_to_partition()
+            [3, 2, 1]
+
+        If you input a skew-partition that is not a k-boundary to begin with, it will error::
+
+            sage: SkewPartition([[3, 1], [2]]).k_boundary_to_partition(k=2)
+            Traceback (most recent call last):
+            ...
+            AssertionError
+
+        However, you may tell the method to *not* check that the input is a k-boundary::
+
+            sage: SkewPartition([[3, 1], [2]]).k_boundary_to_partition(check=False)
+            [3, 1]
+
+        ..  SEEALSO::
+
+            :meth:`is_k_boundary`, :meth:`outer`
+        """
+        if check:
+            assert sp.is_k_boundary(k)
+        return sp.outer()
+
+    def is_k_boundary(sp, k=None):
+        r""" Given a skew-shape ``sp`` and natural number ``k``, return ``True`` if and only if ``sp`` is a `k`-boundary.  (Section 2.2 of [LLMS2013]_)
+
+        Given a skew-shape ``sp`` *only*, return ``True`` if and only if there exists some `k` such that ``sp`` is a `k`-boundary.
+
+        TODO: test
+
+        EXAMPLES::
+
+            sage: SkewPartition([[3, 2, 1], [2, 1]]).is_k_boundary()
+            True
+            sage: SkewPartition([[3, 2, 1], [2, 1]]).is_k_boundary(1)
+            True
+            sage: SkewPartition([[3, 2, 1], [2, 1]]).is_k_boundary(2)
+            True
+            sage: SkewPartition([[3, 2, 1], [2, 1]]).is_k_boundary(3)
+            False
+
+        ..  SEEALSO::
+
+            :meth:`Partition.k_boundary`
+        """
+        if k is None:
+            max_hook_length = sp.outer().hook_length(0, 0)
+            return any(sp.is_k_boundary(k_star) for k_star in range(0, max_hook_length+1))
+        elif k == 0:
+            # the only valid 0-boundary is the empty shape
+            return sp.outer() == sp.inner()
+        else:
+            r"""We go down and left of each cell to create the only possible partition that could have led to this potential k-boundary
+
+            (Any other partition containing this skew_shape would necessarily have a northeast corner that the skew_shape does *not have*.  But in order for the skew-shape to be a k-boundary, it *must have* that northeast corner.)
+            """
+            l = sp.k_boundary_to_partition(check=False)
+            r"""now that we have the partition, we simply compute it's hook-length for each cell and verify that for each cell of values k or less, it appears in the sp"""
+            correct_k_boundary = l.k_boundary(k)
+            return sp == correct_k_boundary
 
     def quotient(self, k):
         """
@@ -1450,7 +1796,7 @@ class SkewPartitions(UniqueRepresentation, Parent):
 
     def from_row_and_column_length(self, rowL, colL):
         """
-        Construct a partition from its row lengths and column lengths.
+        Construct a skew-partition from its row lengths and column lengths.
 
         INPUT:
 
@@ -1460,7 +1806,7 @@ class SkewPartitions(UniqueRepresentation, Parent):
 
         OUTPUT:
 
-        - If it exists the unique skew-partitions with row lengths ``rowL``
+        - If it exists the unique skew-partition with row lengths ``rowL``
           and column lengths ``colL``.
         - Raise a ``ValueError`` if ``rowL`` and ``colL`` are not compatible.
 
@@ -1935,6 +2281,132 @@ class SkewPartitions_rowlengths(SkewPartitions):
         for sskp in SkewPartitions(row_lengths=self.co[:-1], overlap=self.overlap):
             for sp in self._from_row_lengths_aux(sskp, self.co[-2], self.co[-1], self.overlap):
                 yield self.element_class(self, sp)
+
+def row_shape_to_linked_skew_partitions(rs):
+    r""" Given a partition ``rs``, find all linked SkewPartitions whose row-shape is ``rs``.
+
+    EXAMPLES:
+
+    Note that [4, 2, 1] / [1, 1] is *not* linked and hence doesn't appear in the list below::
+
+        sage: from sage.combinat.skew_partition import row_shape_to_linked_skew_partitions
+        sage: row_shape_to_linked_skew_partitions(Partition([3, 1, 1]))
+        [[3, 1, 1] / [], [4, 1, 1] / [1], [5, 2, 1] / [2, 1]]
+
+    TESTS::
+
+        sage: p = []
+        sage: row_shape_to_linked_skew_partitions(p)
+        [[] / []]
+
+        sage: p = [7]
+        sage: row_shape_to_linked_skew_partitions(p)
+        [[7] / []]
+
+        sage: p = [1, 1]
+        sage: row_shape_to_linked_skew_partitions(p)
+        [[1, 1] / [], [2, 1] / [1]]
+
+        sage: p = [3, 2, 1]
+        sage: row_shape_to_linked_skew_partitions(p)
+        [[3, 2, 1] / [], [4, 2, 1] / [1], [5, 2, 1] / [2], [6, 3, 1] / [3, 1]]
+
+        sage: p = [2, 2, 2]
+        sage: row_shape_to_linked_skew_partitions(p)
+        [[2, 2, 2] / [], [4, 2, 2] / [2], [6, 4, 2] / [4, 2]]
+
+    ..  SEEALSO::
+
+        :meth:`is_linked`, :meth:`row_lengths`
+    """
+    def ptn_to_linked_things(p):
+        # tests:
+        # p = []
+        # a(ptn_to_linked_things(p), [[[],[]]])
+        # p = [7]
+        # a(ptn_to_linked_things(p), [[[7],[]]])
+        # p = [1, 1]
+        # a(ptn_to_linked_things(p), [[[1, 1],[]], [[2, 1],[1]]])
+        def thing_to_added_row_things(sp, row_len):
+            # tests:
+            # sp = SkewPartition([[1], []])
+            # a(thing_to_added_row_things(sp, 0), [[[1],[]]])
+            # sp = SkewPartition([[5, 2, 1], [2]])
+            # a(thing_to_added_row_things(sp, 0), [[[5, 2, 1], [2]]])
+            # sp = SkewPartition([[4, 3, 1], [1, 1]])
+            # a(thing_to_added_row_things(sp, 0), [])
+            # sp = SkewPartition([[1], []])
+            # a(thing_to_added_row_things(sp, 1), [[[1, 1],[]], [[2, 1],[1]]])
+            # sp = SkewPartition([[2], []])
+            # a(thing_to_added_row_things(sp, 2), [[[2, 2],[]], [[3, 2],[1]], [[4, 2],[2]]])
+            def add_row(sp, row_len, offset):
+                # add the next row onto the skew shape
+                #
+                # tests:
+                # sp = SkewPartition([[4, 3, 1], [1, 1]])
+                # a(add_row(sp, 1, 2), [[6, 5, 3, 1], [3, 3, 2]])
+                outer = sp.outer().to_list()
+                inner = sp.inner().to_list()
+                inner += [0] * (len(outer) - len(inner))
+                outer = [e + offset for e in outer]
+                inner = [e + offset for e in inner]
+                outer.append(row_len)
+                return SkewPartition([outer, inner])
+            # START thing_to_added_row_things
+            previous_checked_col_index = sp.outer()[-1]
+            # find the maximum leftmost offset for the new row
+            col_lens = sp.column_lengths()
+            max_offset = row_len
+            prev_col_len = 0
+            for col_index in range(previous_checked_col_index, -1, -1):
+                # get length of column
+                col_len = col_lens[col_index] if col_index < len(col_lens) else 0
+                # check col-shape partition condition
+                if col_len >= prev_col_len:
+                    # col_index is good, continue
+                    prev_col_len = col_len
+                else:
+                    # col_index is bad, stop
+                    good_col_index = col_index + 1
+                    max_offset = row_len - good_col_index
+                    break
+            # now add all possible positions for the row onto the list
+            return [add_row(sp, row_len, offset) for offset in range(0, max_offset+1)]
+        # START ptn_to_linked_things
+        assert isinstance(p, list) and not isinstance(p, Partition)
+        if len(p) <= 1:
+            return [SkewPartition([p, []])]
+        else:
+            # these incomplete guys are not necessarily skew partitions
+            incomplete_things = ptn_to_linked_things(p[:-1])
+            almost_complete_things = []
+            for incomplete_thing in incomplete_things:
+                almost_complete_things += thing_to_added_row_things(
+                    incomplete_thing, p[-1])
+            return almost_complete_things
+    # START row_shape_to_linked_skew_partitions
+    rs_zero = list(Partition(rs)) + [0]
+    return ptn_to_linked_things(rs_zero)
+
+def size_to_linked_skew_partitions(size):
+    r""" Given a natural number ``size``, return all linked SkewPartitions of size ``size``.
+
+    EXAMPLES::
+
+        sage: from sage.combinat.skew_partition import size_to_linked_skew_partitions
+        sage: size_to_linked_skew_partitions(3)
+        [[3] / [], [2, 1] / [], [3, 1] / [1], [1, 1, 1] / [], [2, 1, 1] / [1], [3, 2, 1] / [2, 1]]
+
+    ..  SEEALSO::
+
+        :meth:`is_linked`, :meth:`size`
+    """
+    linked_skew_ptns = []
+    # Here is one major optimization that's possible: Instead of first calculating all Partitions(size), and then doing the ptn_to_linked_things algo for each partition, actually go through the work of generating the partitions manually, and use ptn_to_linked_things algo as you go.  This is to ELIMINATE the redundancy of having two partitions that START with the same sub-partition.
+    ptns = Partitions(size)
+    for ptn in ptns:
+        linked_skew_ptns += row_shape_to_linked_skew_partitions(ptn)
+    return linked_skew_ptns
 
 from sage.misc.persist import register_unpickle_override
 register_unpickle_override('sage.combinat.skew_partition', 'SkewPartition_class', SkewPartition)
