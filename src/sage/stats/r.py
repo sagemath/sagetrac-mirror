db4d8bb7795a9014421d98585c7bd7d72cb5e964
Timo Kaufmann <timokau@zoho.com>
2018-10-30 00:12:50 +0100

    Replace expect r interface with rpy

--- src/sage/stats/r.py
+++ src/sage/stats/r.py
@@ -39,8 +39,8 @@ def ttest(x,y,conf_level = 0.95, **kw):
 
    Example::
 
-      sage: a, b = ttest([1,2,3,4,5],[1,2,3,3.5,5.121]); a
-      0.941026372027427
+      sage: a, b = ttest([1,2,3,4,5],[1,2,3,3.5,5.121]); a # abs tol 1e-12
+      0.9410263720274274
    """
    if len(x) != len(y):
       raise AttributeError("vectors x and y must be of same length")
