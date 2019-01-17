70a2c6e861bae18d808742c5d258cf40a1e52a1a
Frédéric Chapoton <chapoton@unistra.fr>
2019-01-17 17:54:32 +0100

    adding some doctests, more to do

--- src/sage/combinat/designs/all.py
+++ src/sage/combinat/designs/all.py
@@ -1,5 +1,21 @@
 """
 Combinatorial design features that are imported by default in the interpreter namespace
+
+Test for deprecations of imports into global namespace::
+
+    sage: designs_from_XML
+    doctest:warning...:
+    DeprecationWarning: 
+    Importing designs_from_XML from here is deprecated. If you need to use it, please import it directly from sage.combinat.designs.ext_rep
+    See https://trac.sagemath.org/27066 for details.
+    ...
+
+    sage: designs_from_XML_url
+    doctest:warning...:
+    DeprecationWarning: 
+    Importing designs_from_XML_url from here is deprecated. If you need to use it, please import it directly from sage.combinat.designs.ext_rep
+    See https://trac.sagemath.org/27066 for details.
+    ...
 """
 from __future__ import absolute_import
 
