0dc50c4827e925302652efe4d154f47b1dd97dcb
Baldur Sigurðsson <baldursigurds@gmail.com>
2019-02-28 13:39:17 -0600

    Revert "Merge branch 'u/gh-baldursigurds/plumbing_graph' of git://trac.sagemath.org/sage into t/26945/plumbing_graph"
    This reverts commit 2331ef812716cbb3eff67cc08628e359ac7be1eb, reversing
    changes made to 4f6074a3f3011b8333361f83d9fb1d69efadf205.
    
    this merge shouldn't have happened, my bad

--- src/sage/geometry/all.py
+++ src/sage/geometry/all.py
@@ -24,10 +24,7 @@ from .hyperbolic_space.all import *
 
 from .voronoi_diagram import VoronoiDiagram
 
-lazy_import('sage.geometry.plumbing_graph', ['PlumbingGraph', 'genus_hash', 'big_genus_hash'])
-
-lazy_import('sage.geometry.ribbon_graph', ['RibbonGraph'])
-lazy_import('sage.geometry.tat_graph', ['TatGraph', 'safewalk', 'check_tat_property', 'bipartite_tat_graph', 'blow_up'])
+lazy_import('sage.geometry.ribbon_graph', 'RibbonGraph')
 lazy_import('sage.geometry.hyperplane_arrangement.arrangement', 'HyperplaneArrangements')
 lazy_import('sage.geometry.hyperplane_arrangement.library', 'hyperplane_arrangements')
 
