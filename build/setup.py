8e44d282055d235d0ebc52327516e7c5bdde81de
Matthias Koeppe <mkoeppe@math.ucdavis.edu>
2020-06-30 23:42:45 -0700

    build/setup.py: Install sage_conf.py, sage-env-config

--- build/setup.py
+++ build/setup.py
@@ -1,19 +1,19 @@
 #!/usr/bin/env sage-bootstrap-python
 
+import os
+import sys
+import shutil
+
 from distutils.core import setup
 from distutils.cmd import Command
-from distutils.command.install import install as distutils_install
+from distutils.command.build_scripts import build_scripts as distutils_build_scripts
+from distutils.command.build_py import build_py as distutils_build_py
 from distutils.errors import (DistutilsSetupError, DistutilsModuleError,
                               DistutilsOptionError)
 
-class install(distutils_install):
+class build_py(distutils_build_py):
 
     def run(self):
-        import os
-        import sys
-        import shutil
-        distutils_install.run(self)
-
         DOT_SAGE = os.environ.get('DOT_SAGE', os.path.join(os.environ.get('HOME'), '.sage'))
         # config.status and other configure output has to be writable.
         SAGE_ROOT = os.path.join(DOT_SAGE, 'sage-{}'.format(self.distribution.version))
@@ -26,7 +26,27 @@ class install(distutils_install):
             print("Running {}".format(cmd))
             if os.system(cmd) != 0:
                 raise DistutilsSetupError("configure failed")
-        # TODO: Store SAGE_LOCAL in a module sage_bootstrap.config
+
+        # Install configuration
+        shutil.copyfile(os.path.join(SAGE_ROOT, 'build', 'pkgs', 'sage_conf', 'src', 'sage_conf.py'),
+                        'sage_conf.py')
+        if not self.distribution.py_modules:
+            self.py_modules = self.distribution.py_modules = []
+        self.distribution.py_modules.append('sage_conf')
+        shutil.copyfile(os.path.join(SAGE_ROOT, 'src', 'bin', 'sage-env-config'),
+                        os.path.join('bin', 'sage-env-config'))
+        distutils_build_py.run(self)
+
+class build_scripts(distutils_build_scripts):
+
+    def run(self):
+        self.distribution.scripts.append(os.path.join('bin', 'sage-env-config'))
+        if not self.distribution.entry_points:
+            self.entry_points = self.distribution.entry_points = dict()
+        if 'console_scripts' not in self.distribution.entry_points:
+            self.distribution.entry_points['console_scripts'] = []
+        self.distribution.entry_points['console_scripts'].append('sage-config=sage_conf:_main')
+        distutils_build_scripts.run(self)
 
 setup(
     name='sage_bootstrap',
@@ -44,5 +64,5 @@ setup(
     },
     version='1.0',
     url='https://www.sagemath.org',
-    cmdclass=dict(install=install)
+    cmdclass=dict(build_py=build_py, build_scripts=build_scripts)
 )
