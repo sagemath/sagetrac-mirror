sagemath/sagetrac-mirror

commit 45b175e43621795f890204a9cdfb30b524f961fa

Don't use the string evaluation construction for libgap permutation groups.
Instead just pass the generators directly to libgap.Group, which should
vastly reduce string evaluation overhead (the individual generators may
or may not use string evaluation for their own conversions to libgap but
this is still better than using it for the entire group).

src/sage/groups/perm_gps/permgroup.py
@@ -600,7 +600,7 @@ class PermutationGroup_generic(FiniteGroup):
         if self._libgap is not None:
             return self._libgap
 
-        return super(PermutationGroup_generic, self)._libgap_()
+        return libgap.Group(self.gens())
 
     # Override the default _libgap_ to use the caching as self._libgap
     _libgap_ = gap

commit c9160c10e033565c1e2b538cb55f89f4c518ae72

Further rework of 45b175e43621795f890204a9cdfb30b524f961fa which introduced some regressions.
PermutationGroup_generic._libgap is gone: We add a _libgap_
implementation (as a faster alternative to the existing _gap_init_)
and make it a cached_method.  If the user provides a gap_group argument
we simply use it to set the value of the cached_method.

The .gap() method now simply wraps ._libgap_().  To speed up creation
of SymmetricGroup and AlternatingGroup we also give them _libgap_
methods which are also cached as in the base class.

src/sage/groups/perm_gps/permgroup.py
@@ -376,10 +376,11 @@ class PermutationGroup_generic(FiniteGroup):
 
         INPUT:
 
-        -  ``gens`` - list of generators (default: ``None``)
+        -  ``gens`` - list of generators; if not given, the gap_group argument
+           must be provided (default: ``None``)
 
         -  ``gap_group`` - a gap or libgap permutation group, or a string
-          defining one (default: ``None``)
+           defining one (default: ``None``)
 
         -  ``canonicalize`` - bool (default: ``True``); if ``True``,
            sort generators and remove duplicates
@@ -421,14 +422,19 @@ class PermutationGroup_generic(FiniteGroup):
             # defining the GAP group from the original construction information
             # helps to avoid buffer size overflow since it avoids transferring
             # lists of long generators (example PSp(8,3), see :trac:`26750`
-            self._libgap = libgap.eval(gap_group)
-            gap_group = self._libgap
+            gap_group = libgap.eval(gap_group)
 
         if isinstance(gap_group, LibGapElement):
-            self._libgap = gap_group
+            self._libgap_.set_cache(gap_group)
 
         #Handle the case where only the GAP group is specified.
         if gens is None:
+            if not isinstance(gap_group, (GapElement, LibGapElement)):
+                raise ValueError(
+                    'the gap_group argument must be provided either a'
+                    'GapElement or a valid string for constructing the group '
+                    'with GAP if gens are not provided')
+
             gens = [gen for gen in gap_group.GeneratorsOfGroup()]
 
         if domain is None:
@@ -464,8 +470,6 @@ class PermutationGroup_generic(FiniteGroup):
         if canonicalize:
             gens = sorted(set(gens))
         self._gens = gens
-
-    _libgap = None
 
     def construction(self):
         """
@@ -551,15 +555,36 @@ class PermutationGroup_generic(FiniteGroup):
         return 'Group([%s])'%(', '.join([g._gap_init_() for g in self.gens()]))
 
     @cached_method
+    def _libgap_(self, gap=None):
+        """
+        Returns a :class:`~sage.libs.gap.element.GapElement` representing the
+        group.
+
+        The return value is cached, and may be overridden when initializing
+        this class with the ``gap_group`` argument.
+
+        EXAMPLES:
+
+        See the examples in :meth:`~PermutationGroup_generic.gap`, which wraps
+        this method.
+        """
+
+        if gap is None:
+            gap = libgap
+
+        return libgap.Group(self.gens())
+
     def gap(self):
         r"""
-        this method from :class:`sage.groups.libgap_wrapper.ParentLibGAP` is added in order to achieve
-        compatibility and have :class:`sage.groups.libgap_morphism.GroupHomset_libgap` work for permutation
-        groups, as well
+        this method from :class:`sage.groups.libgap_wrapper.ParentLibGAP` is
+        added in order to achieve compatibility and have
+        :class:`sage.groups.libgap_morphism.GroupHomset_libgap` work for
+        permutation groups, as well
 
         OUTPUT:
 
-        an instance of :class:`sage.libs.gap.element.GapElement` representing this group
+        an instance of :class:`sage.libs.gap.element.GapElement` representing
+        this group
 
         EXAMPLES::
 
@@ -597,13 +622,7 @@ class PermutationGroup_generic(FiniteGroup):
             sage: UPG.gap()
             <permutation group with 5 generators>
         """
-        if self._libgap is not None:
-            return self._libgap
-
-        return libgap.Group(self.gens())
-
-    # Override the default _libgap_ to use the caching as self._libgap
-    _libgap_ = gap
+        return self._libgap_()
 
     def _Hom_(self, G, category=None, check=True):
         r"""
@@ -1684,7 +1703,7 @@ class PermutationGroup_generic(FiniteGroup):
             sage: G.stabilizer(10)
             Subgroup generated by [(2,3,4), (1,2)(3,4)] of (Permutation Group with generators [(1,2)(3,4), (1,2,3,4,10)])
             sage: G.stabilizer(1)
-            Subgroup generated by [(2,3)(4,10), (2,10,3)] of (Permutation Group with generators [(1,2)(3,4), (1,2,3,4,10)])
+            Subgroup generated by [(2,3)(4,10), (2,10,4)] of (Permutation Group with generators [(1,2)(3,4), (1,2,3,4,10)])
             sage: G = PermutationGroup([[(2,3,4)],[(6,7)]])
             sage: G.stabilizer(1)
             Subgroup generated by [(6,7), (2,3,4)] of (Permutation Group with generators [(6,7), (2,3,4)])
@@ -1839,8 +1858,10 @@ class PermutationGroup_generic(FiniteGroup):
             [[()], [()], [(), (3,4), (3,5,4)], [(), (4,5)], [()], [()], [(), (7,8)], [()]]
             sage: G = PermutationGroup([[(1,2,3,4)],[(1,2)]])
             sage: G.strong_generating_system()
-            [[(), (1,2)(3,4), (1,3)(2,4), (1,4)(2,3)],
-             [(), (2,4), (2,3,4)], [(), (3,4)], [()]]
+                [[(), (1,2)(3,4), (1,3)(2,4), (1,4)(2,3)],
+                 [(), (2,4,3), (2,3,4)],
+                 [(), (3,4)],
+                 [()]]
             sage: G = PermutationGroup([[(1,2,3)],[(4,5,7)],[(1,4,6)]])
             sage: G.strong_generating_system()
             [[(), (1,2,3), (1,4,6), (1,3,2), (1,5,7,4,6), (1,6,4), (1,7,5,4,6)],

src/sage/groups/perm_gps/permgroup_named.py
@@ -277,6 +277,28 @@ class SymmetricGroup(PermutationGroup_symalt):
         self._gens = [self.element_class(g, self, check=False)
                       for g in gens]
 
+    @cached_method
+    def _libgap_(self, gap=None):
+        """
+        Return the libgap representation (as a `~sage.libs.gap.element.GapElement`) of this
+        group.
+
+        EXAMPLES::
+
+            sage: S = SymmetricGroup(3)
+            sage: S._libgap_()
+            Sym( [ 1 .. 3 ] )
+            sage: type(_)
+            <class 'sage.libs.gap.element.GapElement'>
+            sage: S = SymmetricGroup(['a', 'b', 'c'])
+            sage: S._libgap_()
+            Sym( [ 1 .. 3 ] )
+        """
+        if gap is None:
+            gap = libgap
+
+        return gap.SymmetricGroup(self.degree())
+
     def _gap_init_(self, gap=None):
         """
         Return the string used to create this group in GAP.
@@ -670,6 +692,28 @@ class AlternatingGroup(PermutationGroup_symalt):
         """
         return "Alternating group of order %s!/2 as a permutation group"%self.degree()
 
+    @cached_method
+    def _libgap_(self, gap=None):
+        """
+        Return the libgap representation (as a `~sage.libs.gap.element.GapElement`) of this
+        group.
+
+        EXAMPLES::
+
+            sage: A = AlternatingGroup(3)
+            sage: A._libgap_()
+            Alt( [ 1 .. 3 ] )
+            sage: type(_)
+            <class 'sage.libs.gap.element.GapElement'>
+            sage: A = AlternatingGroup(['a', 'b', 'c'])
+            sage: A._libgap_()
+            Alt( [ 1 .. 3 ] )
+        """
+        if gap is None:
+            gap = libgap
+
+        return gap.AlternatingGroup(self.degree())
+
     def _gap_init_(self, gap=None):
         """
         Returns the string used to create this group in GAP.
@@ -684,6 +728,7 @@ class AlternatingGroup(PermutationGroup_symalt):
             'AlternatingGroup(3)'
         """
         return 'AlternatingGroup(%s)' % self.degree()
+
 
 class CyclicPermutationGroup(PermutationGroup_unique):
     def __init__(self, n):