398e6dd59ac7b8e3c49dc7917295e4fe4a9bcaba
Tobias Diez <code@tobiasdiez.com>
2021-02-08 22:10:17 +0100

    Narrow down imports to reduce install requires deps

--- src/sage_setup/extensions.py
+++ src/sage_setup/extensions.py
@@ -1,10 +1,10 @@
-from Cython.Build.Dependencies import default_create_extension
-import numpy
 
 def create_extension(template, kwds):
+    from Cython.Build.Dependencies import default_create_extension
+    
     # Add numpy and source folder to the include search path used by the compiler
     # This is a workaround for https://github.com/cython/cython/issues/1480
-
+    import numpy
     include_dirs = kwds.get('include_dirs', []) + [numpy.get_include(), 'src', '.']
     kwds['include_dirs'] = include_dirs
     return default_create_extension(template, kwds)
