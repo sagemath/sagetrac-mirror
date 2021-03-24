3d3af8f1162591a67d92bb29c8276233248ecf3f
Edgar Costa <edgarc@mit.edu>
2021-03-24 10:05:19 -0400

    it compiles

--- src/sage/matrix/matrix_nmod_dense.pxd
+++ src/sage/matrix/matrix_nmod_dense.pxd
@@ -2,10 +2,11 @@ from sage.libs.gmp.types cimport *
 from sage.libs.flint.types cimport nmod_mat_t
 
 from .matrix_dense cimport Matrix_dense
-from sage.rings.integer cimport Integer #FIXME
+from  sage.rings.finite_rings.integer_mod cimport NativeIntStruct
 
 cdef class Matrix_nmod_dense(Matrix_dense):
     cdef nmod_mat_t _matrix
+    cdef NativeIntStruct _modulus
     cdef void set_unsafe_si(self, Py_ssize_t i, Py_ssize_t j, long value)
     cdef long get_unsafe_si(self, Py_ssize_t i, Py_ssize_t j)
 
--- src/sage/matrix/matrix_nmod_dense.pyx
+++ src/sage/matrix/matrix_nmod_dense.pyx
@@ -9,16 +9,20 @@ AUTHORS:
 
 from cpython.sequence cimport *
 
-from cysignals.memory cimport sig_free
+from cysignals.signals cimport sig_str, sig_off
 
 from sage.structure.sage_object cimport SageObject
 from sage.libs.flint.nmod_mat cimport *
 
+from .args cimport SparseEntry, MatrixArgs_init
 
-cdef class Nmod_mat(Matrix_dense):
+import sage.matrix.matrix_space as matrix_space
+
+
+cdef class Matrix_nmod_dense(Matrix_dense):
     def __cinit__(self):
         sig_str("FLINT exception")
-        nmod_mat_init(self._matrix, self._nrows, self._ncols, self.__modulus.int64)
+        nmod_mat_init(self._matrix, self._nrows, self._ncols, self._modulus.int64)
         sig_off()
 
 
@@ -27,28 +31,26 @@ cdef class Nmod_mat(Matrix_dense):
 
     def __init__(self, parent, entries=None, bint coerce=True):
         self._parent = parent # MatrixSpace over IntegerMod_int or IntegerMod_int64
-        self.__modulus = parent._base.__modulus
+        self._modulus = parent._base._pyx_order
         ma = MatrixArgs_init(parent, entries)
 
-        cdef mp_limb_t z
-        for t in ma.iter(coerce, True):
+        cdef long z
+        for t in ma.iter(coerce, True): #????
             se = <SparseEntry>t
-            z = <mp_limb_t>se.entry
+            z = <long>se.entry
             nmod_mat_set_entry(self._matrix, se.i, se.j, z.ivalue)
 
     cdef set_unsafe(self, Py_ssize_t i, Py_ssize_t j, object x):
-        self.set_unsafe_si(self._matrix, i, j, x.ivalue)
+        self.set_unsafe_si(i, j, x.ivalue)
 
-    cdef set_unsafe_si(self, Py_ssize_t i, Py_ssize_t j, mp_limb_t ivalue):
-        nmod_mat_set_entry(self._matrix, i, j, ivalue)
+    cdef void set_unsafe_si(self, Py_ssize_t i, Py_ssize_t j, long value):
+        nmod_mat_set_entry(self._matrix, i, j, value)
 
-    cdef get_unsafe_si(self, Py_ssize_t i, Py_ssize_t j):
-        return nmod_mat_get_entry(self._matrix, i, j);
+    cdef long get_unsafe_si(self, Py_ssize_t i, Py_ssize_t j):
+        return nmod_mat_get_entry(self._matrix, i, j)
 
     cdef get_unsafe(self, Py_ssize_t i, Py_ssize_t j):
-        #FIXME, does this make sense?
-        cdef self.__modulus.element_class() z = self._parent._base._new_c_from_long(self.get_unsafe_si(i, j))
-        return z
+        return self._parent._base._new_c(self.get_unsafe_si(i, j))
 
     ########################################################################
     # LEVEL 1 helpers:
@@ -67,10 +69,10 @@ cdef class Nmod_mat(Matrix_dense):
         cdef Matrix_nmod_dense ans = Matrix_nmod_dense.__new__(Matrix_nmod_dense, P, None, None, None)
         return ans
 
-    cdef strong_echelon_form(self):
+    cdef void strong_echelon_form(self):
         nmod_mat_strong_echelon_form(self._matrix)
 
-    cdef howell_form(self):
+    cdef void howell_form(self):
         nmod_mat_howell_form(self._matrix)
 
 
