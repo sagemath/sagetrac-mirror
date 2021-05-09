633db6d3eb5aad5ba8b5998640c0ceffb1893ea4
Matthias Koeppe <mkoeppe@math.ucdavis.edu>
2021-05-09 12:53:29 -0700

    PolyhedralSets: Make it a subcategory of CombinatorialPolyhedralSets

--- src/sage/categories/polyhedra.py
+++ src/sage/categories/polyhedra.py
@@ -44,7 +44,9 @@ class PolyhedralSets(Category_over_base_ring):
          <class 'sage.categories.additive_magmas.AdditiveMagmas.element_class'>,
          <class 'sage.categories.finite_enumerated_sets.FiniteEnumeratedSets.element_class'>,
          <class 'sage.categories.enumerated_sets.EnumeratedSets.element_class'>,
+         <class 'sage.categories.combinatorial_polyhedral_sets.CombinatorialPolyhedralSets.Finite.element_class'>,
          <class 'sage.categories.finite_sets.FiniteSets.element_class'>,
+         <class 'sage.categories.combinatorial_polyhedral_sets.CombinatorialPolyhedralSets.element_class'>,
          <class 'sage.categories.sets_cat.Sets.element_class'>,
          <class 'sage.categories.sets_with_partial_maps.SetsWithPartialMaps.element_class'>,
          <class 'sage.categories.objects.Objects.element_class'>,
@@ -68,11 +70,14 @@ class PolyhedralSets(Category_over_base_ring):
         EXAMPLES::
 
             sage: PolyhedralSets(QQ).super_categories()
-            [Category of commutative magmas, Category of additive monoids]
+            [Category of commutative magmas,
+             Category of additive monoids,
+             Category of combinatorial polyhedral sets]
         """
         from sage.categories.magmas import Magmas
         from sage.categories.additive_monoids import AdditiveMonoids
-        return [Magmas().Commutative(), AdditiveMonoids()]
+        from sage.categories.combinatorial_polyhedral_sets import CombinatorialPolyhedralSets
+        return [Magmas().Commutative(), AdditiveMonoids(), CombinatorialPolyhedralSets()]
 
 
 
