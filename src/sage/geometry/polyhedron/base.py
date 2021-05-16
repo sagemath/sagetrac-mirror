feebc50b2fad496bda7f92bc85311624a7236a99
Matthias Koeppe <mkoeppe@math.ucdavis.edu>
2021-05-16 13:06:22 -0700

    Polyhedron_base.as_manifold_subset, relative_interior_manifold: Add argument ambient_chart

--- src/sage/geometry/polyhedron/base.py
+++ src/sage/geometry/polyhedron/base.py
@@ -5852,7 +5852,7 @@ class Polyhedron_base(Polyhedron_base4):
         return aff_name, aff_latex_name
 
     def as_manifold_subset(self, name=None, latex_name=None, start_index=0, ambient_space=None,
-                           names=None, **kwds):
+                           ambient_chart=None, names=None, **kwds):
         r"""
         Return the relative interior of ``self`` as a subset of its affine hull manifold.
 
@@ -5885,6 +5885,7 @@ class Polyhedron_base(Polyhedron_base4):
                                                       latex_name=relint_latex_name,
                                                       start_index=start_index,
                                                       ambient_space=ambient_space,
+                                                      ambient_chart=ambient_chart,
                                                       names=names,
                                                       **kwds)
         return relint_self.closure(name=name, latex_name=latex_name)
@@ -5914,7 +5915,7 @@ class Polyhedron_base(Polyhedron_base4):
         return relint_name, relint_latex_name
 
     def relative_interior_manifold(self, name=None, latex_name=None, start_index=0, ambient_space=None,
-                                   names=None, **kwds):
+                                   ambient_chart=None, names=None, **kwds):
         r"""
         Return the relative interior of ``self`` as an open subset of its affine hull manifold.
 
@@ -5949,7 +5950,9 @@ class Polyhedron_base(Polyhedron_base4):
         if name is None:
             name, latex_name = self._relative_interior_name_latex_name()
         aff_self = self.affine_hull_manifold(start_index=start_index,
-                                             ambient_space=ambient_space, names=names, **kwds)
+                                             ambient_space=ambient_space,
+                                             ambient_chart=ambient_chart,
+                                             names=names, **kwds)
         C_aff_self = aff_self.default_chart()
         if self.is_full_dimensional():
             x = vector(C_aff_self)
