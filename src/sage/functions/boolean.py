7022906ef572d5e928352f0547a3f95648305ed6
Matthias Koeppe <mkoeppe@math.ucdavis.edu>
2021-08-13 20:24:01 -0700

    sage.functions.boolean.or_symbolic: New

--- src/sage/functions/boolean.py
+++ src/sage/functions/boolean.py
@@ -7,6 +7,7 @@ from sage.symbolic.function import BuiltinFunction
 from sage.symbolic.expression import Expression
 from sage.symbolic.ring import SR
 
+
 class AndSymbolic(BuiltinFunction):
 
     def __init__(self):
@@ -68,4 +69,70 @@ class AndSymbolic(BuiltinFunction):
         """
         return r" \wedge ".join(latex(arg) for arg in args)
 
+
 and_symbolic = AndSymbolic()
+
+
+class OrSymbolic(BuiltinFunction):
+
+    def __init__(self):
+        r"""
+
+        EXAMPLES::
+
+            sage: from sage.functions.boolean import or_symbolic
+            sage: or_symbolic(True, True)
+            1
+            sage: or_symbolic(False, True)
+            1
+            sage: or_symbolic(x>0, False)
+            x > 0
+            sage: or_symbolic(x<0, False, x>1)
+            or_symbolic(x > 0, x < 1)
+            sage: or_symbolic(x<0, True, x>1)
+            0
+
+            sage: or_symbolic(x<0, x>1)._sympy_()
+            (x > 0) & (x > 1)
+
+        """
+        BuiltinFunction.__init__(self, 'or_symbolic', nargs=0,
+                                 conversions=dict(sympy='Or'))
+
+    def _eval_(self, *args):
+
+        if any(arg is True for arg in args):
+            return True
+
+        if not args:
+            # trivially false
+            return False
+
+        if not any(arg is False for arg in args):
+            # leave unevaluated
+            return
+
+        args = [arg for arg in args if arg is not False]
+
+        if not args:
+            # trivially false
+            return False
+
+        if len(args) == 1:
+            return args[0]
+
+        return or_symbolic(*args)
+
+    def _print_latex_(self, *args):
+        r"""
+        EXAMPLES::
+
+            sage: from sage.functions.boolean import or_symbolic
+            sage: latex(or_symbolic(x>0, x<1))
+            x > 0 \wedge x < 1
+
+        """
+        return r" \vee ".join(latex(arg) for arg in args)
+
+
+or_symbolic = OrSymbolic()
