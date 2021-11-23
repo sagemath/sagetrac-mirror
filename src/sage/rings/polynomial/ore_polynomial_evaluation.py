47349f786ed4521f75fb7a1ea7dffe15f85ab214
Durand Amaury <amadurand@math.u-bordeaux.fr>
2021-11-23 23:13:47 +0100

    Code and formatage improvement.
    OrePolynomialEvaluation now inherits from Morphism and improvement of the representation.

--- src/sage/rings/polynomial/ore_polynomial_evaluation.py
+++ src/sage/rings/polynomial/ore_polynomial_evaluation.py
@@ -1,58 +1,59 @@
 import sage
-from sage.misc.prandom import randint
-from sage.misc.cachefunc import cached_method
-from sage.rings.infinity import Infinity
-from sage.structure.category_object import normalize_names
-
-from sage.structure.unique_representation import UniqueRepresentation
-from sage.rings.ring import Algebra
-from sage.rings.integer import Integer
-from sage.structure.element import Element
-
-from sage.categories.commutative_rings import CommutativeRings
-from sage.categories.algebras import Algebras
-from sage.rings.ring import _Fields
+from sage.categories.morphism import IdentityMorphism, Morphism
+from sage.categories.homset import Hom
 
 
-class OrePolynomialEvaluation():
+class OrePolynomialEvaluation(Morphism):
     def __init__(self, P, c):
         self.P = P
         self.c = c
-        self._sigma = P.parent().twisting_morphism()
-        self._derivation = P.parent().twisting_derivation()
+        parent = P.parent()
+        super().__init__(Hom(parent.base_ring(), parent.base_ring()))
+        if parent.twisting_morphism() is None:
+            self._sigma = Hom(parent.base_ring(), parent.base_ring()).identity()
+        else:
+            self._sigma = parent.twisting_morphism()
+        if parent.twisting_derivation() is None:
+            self._derivation = lambda x: parent.base_ring()(0)
+        else:
+            self._derivation = parent.twisting_derivation()
 
-    def __call__(self, a):
+    def _call_(self, a):
         coeffs = self.P.list()
         result = coeffs[0]
-        #On peut factoriser facilement cette partie en initialisant sigma à l'identité et derivation à 0. Mais je n'ai pas trouvé comment faire.
-        if self._sigma == None and self._derivation == None:
-            for i in coeffs[1:]:
-                a = self.c*a
-                result += i * a
-        if self._sigma == None and self._derivation != None:
-            for i in coeffs[1:]:
-                a = self.c*a + self._derivation(a)
-                result += i * a
-        if self._sigma != None and self._derivation == None:
-            for i in coeffs[1:]:
-                a = self.c*self._sigma(a)
-                result += i * a
-        if self._sigma != None and self._derivation != None:
-            for i in coeffs[1:]:
-                a = self.c*self._sigma(a) + self._derivation(a)
-                result += i * a
+        for i in coeffs[1:]:
+            a = self.c*self._sigma(a) + self._derivation(a)
+            result += i * a
         return result
 
-    def __repr__(self):
-        s = "Ore Polynomial Evaluation P(c*sigma + der) with :\n"
-        s += "P is %s \n" % self.P._repr_()
-        s += "c is %s\n" % str(self.c)
-        if self._sigma == None:
-            s += "sigma is the identity morphism on %s \n" % self.P.parent().base_ring()._repr_()
+    def _repr_(self):
+        if self.P.parent().twisting_derivation() is None:
+            der = ""
         else:
-            s += "sigma is %s \n" % self._sigma._repr_()
-        if self._derivation == None:
-            s += "der is 0"
+            der = "der"
+        if self.c == 0:
+            sig = ""
         else:
-            s += "der is %s" % self._derivation._repr_()
+            if self.P.parent().twisting_derivation() is not None:
+                der += " + "
+            sig = "{}*sig".format(self.c)
+        evc = der + sig
+        s = "Ore evaluation endomorphism of {} by the pseudo-linear morphism ".format(self.P) + evc + " with :"
+        if self.P.parent().twisting_derivation() is not None:
+            s += "\n der is {}".format(self._derivation)
+        if self.c != 0:
+            s += "\n sig is {}".format(self._sigma)
+        s += "\n    a |----> "
+        coeffs = self.P.list()
+        n = len(coeffs)
+        if n == 1:
+            s += "{}".format(coeffs[0])
+            return s
+        for i in reversed(coeffs):
+            n -= 1
+            if i == 1:
+                s += "(" + evc + ")^{}(a) + ".format(n)
+            elif i != 0:
+                s += "{}*(".format(i) + evc + ")^{}(a) + ".format(n)
+            s = s[:-2]
         return s
