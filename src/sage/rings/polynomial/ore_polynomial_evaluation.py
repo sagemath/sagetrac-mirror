b0176fc32c86294b6cb5779b839a4a18c62bc069
Durand Amaury <amadurand@math.u-bordeaux.fr>
2021-12-02 11:34:33 +0100

    OrePolynomialEvaluation derive from RingHomomorphism.
    Upgrade the representation, add hash and _repr_short. Problem with TestSuite : isinstance(self, self.parent().category().element_class) return false. I don't know how to correct this error.

--- src/sage/rings/polynomial/ore_polynomial_evaluation.py
+++ src/sage/rings/polynomial/ore_polynomial_evaluation.py
@@ -1,16 +1,32 @@
-import sage
-from sage.categories.morphism import IdentityMorphism, Morphism
+from sage.rings.morphism import RingHomomorphism
 from sage.categories.homset import Hom
 
 
-class OrePolynomialEvaluation(Morphism):
+class OrePolynomialEvaluation(RingHomomorphism):
     def __init__(self, P, c):
+        """
+        Define the evaluation map for P and c.
+        
+        TESTS::
+
+            sage: A.<t> = GF(5^3)[]
+            sage: A = A.fraction_field()
+            sage: der = A.derivation()
+            sage: K = OrePolynomialRing(A, der, 'X')
+            sage: P = K.random_element()
+            sage: c = A.random_element()
+            sage: eval = P.eval(c)
+            sage: TestSuite(eval).run()
+            
+        """
         self.P = P
         self.c = c
+        self.coeffs = P.list()
         parent = P.parent()
-        super().__init__(Hom(parent.base_ring(), parent.base_ring()))
+        self.domain = parent.base_ring()
+        RingHomomorphism.__init__(self, Hom(self.domain, self.domain))
         if parent.twisting_morphism() is None:
-            self._sigma = Hom(parent.base_ring(), parent.base_ring()).identity()
+            self._sigma = Hom(self.domain, self.domain).identity()
         else:
             self._sigma = parent.twisting_morphism()
         if parent.twisting_derivation() is None:
@@ -43,17 +59,75 @@ class OrePolynomialEvaluation(Morphism):
             s += "\n der is {}".format(self._derivation)
         if self.c != 0:
             s += "\n sig is {}".format(self._sigma)
-        s += "\n    a |----> "
-        coeffs = self.P.list()
-        n = len(coeffs)
+        s += "\n Defn:   a |----> "
+        n = len(self.coeffs)
         if n == 1:
-            s += "{}".format(coeffs[0])
+            s += "{}".format(self.coeffs[0])
             return s
-        for i in reversed(coeffs):
+        for i in reversed(self.coeffs):
             n -= 1
-            if i == 1:
-                s += "(" + evc + ")^{}(a) + ".format(n)
-            elif i != 0:
-                s += "{}*(".format(i) + evc + ")^{}(a) + ".format(n)
-            s = s[:-2]
+            if n > 1:
+                if i == 1:
+                    s += "(" + evc + ")^{}(a) + ".format(n)
+                elif i != 0:
+                    s += "{}*(".format(i) + evc + ")^{}(a) + ".format(n)
+            elif n == 1:
+                if i == 1:
+                    s += "(" + evc + ")(a) + "
+                elif i != 0:
+                    s += "{}*(".format(i) + evc + ")(a) + "
+            elif n == 0:
+                if i != 0:
+                    s += "{}+ ".format(i)
+        s = s[:-2]
         return s
+
+    #def _update_slots(self):
+        
+    def _hash_(self):
+        return hash((self.P.parent(), self.P, self.c))
+
+    def domain(self):
+        return self.domain
+
+    def category(self):
+        return Hom(self.domain, self.domain).category()
+    
+    def _repr_short(self):
+        if self.P.parent().twisting_derivation() is None:
+            der = ""
+        else:
+            der = "der"
+        if self.c == 0:
+            sig = ""
+        else:
+            if self.P.parent().twisting_derivation() is not None:
+                der += " + "
+            sig = "{}*sig".format(self.c)
+        evc = der + sig
+        s = "a |----> "
+        n = len(self.coeffs)
+        if n == 1:
+            s += "{}".format(self.coeffs[0])
+            return s
+        for i in reversed(self.coeffs):
+            n -= 1
+            if n > 1:
+                if i == 1:
+                    s += "(" + evc + ")^{}(a) + ".format(n)
+                elif i != 0:
+                    s += "{}*(".format(i) + evc + ")^{}(a) + ".format(n)
+            elif n == 1:
+                if i == 1:
+                    s += "(" + evc + ")(a) + "
+                elif i != 0:
+                    s += "{}*(".format(i) + evc + ")(a) + "
+            elif n == 0:
+                if i != 0:
+                    s += "{}+ ".format(i)
+        s = s[:-2]
+        return s
+
+    #def _latex_(self):
+
+    
