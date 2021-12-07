5e019bc681f475c60c9818b55ff31f54c577445c
Durand Amaury <amadurand@math.u-bordeaux.fr>
2021-12-07 15:14:25 +0100

    Correct many errors in code (repr when P = 0), add some tests.

--- src/sage/rings/polynomial/ore_polynomial_evaluation.py
+++ src/sage/rings/polynomial/ore_polynomial_evaluation.py
@@ -1,10 +1,13 @@
-from sage.rings.morphism import RingHomomorphism
+from sage.rings.morphism import RingMap
 from sage.categories.homset import Hom
 
 
-class OrePolynomialEvaluation(RingHomomorphism):
+
+class OrePolynomialEvaluation(RingMap):
+
+    
     def __init__(self, P, c):
-        """
+        r"""
         Define the evaluation map for P and c.
         
         TESTS::
@@ -14,35 +17,49 @@ class OrePolynomialEvaluation(RingHomomorphism):
             sage: der = A.derivation()
             sage: K = OrePolynomialRing(A, der, 'X')
             sage: P = K.random_element()
+            sage: Q = K.random_element()
             sage: c = A.random_element()
             sage: eval = P.eval(c)
             sage: TestSuite(eval).run()
+            sage: x = A.random_element()
+            sage: y = A.random_element()
+            sage: P.eval(c)(x) + Q.eval(c)(x) == (P+Q).eval(c)(x)
+            True
+            sage: P.hilbert_shift(y).eval(c-y)(x) == P.eval(c)(x)
+            True
             
         """
         self.P = P
         self.c = c
         self.coeffs = P.list()
         parent = P.parent()
-        self.domain = parent.base_ring()
-        RingHomomorphism.__init__(self, Hom(self.domain, self.domain))
+        self._domain = parent.base_ring()
+        RingMap.__init__(self, Hom(self._domain, self._domain))
         if parent.twisting_morphism() is None:
-            self._sigma = Hom(self.domain, self.domain).identity()
+            self._sigma = Hom(self._domain, self._domain).identity()
         else:
             self._sigma = parent.twisting_morphism()
         if parent.twisting_derivation() is None:
-            self._derivation = lambda x: parent.base_ring()(0)
+            self._derivation = lambda x: 0
         else:
             self._derivation = parent.twisting_derivation()
 
     def _call_(self, a):
-        coeffs = self.P.list()
-        result = coeffs[0]
-        for i in coeffs[1:]:
+        if len(self.coeffs) == 0:
+            return self.P.parent()(0)
+        result = self.coeffs[0]
+        for i in self.coeffs[1:]:
             a = self.c*self._sigma(a) + self._derivation(a)
             result += i * a
         return result
 
     def _repr_(self):
+        if n == 0:
+            s = "Defn:   a |----> 0"
+            return s
+        if n == 1:
+            s += "Defn:   a |----> {}".format(self.coeffs[0])
+            return s
         if self.P.parent().twisting_derivation() is None:
             der = ""
         else:
@@ -61,6 +78,9 @@ class OrePolynomialEvaluation(RingHomomorphism):
             s += "\n sig is {}".format(self._sigma)
         s += "\n Defn:   a |----> "
         n = len(self.coeffs)
+        if n == 0:
+            s += "0"
+            return s
         if n == 1:
             s += "{}".format(self.coeffs[0])
             return s
@@ -86,12 +106,6 @@ class OrePolynomialEvaluation(RingHomomorphism):
         
     def _hash_(self):
         return hash((self.P.parent(), self.P, self.c))
-
-    def domain(self):
-        return self.domain
-
-    def category(self):
-        return Hom(self.domain, self.domain).category()
     
     def _repr_short(self):
         if self.P.parent().twisting_derivation() is None:
@@ -107,6 +121,9 @@ class OrePolynomialEvaluation(RingHomomorphism):
         evc = der + sig
         s = "a |----> "
         n = len(self.coeffs)
+        if n == 0:
+            s += "0"
+            return s
         if n == 1:
             s += "{}".format(self.coeffs[0])
             return s
@@ -127,7 +144,19 @@ class OrePolynomialEvaluation(RingHomomorphism):
                     s += "{}+ ".format(i)
         s = s[:-2]
         return s
+  
+    def _latex_(self):
+        r"""
+        EXAMPLES::
+        
+            sage: A = GF(5^2)
+            sage: A.<t> = A[]
+            sage: A = A.fraction_field()
+            sage: der = A.derivation()
+            sage: K = OrePolynomialRing(A, der, 'X')
+            sage: x = K.gen()
+            sage: (x.eval(t+1))._latex_()
+            'ev_{t + 1}(X)'
 
-    #def _latex_(self):
-
-    
+        """
+        return "ev_{%s}(%s)" %(self.c._latex_(), self.P._latex_())
