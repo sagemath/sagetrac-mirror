mkoeppe/sage-archive-2023-02-06

.github/workflows/extract-sage-local.sh: Use short option with touch so it works on macOS; handle var/lib/sage/venv* too

.github/workflows/extract-sage-local.sh
@@ -8,7 +8,7 @@ fi
 # Show all tar files
 ls -l $*
 
-# We specifically use the cygwin tar so that symlinks are saved/restored correctly on Windows.
+# Cygwin note: We specifically use the cygwin tar so that symlinks are saved/restored correctly on Windows.
 for a in $*; do
     echo Extracting $a
     (cd / && tar xf -) < $a
@@ -17,10 +17,21 @@ done
 
 # We set the installation records to the same mtime so that no rebuilds due to dependencies
 # among these packages are triggered.
-(cd "$SAGE_LOCAL"/var/lib/sage/installed/ && touch .dummy && touch --reference=.dummy *)
+dummy="$SAGE_LOCAL"/var/lib/sage/installed/.dummy
+if [ -f "$dummy" ]; then
+    touch "$dummy"
+    for inst in "$SAGE_LOCAL" "$SAGE_LOCAL"/var/lib/sage/venv*; do
+        inst="$tree"/var/lib/sage/installed
+        if [ -d "$inst" ]; then
+            # -r is --reference; the macOS version of touch does not accept the long option.
+            (cd "$inst" && touch -r "$dummy" .dummy *)
+            # Show what has been built already.
+            ls -l "$tree" "$inst"
+        fi
+    done
+fi
 
-# Show what has been built already.
-ls -l "$SAGE_LOCAL" "$SAGE_LOCAL"/var/lib/sage/installed/
+# Show how we are doing on free space.
 df -h
 
 # Rebase!

sagemath/sagetrac-mirror

.github/workflows/extract-sage-local.sh
@@ -8,7 +8,7 @@ fi
 # Show all tar files
 ls -l $*
 
-# We specifically use the cygwin tar so that symlinks are saved/restored correctly on Windows.
+# Cygwin note: We specifically use the cygwin tar so that symlinks are saved/restored correctly on Windows.
 for a in $*; do
     echo Extracting $a
     (cd / && tar xf -) < $a
@@ -17,10 +17,21 @@ done
 
 # We set the installation records to the same mtime so that no rebuilds due to dependencies
 # among these packages are triggered.
-(cd "$SAGE_LOCAL"/var/lib/sage/installed/ && touch .dummy && touch --reference=.dummy *)
+dummy="$SAGE_LOCAL"/var/lib/sage/installed/.dummy
+if [ -f "$dummy" ]; then
+    touch "$dummy"
+    for tree in "$SAGE_LOCAL" "$SAGE_LOCAL"/var/lib/sage/venv*; do
+        inst="$tree"/var/lib/sage/installed
+        if [ -d "$inst" ]; then
+            # -r is --reference; the macOS version of touch does not accept the long option.
+            (cd "$inst" && touch -r "$dummy" .dummy *)
+            # Show what has been built already.
+            ls -l "$tree" "$inst"
+        fi
+    done
+fi
 
-# Show what has been built already.
-ls -l "$SAGE_LOCAL" "$SAGE_LOCAL"/var/lib/sage/installed/
+# Show how we are doing on free space.
 df -h
 
 # Rebase!