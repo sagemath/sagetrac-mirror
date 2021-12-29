daceeff4a7a72346ef0c3b5b12d48357f0c3c335
Matthias Koeppe <mkoeppe@math.ucdavis.edu>
2021-12-29 13:36:34 -0800

    Restore lost # indirect doctest, # known bug

--- src/sage/geometry/polyhedron/backend_ppl.py
+++ src/sage/geometry/polyhedron/backend_ppl.py
@@ -235,7 +235,7 @@ class Polyhedron_ppl(Polyhedron_mutable):
 
         EXAMPLES::
 
-            sage: p = Polyhedron(vertices=[(0,1/2),(2,0),(4,5/6)],
+            sage: p = Polyhedron(vertices=[(0,1/2), (2,0), (4,5/6)],  # indirect doctest
             ....:                backend='ppl')
             sage: p.Hrepresentation()
             (An inequality (1, 4) x - 2 >= 0,
@@ -275,7 +275,7 @@ class Polyhedron_ppl(Polyhedron_mutable):
 
         EXAMPLES::
 
-            sage: p = Polyhedron(vertices=[(0,1/2),(2,0),(4,5/6)],
+            sage: p = Polyhedron(vertices=[(0,1/2), (2,0), (4,5/6)],  # indirect doctest
             ....:                backend='ppl')
             sage: p.Hrepresentation()
             (An inequality (1, 4) x - 2 >= 0,
