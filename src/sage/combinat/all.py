231fe031e4cab35489bcd6c0f9acdc920329523d
Matthias Koeppe <mkoeppe@math.ucdavis.edu>
2022-02-19 14:05:31 -0800

    src/sage/combinat/all.py: Deprecate the global imports of CombinatorialObject, CombinatorialClass, MapCombinatorialClass

--- src/sage/combinat/all.py
+++ src/sage/combinat/all.py
@@ -54,10 +54,13 @@ from sage.misc.lazy_import import lazy_import
 
 from .combinat import bell_number, catalan_number, euler_number, fibonacci, \
         lucas_number1, lucas_number2, stirling_number1, stirling_number2, \
-        polygonal_number, CombinatorialObject, CombinatorialClass, \
-        MapCombinatorialClass, \
+        polygonal_number, \
         tuples, number_of_tuples, unordered_tuples, number_of_unordered_tuples, \
         bell_polynomial, fibonacci_sequence, fibonacci_xrange, bernoulli_polynomial
+
+lazy_import('sage.combinat.combinat',
+            ('CombinatorialObject', 'CombinatorialClass', 'MapCombinatorialClass'),
+            deprecation=(32121, 'this class is deprecated, do not use'))
 
 lazy_import('sage.combinat.combinat',
             ('InfiniteAbstractCombinatorialClass', 'UnionCombinatorialClass',
