9c82ac9edc306aec77bc7bc815919a2c46b38aea
Vincent Delecroix <20100.delecroix@gmail.com>
2022-03-04 18:55:46 +0100

    33453: follow python specifications + improved doc

--- src/sage/stats/statistics.py
+++ src/sage/stats/statistics.py
@@ -21,10 +21,10 @@ Some doctests from the former ``basic_stats`` module::
 
         sage: import sage.stats.statistics as statistics
 
-        sage: v = [1,2,4,1,6,2,6,7,1]
-        sage: statistics.multimode(v)
+        sage: data = [1,2,4,1,6,2,6,7,1]
+        sage: statistics.multimode(data)
         [1]
-        sage: v.count(1)
+        sage: data.count(1)
         3
         sage: statistics.multimode([])
         []
@@ -51,23 +51,26 @@ Some doctests from the former ``basic_stats`` module::
         StatisticsError: no median for empty data
 """
 
-from statistics import mode, multimode, median
+from statistics import mode, multimode, median, StatisticsError
 from warnings import warn
 
-from sage.structure.coerce import is_numpy_type
-from sage.symbolic.constants import NaN
+from sage.structure.coerce import is_numpy_type, py_scalar_to_element
 from sage.misc.functional import sqrt
 
-def mean(v):
+def mean(data):
     """
-    Return the mean of the elements of ``v``.
+    Return the sample mean of ``data``.
 
-    For empty data ``v``, a warning is raised and the (symbolic) ``NaN`` is
-    returned.
+    For empty ``data``, a ``StatisticsError`` is raised.
+
+    If ``data`` is a numpy array, then the specialized method ``mean`` is
+    called. Otherwise, the elements are converted to sage objects and their
+    mean is computed.
 
     EXAMPLES::
 
         sage: import sage.stats.statistics as statistics
+
         sage: statistics.mean([pi, e])
         1/2*pi + 1/2*e
         sage: statistics.mean([I, sqrt(2), 3/5])
@@ -75,8 +78,6 @@ def mean(v):
         sage: statistics.mean([RIF(1.0103,1.0103), RIF(2)])
         1.5051500000000000?
         sage: statistics.mean(range(4))
-        1.5
-        sage: statistics.mean(srange(4))
         3/2
 
         sage: import numpy
@@ -85,29 +86,37 @@ def mean(v):
         3.0
 
         sage: statistics.mean([])
-        doctest:warning
+        Traceback (most recent call last):
         ...
-        RuntimeWarning: mean of empty data
-        NaN
+        statistics.StatisticsError: mean requires at least one data point
+
+    TESTS::
+
+        sage: import sage.stats.statistics as sage_statistics
+        sage: import statistics as python_statistics
+        sage: v = [float(1), float(2), float(3)]
+        sage: sage_statistics.mean(v) == python_statistics.mean(v)
+        True
     """
-    if is_numpy_type(type(v)):
+    if iter(data) is data:
+        data = list(data)
+    if not len(data):
+        raise StatisticsError(" mean requires at least one data point")
+    if is_numpy_type(type(data)):
         import numpy
-        if isinstance(v, numpy.ndarray):
-            return v.mean()
-
-    if not v:
-        warn("mean of empty data", RuntimeWarning)
-        return NaN
-
-    return sum(v) / len(v)
+        if isinstance(data, numpy.ndarray):
+            return data.mean()
+    return sum(map(py_scalar_to_element, data)) / len(data)
 
 
-def stdev(v):
+def stdev(data, xbar=None):
     r"""
-    Return the standard deviation of the elements of ``v``.
+    Return the square root of the sample variance of ``data``.
 
-    For empty data ``v``, a warning is raised and the (symbolic) ``NaN`` is
-    returned.
+    See :func:`variance` for arguments.
+
+    If ``data`` is a numpy array, then the specialized method ``std`` is
+    called.
 
     EXAMPLES::
 
@@ -125,29 +134,41 @@ def stdev(v):
         1.58113883008419
 
         sage: statistics.stdev([])
-        doctest:warning
+        Traceback (most recent call last):
         ...
-        RuntimeWarning: standard deviation of empty data
-        NaN
+        statistics.StatisticsError: stdev requires at least two data points
+        sage: statistics.stdev(numpy.array([]))
+        Traceback (most recent call last):
+        ...
+        statistics.StatisticsError: stdev requires at least two data points
+
+    TESTS::
+
+        sage: import sage.stats.statistics as sage_statistics
+        sage: import statistics as python_statistics
+        sage: v = [float(1), float(2), float(3)]
+        sage: sage_statistics.stdev(v) == python_statistics.stdev(v)
+        True
     """
-    if is_numpy_type(type(v)):
+    if iter(data) is data:
+        data = list(data)
+    if len(data) < 2:
+        raise StatisticsError("stdev requires at least two data points")
+    if is_numpy_type(type(data)):
         import numpy
-        if isinstance(v, numpy.ndarray):
-            return v.std(ddof=1)
-
-    if not v:
-        warn("standard deviation of empty data", RuntimeWarning)
-        return NaN
-
-    return sqrt(variance(v))
+        if isinstance(data, numpy.ndarray):
+            return data.std(ddof=1)
+    return sqrt(variance(data, xbar))
 
 
-def pstdev(v):
+def pstdev(data, mu=None):
     """
-    Return the population standard deviation of the elements of ``v``.
+    Return the square root of the population variance of ``data``.
 
-    For empty data ``v``, a warning is raised and the (symbolic) ``NaN`` is
-    returned.
+    See :func:`pvariance` for arguments.
+
+    If ``data`` is a numpy array, then the specialized method ``std`` is
+    called.
 
     EXAMPLES::
 
@@ -170,10 +191,13 @@ def pstdev(v):
         1.41421356237310
 
         sage: statistics.pstdev([])
-        doctest:warning
+        Traceback (most recent call last):
         ...
-        RuntimeWarning: population standard deviation of empty data
-        NaN
+        statistics.StatisticsError: pstdev requires at least one data point
+        sage: statistics.pstdev(numpy.array([]))
+        Traceback (most recent call last):
+        ...
+        statistics.StatisticsError: pstdev requires at least one data point
 
     TESTS::
     
@@ -183,30 +207,37 @@ def pstdev(v):
         sage: sage_statistics.pstdev(v) == python_statistics.pstdev(v)
         True
     """
-    if is_numpy_type(type(v)):
+    if iter(data) is data:
+        data = list(data)
+    if not len(data):
+        raise StatisticsError("pstdev requires at least one data point")
+    if is_numpy_type(type(data)):
         import numpy
-        if isinstance(v, numpy.ndarray):
-            return v.std()
-
-    if not v:
-        warn("population standard deviation of empty data", RuntimeWarning)
-        return NaN
-
-    return sqrt(pvariance(v))
+        if isinstance(data, numpy.ndarray):
+            return data.std()
+    return sqrt(pvariance(data, mu))
 
 
-def variance(v):
+def variance(data, xbar=None):
     r"""
-    Return the variance of the elements of `v`.
+    Return the sample variance of ``data``.
 
-    For empty data ``v``, a warning is raised and the (symbolic) ``NaN`` is
-    returned.
+    If you have already calculated the mean of your data, you can pass it as
+    the optional second argument ``xbar`` to avoid recalculating it.
+
+    If ``data`` is a numpy array, then the specialized method ``var`` is
+    called. Otherwise, the elements are converted to sage objects and their
+    population variance is computed.
 
     EXAMPLES::
 
         sage: import sage.stats.statistics as statistics
         sage: statistics.variance([1..6])
         7/2
+        sage: m = statistics.mean([1..6])
+        sage: statistics.variance([1..6], m)
+        7/2
+
         sage: statistics.variance([e, pi])
         1/2*(pi - e)^2
         sage: statistics.variance([I, sqrt(2), 3/5])
@@ -220,11 +251,14 @@ def variance(v):
         sage: statistics.variance([1.,2.,3.,4.,5.])
         2.50000000000000
 
-        sage: statistics.variance([])
-        doctest:warning
+        sage: statistics.variance([1])
+        Traceback (most recent call last):
         ...
-        RuntimeWarning: variance of empty data
-        NaN
+        statistics.StatisticsError: variance requires at least two data points
+        sage: statistics.variance(numpy.array([1]))
+        Traceback (most recent call last):
+        ...
+        statistics.StatisticsError: variance requires at least two data points
 
     TESTS::
 
@@ -234,31 +268,40 @@ def variance(v):
         sage: sage_statistics.variance(v) == python_statistics.variance(v)
         True
     """
-    if is_numpy_type(type(v)):
+    if iter(data) is data:
+        data = list(data)
+    if len(data) < 2:
+        raise StatisticsError('variance requires at least two data points')
+    if is_numpy_type(type(data)):
         import numpy
-        if isinstance(v, numpy.ndarray):
-            return v.var(ddof=1)
-
-    if not v:
-        warn("variance of empty data", RuntimeWarning)
-        return NaN
-
-    mu = mean(v)
-    return sum( (x - mu)**2 for x in v) / (len(v) - 1)
+        if isinstance(data, numpy.ndarray):
+            return data.var(ddof=1)
+    if xbar is None:
+        xbar = mean(data)
+    xbar = py_scalar_to_element(xbar)
+    return sum( (py_scalar_to_element(x) - xbar)**2 for x in data) / (len(data) - 1)
 
 
-def pvariance(v):
+def pvariance(data, mu=None):
     r"""
-    Return the population variance of the elements of `v`.
+    Return the population variance of ``data``.
 
-    For empty data ``v``, a warning is raised and the (symbolic) ``NaN`` is
-    returned.
+    The optional argument ``mu``, if given, should be the mean of the data. If
+    it is missing or None, the mean is automatically calculated.
+
+    If ``data`` is a numpy array, then the specialized method ``var`` is
+    called. Otherwise, the elements are converted to sage objects and their
+    population variance is computed.
 
     EXAMPLES::
 
         sage: import sage.stats.statistics as statistics
         sage: statistics.pvariance([1..6])
         35/12
+        sage: mu = statistics.mean([1..6])
+        sage: statistics.pvariance([1..6], mu)
+        35/12
+
         sage: statistics.pvariance([e, pi])
         1/4*(pi - e)^2
 
@@ -270,10 +313,13 @@ def pvariance(v):
         2.00000000000000
 
         sage: statistics.pvariance([])
-        doctest:warning
+        Traceback (most recent call last):
         ...
-        RuntimeWarning: population variance of empty data
-        NaN
+        statistics.StatisticsError: pvariance requires at least one data point
+        sage: statistics.pvariance(numpy.array([]))
+        Traceback (most recent call last):
+        ...
+        statistics.StatisticsError: pvariance requires at least one data point
 
     TESTS::
 
@@ -283,14 +329,15 @@ def pvariance(v):
         sage: sage_statistics.pvariance(v) == python_statistics.pvariance(v)
         True
     """
-    if is_numpy_type(type(v)):
+    if iter(data) is data:
+        data = list(data)
+    if len(data) < 1:
+        raise StatisticsError('pvariance requires at least one data point')
+    if is_numpy_type(type(data)):
         import numpy
-        if isinstance(v, numpy.ndarray):
-            return v.var()
-
-    if not v:
-        warn("population variance of empty data", RuntimeWarning)
-        return NaN
-
-    mu = mean(v)
-    return sum( (x - mu)**2 for x in v) / len(v)
+        if isinstance(data, numpy.ndarray):
+            return data.var()
+    if mu is None:
+        mu = mean(data)
+    mu = py_scalar_to_element(mu)
+    return sum( (py_scalar_to_element(x) - mu)**2 for x in data) / len(data)
