e61cdbb667b4ea1279eb1a2c3fb9632dc0fbdb61
Tobias Diez <code@tobiasdiez.com>
2022-03-25 13:58:17 +0100

    Rename test classes in nested_class_test

--- src/sage/misc/nested_class_test.py
+++ src/sage/misc/nested_class_test.py
@@ -10,8 +10,8 @@ EXAMPLES:
 Currently pickling fails for parents using nested classes (typically
 for categories), but deriving only from Parent::
 
-    sage: from sage.misc.nested_class_test import TestParent1, TestParent2, TestParent3, TestParent4
-    sage: P = TestParent1()
+    sage: from sage.misc.nested_class_test import _TestParent1, _TestParent2, _TestParent3, _TestParent4
+    sage: P = _TestParent1()
     sage: TestSuite(P).run()
     Failure ...
     The following tests failed: _test_elements, _test_pickling
@@ -19,7 +19,7 @@ for categories), but deriving only from Parent::
 They actually need to be in the NestedClassMetaclass. However, due to
 a technical detail, this is currently not directly supported::
 
-    sage: P = TestParent2()
+    sage: P = _TestParent2()
     Traceback (most recent call last):
     ...
     TypeError: metaclass conflict: the metaclass of a derived class must be a (non-strict) subclass of the metaclasses of all its bases
@@ -28,13 +28,13 @@ a technical detail, this is currently not directly supported::
 Instead, the easiest is to inherit from UniqueRepresentation, which is
 what you want to do anyway most of the time::
 
-    sage: P = TestParent3()
+    sage: P = _TestParent3()
     sage: TestSuite(P).run()
 
 This is what all Sage's parents using categories currently do. An
 alternative is to use ClasscallMetaclass as metaclass::
 
-    sage: P = TestParent4()
+    sage: P = _TestParent4()
     sage: TestSuite(P).run()
 
 """
@@ -54,13 +54,13 @@ from sage.misc.classcall_metaclass import ClasscallMetaclass
 from sage.misc.nested_class import NestedClassMetaclass
 
 
-class TestParent1(Parent):
+class _TestParent1(Parent):
     def __init__(self):
         """
         EXAMPLES::
 
-            sage: sage.misc.nested_class_test.TestParent1()
-            <sage.misc.nested_class_test.TestParent1_with_category object at ...>
+            sage: sage.misc.nested_class_test._TestParent1()
+            <sage.misc.nested_class_test._TestParent1_with_category object at ...>
         """
         from sage.categories.sets_cat import Sets
         Parent.__init__(self, category = Sets())
@@ -69,12 +69,12 @@ class TestParent1(Parent):
         pass
 
 
-class TestParent2(Parent, metaclass=NestedClassMetaclass):
+class _TestParent2(Parent, metaclass=NestedClassMetaclass):
     def __init__(self):
         """
         EXAMPLES::
 
-            sage: sage.misc.nested_class_test.TestParent2()
+            sage: sage.misc.nested_class_test._TestParent2()
             Traceback (most recent call last):
             ...
             TypeError: metaclass conflict: the metaclass of a derived class must be a (non-strict) subclass of the metaclasses of all its bases
@@ -86,14 +86,14 @@ class TestParent2(Parent, metaclass=NestedClassMetaclass):
         pass
 
 
-class TestParent3(UniqueRepresentation, Parent):
+class _TestParent3(UniqueRepresentation, Parent):
 
     def __init__(self):
         """
         EXAMPLES::
 
-            sage: sage.misc.nested_class_test.TestParent3()
-            <sage.misc.nested_class_test.TestParent3_with_category object at ...>
+            sage: sage.misc.nested_class_test._TestParent3()
+            <sage.misc.nested_class_test._TestParent3_with_category object at ...>
         """
         from sage.categories.sets_cat import Sets
         Parent.__init__(self, category = Sets())
@@ -102,13 +102,13 @@ class TestParent3(UniqueRepresentation, Parent):
         pass
 
 
-class TestParent4(Parent, metaclass=ClasscallMetaclass):
+class _TestParent4(Parent, metaclass=ClasscallMetaclass):
     def __init__(self):
         """
         EXAMPLES::
 
-            sage: sage.misc.nested_class_test.TestParent4()
-            <sage.misc.nested_class_test.TestParent4_with_category object at ...>
+            sage: sage.misc.nested_class_test._TestParent4()
+            <sage.misc.nested_class_test._TestParent4_with_category object at ...>
         """
         from sage.categories.sets_cat import Sets
         Parent.__init__(self, category=Sets())
@@ -117,8 +117,8 @@ class TestParent4(Parent, metaclass=ClasscallMetaclass):
         """
         EXAMPLES::
 
-            sage: from sage.misc.nested_class_test import TestParent4
-            sage: TestParent4() == TestParent4()
+            sage: from sage.misc.nested_class_test import _TestParent4
+            sage: _TestParent4() == _TestParent4()
             True
         """
         return self.__class__ == other.__class__
@@ -127,8 +127,8 @@ class TestParent4(Parent, metaclass=ClasscallMetaclass):
         """
         EXAMPLES::
 
-            sage: from sage.misc.nested_class_test import TestParent4
-            sage: TestParent4() != TestParent4()
+            sage: from sage.misc.nested_class_test import _TestParent4
+            sage: _TestParent4() != _TestParent4()
             False
         """
         return self.__class__ != other.__class__
@@ -139,8 +139,8 @@ class TestParent4(Parent, metaclass=ClasscallMetaclass):
 
         EXAMPLES::
 
-            sage: from sage.misc.nested_class_test import TestParent4
-            sage: hash(TestParent4()) == hash(TestParent4())
+            sage: from sage.misc.nested_class_test import _TestParent4
+            sage: hash(_TestParent4()) == hash(_TestParent4())
             True
         """
         return hash(8960522744683456048)
@@ -215,7 +215,7 @@ class ALBMeta(metaclass=NestedClassMetaclass):
 CMeta = ALBMeta.CMeta
 
 
-class TestNestedParent(UniqueRepresentation, Parent):
+class _TestNestedParent(UniqueRepresentation, Parent):
     """
     This is a dummy for testing source inspection of nested classes.
 
