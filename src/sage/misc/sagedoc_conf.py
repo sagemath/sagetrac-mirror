f2b34ae82f08aef7a3cd14b5ef44756c78bf4c13
Matthias Koeppe <mkoeppe@math.ucdavis.edu>
2022-06-09 17:32:48 -0700

    src/sage/misc/sagedoc_conf.py: Move default_role here from sage_docbuild.conf

--- src/sage/misc/sagedoc_conf.py
+++ src/sage/misc/sagedoc_conf.py
@@ -1,3 +1,6 @@
 r"""
 Sphinx configuration shared by sage.misc.sphinxify and sage_docbuild
 """
+
+# The reST default role (used for this markup: `text`) to use for all documents.
+default_role = 'math'
--- src/sage_docbuild/conf.py
+++ src/sage_docbuild/conf.py
@@ -150,9 +150,6 @@ release = sage.version.version
 # separators on all platforms.
 exclude_patterns = ['.build']
 
-# The reST default role (used for this markup: `text`) to use for all documents.
-default_role = 'math'
-
 # If true, '()' will be appended to :func: etc. cross-reference text.
 #add_function_parentheses = True
 
