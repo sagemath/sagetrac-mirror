64601adc95e2df32e5adec38a376fd053b09a2ec
Trevor K. Karn <karnx018@umn.edu>
2022-08-05 19:09:59 -0500

    Add rothe_diagram, n_reduced_words, and Stanley symmetric function

--- src/sage/combinat/permutation.py
+++ src/sage/combinat/permutation.py
@@ -219,6 +219,9 @@ AUTHORS:
 - Trevor K. Karn (2022-08-05): Add :meth:`Permutation.n_reduced_words`
 - Amrutha P, Shriya M, Divya Aggarwal (2022-08-16): Added Multimajor Index.
 
+- Trevor K. Karn (2022-08-05): Add :meth:`Permutation.n_reduced_words` and
+  :meth:`Permutation.stanley_symmetric_function`.
+
 Classes and methods
 ===================
 """
@@ -2993,31 +2996,14 @@ class Permutation(CombinatorialElement):
     def rothe_diagram(self):
         r"""
         Return the Rothe diagram of ``self``.
-
-        EXAMPLES::
-
-            sage: p = Permutation([4,2,1,3])
-            sage: D = p.rothe_diagram(); D
-            [(0, 0), (0, 1), (0, 2), (1, 0)]
-            sage: D.pp()
-            O O O .
-            O . . .
-            . . . .
-            . . . .
         """
         from sage.combinat.diagram import RotheDiagram
         return RotheDiagram(self)
 
-    def number_of_reduced_words(self):
+    def n_reduced_words(self):
         r"""
         Return the number of reduced words of ``self`` without explicitly
         computing them all.
-
-        EXAMPLES::
-
-            sage: p = Permutation([6,4,2,5,1,8,3,7])
-            sage: len(p.reduced_words()) == p.number_of_reduced_words()
-            True
         """
         Tx = self.rothe_diagram().peelable_tableaux()
 
@@ -5274,17 +5260,8 @@ class Permutation(CombinatorialElement):
 def _tableau_contribution(T):
     r"""
     Get the number of SYT of shape(``T``).
-
-    EXAMPLES::
-
-        sage: T = Tableau([[1,1,1],[2]])
-        sage: from sage.combinat.permutation import _tableau_contribution
-        sage: _tableau_contribution(T)
-        3
     """
-    from sage.combinat.tableau import StandardTableaux
     return(StandardTableaux(T.shape()).cardinality())
-
 ################################################################
 # Parent classes
 ################################################################
