9701928844d3f8e76a31d175cd6e7f097d1b1481
Lorenz Panny <lorenz@yx7.cc>
2022-08-24 12:16:51 +0800

    add changes to history

--- src/sage/schemes/elliptic_curves/ell_point.py
+++ src/sage/schemes/elliptic_curves/ell_point.py
@@ -103,6 +103,7 @@ AUTHORS:
 - Mariah Lenox (March 2011) -- Added ``tate_pairing`` and ``ate_pairing``
   functions to ``EllipticCurvePoint_finite_field`` class
 
+- Lorenz Panny (2022): point addition over general rings
 """
 
 # ****************************************************************************
