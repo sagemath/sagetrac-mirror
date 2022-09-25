be75b3c2189a265f99c56223dbf88b4f17b4c1d7
Jacob Dennerlein <nielrenned@gmail.com>
2022-09-25 15:50:41 -0400

    Implement the 'standard' sum and product methods.

--- src/sage/rings/padics/witt_vector.py
+++ src/sage/rings/padics/witt_vector.py
@@ -23,3 +23,47 @@ class WittVector(CommutativeRingElement):
     
     def _repr_(self):
         return '(' + ', '.join(map(str, self.vec)) + ')'
+    
+    def _add_(self, other):
+        P = self.parent()
+        C = self.__class__
+        
+        # As a slight optimization, we'll check for zero ahead of time.
+        # This has the benefit of allowing us to create polynomials, 
+        # even if ``P._algorithm`` is 'none'.
+        if other == P.zero():
+            return self
+        elif self == P.zero():
+            return other
+        
+        alg = P._algorithm
+        if alg == 'standard':
+            s = P.sum_polynomials
+            # note here this is tuple addition, i.e. concatenation
+            sum_vec = tuple(s[i](*(self.vec + other.vec)) for i in range(self.prec))
+            return C(P, vec=sum_vec)
+        else:
+            return NotImplemented
+    
+    def _mul_(self, other):
+        P = self.parent()
+        C = self.__class__
+        
+        # As a slight optimization, we'll check for zero or one ahead of time.
+        # This has the benefit of allowing us to create polynomials,
+        # even if ``P._algorithm`` is 'none'.
+        if self == P.zero() or other == P.zero():
+            return P.zero()
+        elif other == P.one():
+            return self
+        elif self == P.one():
+            return other
+        
+        alg = P._algorithm
+        if alg == 'standard':
+            p = P.prod_polynomials
+            # note here this is tuple addition, i.e. concatenation
+            prod_vec = tuple(p[i](*(self.vec + other.vec)) for i in range(self.prec))
+            return C(P, vec=prod_vec)
+        else:
+            return NotImplemented
