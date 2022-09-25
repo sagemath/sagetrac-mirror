952c9b5eb1f8d5096c6ff96bb4c2699d9d958b70
Jacob Dennerlein <nielrenned@gmail.com>
2022-09-25 14:35:40 -0400

    Add constructors for WittRings.

--- src/sage/rings/padics/witt_ring.py
+++ src/sage/rings/padics/witt_ring.py
@@ -1,20 +1,51 @@
 from .witt_vector import WittVector
 from sage.rings.ring import CommutativeRing
+from sage.categories.commutative_rings import CommutativeRings
 from sage.structure.unique_representation import UniqueRepresentation
 
-class WittRing_general(CommutativeRing, UniqueRepresentation):
+class WittRing_base(CommutativeRing, UniqueRepresentation):
     Element = WittVector
-    def __init__(self):
-        pass
+    def __init__(self, base_ring, prec, prime, generate_op_polys=False, category=None):
+        self.prec = prec
+        self.prime = prime
+        
+        self._algorithm = 'none'
+        self.sum_polys = None
+        self.prod_polys = None
+        
+        if generate_op_polys:
+            self._algorithm = 'standard'
+            self.generate_sum_and_product_polynomials(base)
+        
+        if category is None:
+            category = CommutativeRings()
+        CommutativeRing.__init__(self, base_ring, category=category)
 
-class WittRing_p_typical(WittRing_general):
-    pass
+class WittRing_p_typical(WittRing_base):
+    def __init__(self, base_ring, prec, prime, algorithm='none', category=None):
+        if algorithm == 'none' or algorithm == 'finotti':
+            generate_op_polys = False
+        elif algorithm == 'standard':
+            generate_op_polys = True
+        else:
+            raise ValueError(f'Algorithm {algorithm} is not a valid option.')
+        
+        self._algorithm = algorithm
+        
+        WittRing_base.__init__(self, base_ring, prec, prime, 
+            generate_op_polys=generate_op_polys, category=category)
 
 class WittRing_finite_field(WittRing_p_typical):
-    pass
+    def __init__(self, base_ring, prec, prime, category=None):
+        WittRing_p_typical.__init__(self, base_ring, prec, prime, 
+            algorithm='none', category=category)
 
-class WittRing_non_p_typical(WittRing_general):
-    pass
+class WittRing_non_p_typical(WittRing_base):
+    def __init__(self, base_ring, prec, prime, generate_op_polys=False, category=None):
+        WittRing_base.__init__(self, base_ring, prec, prime, 
+            generate_op_polys=generate_op_polys, category=category)
 
 class WittRing_p_invertible(WittRing_non_p_typical):
-    pass
+    def __init__(self, base_ring, prec, prime, category=None):
+        WittRing_non_p_typical.__init__(self, base_ring, prec, prime,
+            generate_op_polys=False, category=category)
--- src/sage/rings/padics/witt_ring_constructor.py
+++ src/sage/rings/padics/witt_ring_constructor.py
@@ -13,21 +13,29 @@ def WittRing(base_ring, prec=1, p=None, *args, **kwds):
         raise TypeError(f'Base ring {base_ring} must be a ring.')
     
     if base_ring not in _CommutativeRings:
-        raise TypeError(f'Cannot create Ring of Witt Vectors over {base}: {base} is not a commutative ring.')
+        raise TypeError(f'Cannot create Ring of Witt Vectors over {base_ring}: {base_ring} is not a commutative ring.')
     
     char = base_ring.characteristic()
     prime = None
     if p is None:
         if char not in _Primes:
-            raise ValueError(f'Cannot create Ring of Witt Vectors over {base}: {base} has non-prime characteristic, and no prime was supplied.')
+            raise ValueError(f'Cannot create Ring of Witt Vectors over {base_ring}: {base_ring} has non-prime characteristic and no prime was supplied.')
         else:
             prime = char
     else:
         prime = p
     
-    if prime == char: # p-typical
-        pass
-    else: # non-p-typical
-        pass
+    # Look for 'algorithm' and 'category' in kwds?
     
-    return WittRing_general()
+    # Throw error if given a keyword that's not used.
+    
+    if prime == char: # p-typical
+        if base_ring.is_field() and base_ring.is_finite():
+            return WittRing_finite_field(base_ring, prec, prime, category=_CommutativeRings)
+        else:
+            return WittRing_p_typical(base_ring, prec, prime, category=_CommutativeRings)
+    else: # non-p-typical
+        if base_ring(prime).is_unit():
+            return WittRing_p_invertible(base_ring, prec, prime, category=_CommutativeRings)
+        else:
+            return WittRing_non_p_typical(base_ring, prec, prime, category=_CommutativeRings)
