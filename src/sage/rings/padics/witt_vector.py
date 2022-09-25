ebaae1136d6dd00cd21f4dd69bc7cb6cead625d2
Jacob Dennerlein <nielrenned@gmail.com>
2022-09-25 14:44:53 -0400

    Implement basic WittVector methods.
    Implement constructor, compare, and representation methods for WittVectors.

--- src/sage/rings/padics/witt_vector.py
+++ src/sage/rings/padics/witt_vector.py
@@ -1,4 +1,25 @@
 from sage.structure.element import CommutativeRingElement
 
 class WittVector(CommutativeRingElement):
-   pass
+   def __init__(self, parent, vec=None):
+        self.prec = parent.precision()
+        B = parent.base()
+        if vec is not None:
+            if len(vec) != self.prec:
+                raise ValueError(f'{vec} is not the correct length. Expected length to be {self.prec}.')
+            self.vec = tuple(B(x) for x in vec)
+        else:
+            self.vec = (B(0) for i in range(self.prec))
+        CommutativeRingElement.__init__(self, parent)
+    
+    def _richcmp_(self, other, op):
+        from sage.structure.richcmp import op_EQ, op_NE
+        if op == op_EQ:
+            return self.vec == other.vec
+        elif op == op_NE:
+            return self.vec != other.vec
+        else:
+            return NotImplemented
+    
+    def _repr_(self):
+        return '(' + ', '.join(map(str, self.vec)) + ', ...)'
