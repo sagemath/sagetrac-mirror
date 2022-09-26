128fea647d80ba7e8cab1a4165f94e1b056eaaf2
Jacob Dennerlein <nielrenned@gmail.com>
2022-09-26 09:44:36 -0400

    Implement Witt Vector inversion and division.

--- src/sage/rings/padics/witt_vector.py
+++ src/sage/rings/padics/witt_vector.py
@@ -1,4 +1,5 @@
 from sage.structure.element import CommutativeRingElement
+from sage.rings.polynomial.polynomial_ring_constructor import PolynomialRing
 
 class WittVector(CommutativeRingElement):
     def __init__(self, parent, vec=None):
@@ -78,3 +79,43 @@ class WittVector(CommutativeRingElement):
             return all_ones*self
         neg_vec = tuple(-self.vec[i] for i in range(self.prec))
         return C(P, vec=neg_vec)
+    
+    def _div_(self, other):
+        P = self.parent()
+        # As a slight optimization, we'll check for one ahead of time.
+        # This has the benefit of allowing us to create polynomials,
+        # even if ``P._algorithm`` is 'none'.
+        if other == P.one():
+            return self
+        elif self == P.one():
+            return other._invert_()
+        
+        return self * other._invert_()
+    
+    def _invert_(self):
+        if not self.vec[0].is_unit():
+            raise ZeroDivisionError(f"Inverse of {self} does not exist.")
+        P = self.parent()
+        C = self.__class__
+        
+        if self == P.one():
+            return self
+        if self.prec == 1:
+            return P((self.vec[0]**-1, ))
+        
+        # Strategy: Multiply ``self`` by ``(Y_0, Y_1, ...)``, set equal
+        # to (1, 0, 0, ...), and solve.
+        var_names = [f'Y{i}' for i in range(1, self.prec)]
+        poly_ring = PolynomialRing(P.base(), var_names)
+        inv_vec = list( (self.vec[0]**-1, ) + poly_ring.gens()) # We'll fill this in one-by-one
+        
+        # TODO: Remove the algorithm argument once other algs are implemented
+        from sage.rings.padics.witt_ring_constructor import WittRing
+        W = WittRing(poly_ring, prec=P.prec, algorithm=P._algorithm)
+        prod_vec = (W(self.vec)*W(inv_vec)).vec
+        for i in range(1, self.prec):
+            poly = prod_vec[i](inv_vec[1:])
+            Y_i = poly.parent().gens()[i-1]
+            inv_vec[i] = -poly.constant_coefficient() / poly.monomial_coefficient(Y_i)
+        
+        return C(P, vec=inv_vec)
