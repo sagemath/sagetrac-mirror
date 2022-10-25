39f7ee231cffe13bee7ca84c25020877e5b16d00
Jing Guo <dev.guoj@gmail.com>
2022-10-25 12:43:02 +0800

    34559: Long time comments

--- src/sage/schemes/projective/projective_subscheme.py
+++ src/sage/schemes/projective/projective_subscheme.py
@@ -1454,7 +1454,7 @@ class AlgebraicScheme_subscheme_projective_field(AlgebraicScheme_subscheme_proje
 
             sage: P.<x,y,z> = ProjectiveSpace(QQ, 2)
             sage: X = P.subscheme([z^2 - 101*y^2 - 3*x*z])
-            sage: X.global_height()
+            sage: X.global_height() # long time
             4.61512051684126
         """
         return self.Chow_form().global_height(prec)
